9dd72b612d6d22286960d58a03e49486d6a024cb
rain <parkkwang2@naver.com>
2020-06-04 17:56:53 +0900

    1

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -7,10 +8,21 @@ int main(){
   cin.tie(NULL);
   cin.sync_with_stdio(false);	
 
-  int a,b,c,d,e;
-  int aArr[10]={-1};
-  cin>>a
-
+  int a,b,c,d;
+  int arr[1000];
+  cin>>a;//테스트 케이스의 개수
+  for(int j=0;j<a;j++){
+    cin>>b;//학생의 수
+    int i=0;
+    while(true){
+      cin>>arr[i];
+      i++
+      if(cin.eof()){
+        break;
+      }
+    }
+  }
+  
   return 0;
 }
 
