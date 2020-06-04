83eadb9eb0b1aa2056885d662cb538074772ec37
rain <parkkwang2@naver.com>
2020-06-04 17:17:59 +0900

    1

---
+++ BaekJoon/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  cin.tie(NULL);
+  cin.sync_with_stdio(false);	
+
+  int a,b,c,d,e;
+  int aArr[10]={-1};
+  cin>>a
+
+  return 0;
+}
+
