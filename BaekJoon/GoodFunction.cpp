01cfeeb65552dac3b213d6bee6bf5aa232f80c28
rain <parkkwang2@naver.com>
2020-06-09 09:40:38 +0900

    aa

---
+++ BaekJoon/GoodFunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int num){
+  if(num%2==0){
+    if(num==2){
+      return true;
+    }
+    return false;
+  }
+  if(num==1){
+    return false;
+  }
+  int tstNum=num-2;
+  while(tstNum>1){
+    if(num%tstNum==0){
+      return false;
+    }
+  }
+  return true;
+}
