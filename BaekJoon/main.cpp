6c19c33683d21c78e837d2b0628856c4bc9b7007
rain <parkkwang2@naver.com>
2020-06-11 17:40:44 +0900

    mergeSortOnGoing

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -7,42 +7,45 @@
 
 using namespace std;
 
-void Merge(vector<int> &v1, int start, int end){  //[start,end) sorting 
+void Merge(int* &array, int start, int end){  //[start,end) sorting 
   int middle=(end+start)/2; //두 vector 사이의 경계 idx
   int mainRotate=start;     //현재 sorting을 해야하는 곳. 이것 전까진 sorted
   int rotateFront=start;    //앞의 vector의 index
   int rotateBack=middle;//뒤의 vector의 index
-  vector<int>::iterator iter;
-  while(!(rotateFront==middle||rotateBack==end)){ //앞부분이 끝날때와 뒷부분이 끝날 때
-    iter=v1.begin();
-    if(*(iter+rotateFront)<*(iter+rotateBack)){
+  vector<int> tempV;  //front를 저장해놓은 임시 배열. 반대로 sorting되어 있을경우, 배열내에서 요소를 계속 옮기는 것은 비용이 너무 크다. 
+  for(int i=start;i<middle;i++){
+    tempV.push_back(array[i]);
+  }
+  vector<int>::iterator iter=tempV.begin();
+  int temp=*iter;   //unsorted의 첫번째 원소가 임시로 저장되어 있다.
+  while(!(rotateFront==middle)){ //앞부분이 끝날때와 뒷부분이 끝날 때
+    if(temp<array[rotateBack]){
+      array[mainRotate]=*(iter+rotateFront-start);
       rotateFront++;
+      iter++;
       mainRotate++;
+      temp=*iter;
     }else{
-      v1.insert(iter+mainRotate,*(iter+rotateBack));
+      array[mainRotate]=array[rotateBack];
       rotateBack++;
-      iter=v1.begin()+rotateBack;
-      v1.erase(iter); //기존의 원소는 한칸 뒤로 밀리게 된다. 이 원소는 위치를 이동했으므로 삭제해준다.
       mainRotate++; //하나 더 sorted 됐으므로
-      rotateFront++;  //1칸 밀리게 됨
-      middle++;    //1칸 밀리게 됨
     }
   }
   return;
 }
 
 
-void MergeSort(vector<int> &v1,int start,int end){
+void MergeSort(int* &array,int start,int end){
   //divide
   int middle=(end+start)/2;
   if(end-start==1){
     return;
   }else{
-    MergeSort(v1,start,middle);
-    MergeSort(v1,middle,end);
+    MergeSort(array,start,middle);
+    MergeSort(array,middle,end);
   }
   //conquer
-  Merge(v1,start,end);
+  Merge(array,start,end);
 }
 
 int main(){
@@ -50,18 +53,17 @@ int main(){
   cin.sync_with_stdio(false);
   
   //입력 받기 및 벡터 형성
-  int vectorSize, num;
-  cin>>vectorSize;
-  vector<int> arrayV;
-  for(int i=0;i<vectorSize;i++){
-    cin>>num;
-    arrayV.push_back(num);
+  int arraySize;
+  cin>>arraySize;
+  int* array= new int[arraySize];
+  for(int i=0;i<arraySize;i++){
+    cin>>array[i];
   }
   //sorting
-  MergeSort(arrayV,0,vectorSize);  //참조형식이기 때문에 반환형 불필요
+  MergeSort(array,0,arraySize);  //참조형식이기 때문에 반환형 불필요
   //출력 
-  for(int number:arrayV){
-    cout<<number<<"\n";
+  for(int i=0;i<arraySize;i++){
+    cout<<array[i]<<"\n";
   }
 
   
