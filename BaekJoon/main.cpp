7ce63553c30dbf38060569c447845e40416403f0
rain <parkkwang2@naver.com>
2020-06-15 17:48:27 +0900

    temp

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include <cstring>
+#include <string>
 
 #include "GoodFunction.h"
 
 using namespace std;
 
-class Point{
-  int xpos;
-  int ypos;
+class Alphabet{
+  int len;
+  string word;
 public:
-  Point(int xpos=-100001, int ypos=100001):xpos(xpos), ypos(ypos){}
-  Point& operator= (const Point& pos){
-    xpos=pos.xpos;
-    ypos=pos.ypos;
+  Alphabet(int len=0,string word=NULL):len(len), word(word){}
+  Alphabet& operator= (const Alphabet& alp){
+    len=alp.len;
+    word=alp.word;
     return *this;
   }
-  int operator[] (int idx) const{
-    if(idx==0){
-      return xpos;
-    }else{
-      return ypos;
-    }
+  // vector<char> operator[] (int idx) const{
+  //   if(idx==0){
+  //     return len;
+  //   }else{
+  //     return word;
+  //   }
   }
-  friend ostream& operator<<(ostream& os,const Point& pos); 
+  friend ostream& operator<<(ostream& os,const Alphabet& pos); 
 };
 
-ostream& operator<<(ostream& os, const Point& pos){
-  os<<pos.xpos<<' '<<pos.ypos;
+ostream& operator<<(ostream& os, const Alphabet& pos){
+  os<<pos.len<<' '<<pos.word;
   return os;
 }
 
@@ -36,19 +36,41 @@ int main(){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
   
-  vector<Point> pointV;
-  int N,X,Y;
+  vector<Alphabet> pointV;
+  int N;
+  string words;
+  Alphabet(4,words);
+  Alphabet[0];
   cin>>N;
   for(int i=0;i<N;i++){
     cin>>X>>Y;
-    pointV.push_back(Point(X,Y));
+    pointV.push_back(Alphabet(X,Y));
   }
 
   MergeSortT(pointV,0,0,N);
 
+  int count=1;
+  int prevNum=-100001;
+  int i;
+  for(i=0;i<N;i++){
+    if(pointV[i][0]==prevNum){
+      count++;
+    }else{
+      if(count>1){
+        MergeSortT(pointV,1,i-count,i);
+      }
+      prevNum=pointV[i][0];
+      count=1;
+    }
+  }
+  if(count>1){
+    MergeSortT(pointV,1,i-count,i);
+  }
+
   for(int i=0;i<N;i++){
     cout<<pointV[i]<<"\n";
   }
+  
   return 0;
 }
 
