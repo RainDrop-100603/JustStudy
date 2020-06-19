a0b15b62fad25ca705dfa3fc63c54f8ffcc56267
rain <parkkwang2@naver.com>
2020-06-19 10:55:21 +0900

    N_Queen Finish

--- BaekJoon/GoodFunction.cpp
+++ BaekJoon/GoodFunction.cpp
@@ -207,98 +207,23 @@ void PrintAllCombi4(int n, int r, int depth, int* num, int start){
     PrintAllCombi4(n,r,depth+1,num,i);
   }
 }
-int N_Queen(int N,vector<bool>& rowQueen,vector<bool>& colQueen,vector<bool>& lineRDQueen,vector<bool>& lineLDQueen, int row, int count){
+int N_Queen(int N,vector<bool>& colQueen,vector<bool>& lineRDQueen,vector<bool>& lineLDQueen, int row){
   int sum=0;
-  if(count==N){
+  if(row==N){
     return 1; //N개가 모두 놓아야 하나의 case 이므로 
   }
 
-  for(int i=row;i<N;i++){
-    if(!rowQueen[i]){
-      for(int j=0;j<N;j++){
-        if(!colQueen[j]){
-          if(!(lineRDQueen[N-i+j]||lineLDQueen[i+j])){
-            rowQueen[i]=true;
-            colQueen[j]=true;
-            lineRDQueen[N-i+j]=true;
-            lineLDQueen[i+j]=true;
-            sum+=N_Queen(N,rowQueen,colQueen,lineRDQueen,lineLDQueen,i+1,count+1);
-            rowQueen[i]=false;
-            colQueen[j]=false;
-            lineRDQueen[N-i+j]=false;
-            lineLDQueen[i+j]=false;
-          }
-        }
-      }
+  for(int j=0;j<N;j++){
+    if(!(colQueen[j]||lineRDQueen[N-row+j]||lineLDQueen[row+j])){
+      colQueen[j]=true;
+      lineRDQueen[N-row+j]=true;
+      lineLDQueen[row+j]=true;
+      sum+=N_Queen(N,colQueen,lineRDQueen,lineLDQueen,row+1);
+      colQueen[j]=false;
+      lineRDQueen[N-row+j]=false;
+      lineLDQueen[row+j]=false;        
     }
   }
-  
+
   return sum;
 }
-// int N_Queen(int N,vector<bool>& rowQueen,vector<bool>& colQueen,vector<Queen>& currentQ, int row, int count){
-//   int sum=0;
-//   if(count==N){
-//     return 1; //N개가 모두 놓아야 하나의 case 이므로 
-//   }
-
-//   int count2=0;
-//   for(int i=row;i<N;i++){
-//     if(!rowQueen[i]){
-//       for(int j=0;j<N;j++){
-//         if(!colQueen[j]){
-//           count2=0;
-//           for(const auto& ele: currentQ){
-//             if(!ele.QueenOK(i,j)){
-//               break;
-//             }
-//             count2++;
-//           }
-//           if(count!=count2){
-//             continue;
-//           }
-//           rowQueen[i]=true;
-//           colQueen[j]=true;
-//           currentQ.push_back(Queen(i,j));
-//           sum+=N_Queen(N,rowQueen,colQueen,currentQ,i+1,count+1);
-//           rowQueen[i]=false;
-//           colQueen[j]=false;
-//           currentQ.pop_back();
-//         }
-//       }
-//     }
-//   }
-  
-//   return sum;
-// }
-
-// int N_Queen(int N, table,int row, int count){
-//   int sum=0;
-//   if(count==N){
-//     return 1; //N개가 모두 놓아야 하나의 case 이므로 
-//   }
-//   for(int i=row;i<N;i++){
-//     for(int j=0;j<N;j++){
-//       if(table[i][j]==0){ //0이 아니면 막히는게 있다는 뜻
-//         for(int k=0;k<N;k++){ //table 정리 ++
-//           table[i][k]++;   //세로
-//           table[k][j]++;   //가로
-//           if(i-k>=0&&j-k>=0){table[i-k][j-k]++;} //왼아래   
-//           if(i-k>=0&&j+k<N){table[i-k][j+k]++;}//왼위
-//           if(i+k<N&&j-k>=0){table[i+k][j-k]++;}//오른아래
-//           if(i+k<N&&j+k<N){table[i+k][j+k]++;}//오른위
-//         } 
-//         sum+=N_Queen(N,table,i+1,count+1);
-//         for(int k=0;k<N;k++){ //table 정리 --
-//           table[i][k]--;   //세로
-//           table[k][j]--;   //가로
-//           if(i-k>=0&&j-k>=0){table[i-k][j-k]--;} //왼아래   
-//           if(i-k>=0&&j+k<N){table[i-k][j+k]--;}//왼위
-//           if(i+k<N&&j-k>=0){table[i+k][j-k]--;}//오른아래
-//           if(i+k<N&&j+k<N){table[i+k][j+k]--;}//오른위
-//         } 
-//       }
-//     }
-//   }
-  
-//   return sum;
-// }
--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -8,18 +8,6 @@
 
 using namespace std;
 
-//느려서 틀림, 시간초과 
-//퀸이 놓여있는 bool table을 만들자
-//새로운 위치에 퀸을 놓을수 있는지 bool table을 통해 체크하는 함수
-//이때 이 함수는 대각선만 체크한다.가로세로는 bool table을 통해 체크
-//  ㄴ  체크를 bool table이 아닌, bool 보조 table(가로세로 체크 전용)을 만들어서 많은 단계를 건너 뛸 수 있을것 같다.
-//    ㄴ  가망없는것은 뛰어넘는 BackTracking에 적합하다. 
-//      ㄴ  보조 table은 bool table을 참조해서 값을 업데이트 할 필요가 없다.
-//대각선 체크 함수 방법
-//  ㄴ  대각선은 똑같이 움직인다. 값의 차의 절대값이 같다면 대각선 관계에 있는 것이다.
-//    ㄴ  |x-x1|==|y-y1|, table의 모든 값과 비교해야하는데, 이걸 굳이 N^2 table로 만들 필요가 없을 것 같다.
-
-
 int main(){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
@@ -27,15 +15,19 @@ int main(){
   int N;
   cin>>N;
 
-  vector<bool> rowQueen(N);         //행
-  vector<bool> colQueen(N);         //열
+  vector<bool> colQueen(N);       //열       
   vector<bool> lineRDQueen(2*N-1);  //오른쪽 아래로 가는 대각선, F(n)=F(y-x+N);
   vector<bool> lineLDQueen(2*N-1);  //왼쪽 아래로 가는 대각선, F(n)=F(x+y)
   // vector<Queen> currentQ;
 
-  cout<<N_Queen(N,rowQueen,colQueen,lineRDQueen,lineLDQueen,0,0);
+  cout<<N_Queen(N,colQueen,lineRDQueen,lineLDQueen,0);
 
-  
+  /*  
+    기존의 알고리즘은 i행을 체크한 후, i+1행부터 N-1행까지 모두 체크해버렸다.
+      문제는 재귀를 쓰면, i+1행 체크할때는 i+2행부터 N-1행까지 모두 체크한다는 것이다
+        즉 N-1행은 총 N번 체크해버리는 것이다.
+    새로운 알고리즘은 i행을 체크한 후, i+1행만 체크하게 변경했다.
+  */
 
   return 0;
 }
