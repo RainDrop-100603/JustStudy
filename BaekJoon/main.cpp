f95ab559558883f8fc4d51cb033835bd07bfce27
rain <parkkwang2@naver.com>
2020-06-22 14:34:26 +0900

    bigFibonacci

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -14,23 +14,40 @@ int main(){
   cin.sync_with_stdio(false);
   
   //입력
-  int peopleNum;
-  cin>>peopleNum;
-  vector<vector<int>> table(peopleNum,vector<int>(peopleNum));
-  for(auto& ele1: table){
-    for(auto& ele2:ele1){
-      cin>>ele2;
+  int fibonacciN;
+  cin>>fibonacciN;
+  vector<char> fN_1;
+  fN_1.push_back(1); 
+  vector<char> fN_2;
+  fN_2.push_back(0);
+  int fnSize=1;   //매번 vector.size하기에는 귀찮으니
+  int upbeat=0;// 계산시 올라가는 수
+  int temp; // fN-2를 저장하기위해 임시적으로 저장
+  int tmpsum;
+
+  //계산
+  while(fibonacciN>1){
+    for(int i=0;i<fnSize;i++){
+    tmpsum=fN_1[i]+fN_2[i]+upbeat;
+    temp=fN_1[i];
+    fN_1[i]=tmpsum%10;
+    fN_2[i]=temp;
+    upbeat=tmpsum/10;
     }
+    if(upbeat==1){
+      fN_1.push_back(1);
+      fN_2.push_back(0);
+      fnSize++;
+      upbeat=0;
+    }
+    fibonacciN--;
   }
-  //요소 선언
-  vector<int> startTeam(peopleNum/2+1); //[0]에는 현재 배치된 팀원의 수 
-  vector<int> linkTeam(peopleNum/2+1);
-  int gap=INT32_MAX;
-  //함수 실행
-  GetLowGap(gap,table,startTeam,linkTeam,0);  //idx번째 선수를 각 팀에 배분. 배분이 끝나면 gap 을 계산하고 기존 gap보다 낮을시 갱신 
+  
 
-  cout<<gap;
-
+  //출력
+  for(int i=fnSize-1;i>=0;i--){
+    cout<<static_cast<int>(fN_1[i]);
+  }
 
   return 0;
 }
