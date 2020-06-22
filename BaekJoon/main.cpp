9a3572dade4f19913397b486d0c65c6c1a32c97d
rain <parkkwang2@naver.com>
2020-06-22 17:18:00 +0900

    fuckingLngLong

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -14,36 +14,21 @@ int main(){
   cin.sync_with_stdio(false);
   
   //입력
-  int N;
-  cin>>N;
   //변수
-  pair<int,int> fibBefore(1,0); //바로 전 or 0
-  pair<int,int> fib(0,1); //지금 or 1
-  pair<int,int> temp;
-  //계산
-  int i=0;
+  int caseNum;
+  cin>>caseNum;
   int input;
-  while(i<N){
+  for(int i=0;i<caseNum;i++){
+    long long array[6]={3,1,1,1,2,2};
     cin>>input;
-    fibBefore.first=1;
-    fibBefore.second=0;
-    fib.first=0;
-    fib.second=1;
-    while(input>1){
-      temp=fib;
-      fib.first+=fibBefore.first;
-      fib.second+=fibBefore.second;
-      fibBefore=temp;
-      input--;
+    int j=6;
+    while(j<=input){
+      array[j%6]=array[(j+1)%6]+array[(j+5)%6];
+      j++;
     }
-    if(input==0){
-      cout<<fibBefore.first<<' '<<fibBefore.second;
-    }else{
-      cout<<fib.first<<' '<<fib.second;
-    }
-    cout<<"\n";
-    i++;
+    cout<<array[input%6]<<"\n";
   }
+  
   
 
   
