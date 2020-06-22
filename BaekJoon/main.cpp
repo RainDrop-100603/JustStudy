e996cf0331bc39084e72cdd83794e2d5f9ad6941
rain <parkkwang2@naver.com>
2020-06-22 15:11:11 +0900

    fibonacciCount

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -14,40 +14,39 @@ int main(){
   cin.sync_with_stdio(false);
   
   //입력
-  int fibonacciN;
-  cin>>fibonacciN;
-  vector<char> fN_1;
-  fN_1.push_back(1); 
-  vector<char> fN_2;
-  fN_2.push_back(0);
-  int fnSize=1;   //매번 vector.size하기에는 귀찮으니
-  int upbeat=0;// 계산시 올라가는 수
-  int temp; // fN-2를 저장하기위해 임시적으로 저장
-  int tmpsum;
-
+  int N;
+  cin>>N;
+  //변수
+  pair<int,int> fibBefore(1,0); //바로 전 or 0
+  pair<int,int> fib(0,1); //지금 or 1
+  pair<int,int> temp;
   //계산
-  while(fibonacciN>1){
-    for(int i=0;i<fnSize;i++){
-    tmpsum=fN_1[i]+fN_2[i]+upbeat;
-    temp=fN_1[i];
-    fN_1[i]=tmpsum%10;
-    fN_2[i]=temp;
-    upbeat=tmpsum/10;
+  int i=0;
+  int input;
+  while(i<N){
+    cin>>input;
+    fibBefore.first=1;
+    fibBefore.second=0;
+    fib.first=0;
+    fib.second=1;
+    while(input>1){
+      temp=fib;
+      fib.first+=fibBefore.first;
+      fib.second+=fibBefore.second;
+      fibBefore=temp;
+      input--;
     }
-    if(upbeat==1){
-      fN_1.push_back(1);
-      fN_2.push_back(0);
-      fnSize++;
-      upbeat=0;
+    if(input==0){
+      cout<<fibBefore.first<<' '<<fibBefore.second;
+    }else{
+      cout<<fib.first<<' '<<fib.second;
     }
-    fibonacciN--;
+    cout<<"\n";
+    i++;
   }
   
 
-  //출력
-  for(int i=fnSize-1;i>=0;i--){
-    cout<<static_cast<int>(fN_1[i]);
-  }
+  
 
   return 0;
 }
