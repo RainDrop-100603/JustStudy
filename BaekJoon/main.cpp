09d69ebf351aabd4b4bc12cac821edf3c40d0ffe
rain <parkkwang2@naver.com>
2020-06-23 16:37:50 +0900

    bitonicLIS

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -23,9 +23,11 @@ int main(){
   //변수
   vector<int> inputNum(number); //입력을 저장하는 곳
   vector<int> count(number);    //count 저장 
+  vector<int> reverseCount(number); //뒤에서부터 시작해서 LIS
+  vector<int> bitonicCount(number); // X 기준으로 앞부분은 count, 뒷부분은 reverse의 max를 구하여 합치면 bitonic 
   int tmpcount=0; //비교를 위함 
 
-  //입력 
+  //입력 - 수열
   for(int i=0;i<number;i++){
     cin>>input;
     inputNum[i]=input;
@@ -38,9 +40,26 @@ int main(){
     tmpcount=0;
   }
 
+  //reverse
+  for(int i=number-1;i>=0;i--){
+    for(int j=number-1;j>i;j--){
+      if(inputNum[j]<inputNum[i]){
+        tmpcount=max(tmpcount,reverseCount[j]);
+      }
+    }
+    reverseCount[i]=tmpcount+1;
+    tmpcount=0;
+  }
+
+  //bitonic
+
+for(int i=0;i<number;i++){
+  bitonicCount[i]=count[i]+reverseCount[i]-1;
+}
+
   //출력
   for(int i=0;i<number;i++){
-    tmpcount=max(tmpcount,count[i]);
+    tmpcount=max(tmpcount,bitonicCount[i]);
   }
 
   cout<<tmpcount<<"\n";
