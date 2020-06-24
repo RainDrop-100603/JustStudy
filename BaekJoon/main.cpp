2049d935dc4767dfdc40394ad1409608ce8d33da
rain <parkkwang2@naver.com>
2020-06-24 13:59:39 +0900

    LCS_DP

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -16,42 +16,47 @@ int main(){
   //DynmaicPrograming
 
   //입력
-  int number;
-  cin>>number;
-  int input1,input2;
+  string str1;
+  string str2;
+  cin>>str1>>str2;
 
   //변수
-  int array[501]={0}; //array[i]=j, i전깃줄에 j전깃줄이 연결되어 있다.
-  int count[501]={0};    //LIS를 위한 count
-  int tmpcount=0; //비교를 위함 
+  int len1=str1.length();
+  int len2=str2.length();
+  vector<vector<int>> table(len1,vector<int>(len2));
 
-  //입력
-  for(int i=0;i<number;i++){
-    cin>>input1>>input2;
-    array[input1]=input2;
+  //table 초기값 
+  if(str1[0]==str2[0]){
+    table[0][0]=1;
+  }else{
+    table[0][0]=0;
   }
-
-  //array
-  for(int i=1;i<501;i++){
-    if(array[i]!=0){
-      for(int j=1;j<i;j++){
-        if(array[j]<array[i]){
-          tmpcount=max(tmpcount,count[j]);
-        }
-      }
-      count[i]=tmpcount+1;
-      tmpcount=0;
+  for(int i=1;i<len1;i++){
+    if(str1[i]==str2[0]){
+      table[i][0]=1;
+    }else{
+      table[i][0]=table[i-1][0];
     }
-    
+  }
+  for(int i=1;i<len2;i++){
+    if(str2[i]==str1[0]){
+      table[0][i]=1;
+    }else{
+      table[0][i]=table[0][i-1];
+    }
   }
 
-  //출력
-  for(int i=0;i<501;i++){
-    tmpcount=max(tmpcount,count[i]);
+  //table 채우기
+  for(int i=1;i<len1;i++){
+    for(int j=1;j<len2;j++){
+      if(str1[i]==str2[j]){
+        table[i][j]=table[i-1][j-1]+1;
+      }else{
+        table[i][j]=max(table[i][j-1],table[i-1][j]);
+      }
+    }
   }
-
-  cout<<number-tmpcount<<"\n";
-
+  cout<<table[len1-1][len2-1];
   return 0;
 }
 
