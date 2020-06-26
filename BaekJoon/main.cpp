c3ac714e5475db5e42422c47478b3a8941223ecd
rain <parkkwang2@naver.com>
2020-06-26 15:54:23 +0900

    min_GREEDY

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -21,30 +21,27 @@ int main(){
   
   //변수  
   bool minusCount=false;  //minus가 한번이라도 나오면 그 뒤는 전부 마이너스로 치환 가능 
-  string::size_type idxFrom=0;  // 숫자가 문자열로 변환되어 있기 때문에 범위를 알아야한다.
+  // string::size_type idxFrom;  // 숫자가 문자열로 변환되어 있기 때문에 범위를 알아야한다.
   char tmp;
-  int sum=0;
-  char prevtmp='+';
+  int sum=stoi(str1);
+
 
   //계산
   for(int i=0;i<str1.length();i++){
     tmp=str1[i];
     if(tmp=='-'||tmp=='+'){
       if(minusCount){
-        sum-=stoi(str1.substr(idxFrom),&idxFrom);
-      }else if(prevtmp=='-'){
+        sum-=stoi(str1.substr(i+1));
+      }else if(tmp=='-'){
         minusCount=true;
-        sum-=stoi(str1.substr(idxFrom),&idxFrom);
+        sum-=stoi(str1.substr(i+1));
       }else{
-        sum+=stoi(str1.substr(idxFrom),&idxFrom);
-      }
-      idxFrom=i;
-      prevtmp=tmp;
-      
+        sum+=stoi(str1.substr(i+1));
+      }     
     }
   }
 
-  // //출력
+  //출력
   cout<<sum;
 
   return 0;
