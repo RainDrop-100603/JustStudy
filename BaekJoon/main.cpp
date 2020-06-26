02e9efb3a5dd24bc0b67785dcdf1af7c890b3e78
rain <parkkwang2@naver.com>
2020-06-26 16:00:18 +0900

    multi_N_Factor

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -10,39 +10,31 @@
 using namespace std;
 
 int main(){
-  // cin.tie(NULL);
-  // cin.sync_with_stdio(false);
+  cin.tie(NULL);
+  cin.sync_with_stdio(false);
 
-  //Greedy
+  //Math3
 
   //입력
-  string str1;
-  cin>>str1;
+  int num1,num2;
   
   //변수  
-  bool minusCount=false;  //minus가 한번이라도 나오면 그 뒤는 전부 마이너스로 치환 가능 
-  // string::size_type idxFrom;  // 숫자가 문자열로 변환되어 있기 때문에 범위를 알아야한다.
-  char tmp;
-  int sum=stoi(str1);
-
 
   //계산
-  for(int i=0;i<str1.length();i++){
-    tmp=str1[i];
-    if(tmp=='-'||tmp=='+'){
-      if(minusCount){
-        sum-=stoi(str1.substr(i+1));
-      }else if(tmp=='-'){
-        minusCount=true;
-        sum-=stoi(str1.substr(i+1));
-      }else{
-        sum+=stoi(str1.substr(i+1));
-      }     
+  cin>>num1>>num2;
+  while(num1!=0||num2!=0){
+    if(num1%num2==0){
+      cout<<"multiple"<<"\n";
+    }else if(num2%num1==0){
+      cout<<"factor"<<"\n";
+    }else{
+      cout<<"neither"<<"\n";
     }
+    cin>>num1>>num2;
   }
 
   //출력
-  cout<<sum;
+ 
 
   return 0;
 }
