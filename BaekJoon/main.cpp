b6a4db8353b371cb77ff99a839c93f954a402c4b
rain <parkkwang2@naver.com>
2020-06-29 10:42:23 +0900

    tmp1

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -9,6 +9,16 @@
 
 using namespace std;
 
+void chkM(int tmpM,vector<int>& numbers){
+  int A0=numbers[0];
+  for(int i=2;i<numbers.size();i++){
+    if(numbers[i]%tmpM!=A0%tmpM){
+      return;
+    }
+  }
+  cout<<tmpM<<"\n";
+}
+
 int main(){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
@@ -24,25 +34,23 @@ int main(){
   }
 
   //변수
-  sort(numbers.begin(),numbers.end());
-  int minNum2=numbers[1];//입력중 두번째로 작은 수
-  int tmp;
+  sort(numbers.begin(),numbers.end());    //A0 ~ AN-1
+  int min0=numbers[0];   //A0
+  int min1=numbers[1];   //A1
+  int tmpM; //A0과 A1에서 M으로 인정되는 수
 
   //계산
-  for(int i=2;i<=minNum2;i++){
-    tmp=numbers[0]%i;
-    for(auto& ele:numbers){
-      if(ele%i!=tmp){
-        tmp=-1;
-        break;
+  //[2,A0]
+    for(int i=2;i<=min0;i++){
+      if(min0%i==min1%i){
+        chkM(i,numbers);
       }
     }
-    if(tmp!=-1){
-      cout<<i<<' ';
-    }
-  }  
+  //(A0,A1) 1개, M=A1-A0  
+    tmpM=min1-min0;
+    chkM(tmpM,numbers);
+  //[A1,00] 없다
   
-
   //출력
 
   return 0;
