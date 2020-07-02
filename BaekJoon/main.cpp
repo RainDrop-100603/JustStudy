2f2ded3947c78034cb6db839233fc3dc4efe131f
rain <parkkwang2@naver.com>
2020-07-02 17:57:08 +0900

    priorityQueue tmp

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -12,9 +12,14 @@ using namespace std;
 class PriorityQueue{
   vector<pair<int,int>> q;    //first= value, second=priority
   int idx=0;
+  vector<int> priority; //prioirty 개수 저장, 0~priority
 public:
-  PriorityQueue& push(pair<int,int>& p){
+  PriorityQueue(int maxpriority){
+    priority=vector<int>(maxpriority+1);
+  }
+  PriorityQueue& push(pair<int,int> p){
     q.push_back(p);
+    priority[p.second]++;
     return *this;
   }
   int size(){
@@ -31,7 +36,28 @@ public:
     if(this->empty()){
       return make_pair(-1,-1);
     }else{
+      priority[q[idx].second]--;
       return q[idx++];
+    }
+  }
+  int maxPriority(){
+    int len=priority.size();
+    for(int i=len;i>0;i--){
+      if(priority[i]){
+        return i;
+      }
+    }
+    return 0;
+  }
+  pair<int,int> prioirtyPop(){
+    if(this->empty()){
+      return make_pair(-1,-1);
+    }else{
+      int maxP=maxPriority();
+      while(this->front().second!=maxP){
+        this->push(this->pop());
+      }
+      return this->pop();
     }
   }
   pair<int,int> front(){
@@ -48,6 +74,13 @@ public:
       return q.back();
     }
   }
+  void clear(){
+    q.clear();
+    for(auto& ele:priority){
+      ele=0;
+    }
+  }
+  
 };
 
 int main(){
@@ -57,28 +90,35 @@ int main(){
   //Math3
 
   //입력
-  int n,k;
-  cin>>n>>k;
+  int testCase;
+  cin>>testCase;
+  int num,target; //num은 개수, target는 궁금한 문서의 현재위치=입력받은 순서 
+  int priority; //우선순위
 
   //변수
-  Queue q;
-  for(int i=1;i<=n;i++){
-    q.push(i);
-  }
+  PriorityQueue q(9);
+  int result=1; //인쇄되는 순서
 
 
   //계산
-  cout<<'<';
-  while(--n){
-    for(int i=1;i<k;i++){
-      q.push(q.pop());
+  while(testCase--){
+    //입력 및 초기화
+    result=1;
+    q.clear();
+    cin>>num>>target;
+    for(int i=0;i<num;i++){
+      cin>>priority;
+      q.push(make_pair(i,priority));
     }
-    cout<<q.pop()<<", ";
+    //계산
+    while(q.prioirtyPop().first!=target){
+      result++;
+    }
+    cout<<result<<"\n";
   }
-  cout<<q.pop()<<'>';
-
   
   //출력
+  // 1 0일 때 무한루프 
  
   return 0;
 }
