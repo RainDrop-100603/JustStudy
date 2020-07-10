3d74540e18e3e2fb1ac3951079e6ae80beef215b
rain <parkkwang2@naver.com>
2020-07-10 13:50:54 +0900

    Mul

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -9,34 +9,6 @@
 
 using namespace std;
 
-void Divide(vector<vector<int>>& table, pair<int,int> p, int size, int& one, int& zero, int& mOne){
-  int x(p.first),y(p.second);
-  bool oneValue=true; //모두 색이 같은 색종이인가
-  int value=table[x][y]; //1=blue, 0=white
-  for(int i=0;i<size;i++){
-    for(int j=0;j<size;j++){
-      if(table[x+i][y+j]!=value){
-        oneValue=false;
-        i=size; //바깥 루프조건 탈출
-        break;  //안쪽 루프 탈출 
-      }
-    }
-  }
-  if(oneValue){
-    if(value==1){
-      one++;
-    }else if(value==0){
-      zero++;
-    }else{
-      mOne++;
-    }
-    return;
-  }
-  Divide(table,make_pair(x,y),size/3,one,zero,mOne);Divide(table,make_pair(x,y+size/3),size/3,one,zero,mOne);Divide(table,make_pair(x,y+size*2/3),size/3,one,zero,mOne);
-  Divide(table,make_pair(x+size/3,y),size/3,one,zero,mOne);Divide(table,make_pair(x+size/3,y+size/3),size/3,one,zero,mOne);Divide(table,make_pair(x+size/3,y+size*2/3),size/3,one,zero,mOne);
-  Divide(table,make_pair(x+size*2/3,y),size/3,one,zero,mOne);Divide(table,make_pair(x+size*2/3,y+size/3),size/3,one,zero,mOne);Divide(table,make_pair(x+size*2/3,y+size*2/3),size/3,one,zero,mOne);
-}
-
 int main(){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
@@ -45,23 +17,38 @@ int main(){
   
 
   //입력
-  int N;
-  cin>>N;
-  vector<vector<int>> table(N,vector<int>(N)); //N*N 배열
-  for(auto& ele:table){
-    for(auto& ele2:ele){
-      cin>>ele2;
+  unsigned long long A,B,C;
+  cin>>A>>B>>C;
+  
+  //변수
+  int power=log2(B); //2^power<=B<2^(power+1)
+  vector<int> table(power+1); //table[i]=(A^(2^i))%C
+  unsigned long long tmp;
+  table[0]=A%C;
+  for(int i=1;i<=power;i++){     
+    tmp=table[i-1];
+    tmp=tmp*tmp;
+    tmp%=C;
+    table[i]=tmp;
+  }
+  long long result=1;
+
+  //계산
+  for(int i=power;i>=0;i--){
+    if(pow(2,i)<=B){
+      result*=table[i];
+      result%=C;
+      B-=static_cast<int>(pow(2,i));
+      // cout<<B<<"\n";
+      if(B==0){
+        break;
+        cout<<"flag";
+      }
     }
   }
   
-  //변수
-  int one(0),zero(0),mOne(0);
-  
-  //계산
-  Divide(table,make_pair(0,0),N,one,zero,mOne);
-  
   //출력
-  cout<<mOne<<endl<<zero<<endl<<one;
+  cout<<result;
   
  
   return 0;
