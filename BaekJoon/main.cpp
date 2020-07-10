dc94cc020957c682ffa259bdc5a808697edd033e
rain <parkkwang2@naver.com>
2020-07-10 17:41:07 +0900

    tmp

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -17,38 +17,38 @@ int main(){
   
 
   //입력
-  unsigned long long A,B,C;
-  cin>>A>>B>>C;
+  int K,N;
+  cin>>N>>K;
+  int modValue=1000000007;
   
   //변수
-  int power=log2(B); //2^power<=B<2^(power+1)
-  vector<int> table(power+1); //table[i]=(A^(2^i))%C
-  unsigned long long tmp;
-  table[0]=A%C;
-  for(int i=1;i<=power;i++){     
-    tmp=table[i-1];
-    tmp=tmp*tmp;
-    tmp%=C;
-    table[i]=tmp;
-  }
-  long long result=1;
-
+  vector<vector<int>> table(2,vector<int>(K+1));
+  bool first=true;  //fist면 첫번째     
+  table[0][0]=1;    //0C0 초기값, 값이 바뀔일어 없다
+  table[1][0]=1;    //1C0 초기값, 값이 바뀔일이 없다
   //계산
-  for(int i=power;i>=0;i--){
-    if(pow(2,i)<=B){
-      result*=table[i];
-      result%=C;
-      B-=static_cast<int>(pow(2,i));
-      // cout<<B<<"\n";
-      if(B==0){
-        break;
-        cout<<"flag";
+  for(int i=1;i<=K;i++){
+    first=!first;   //바꾸기
+    if(first){
+      table[0][i]=1;
+      for(int j=1;j<i;j++){
+        table[0][j]=(table[1][j-1]+table[1][j])%modValue;
+      }
+    }else{
+      table[1][i]=1;
+      for(int j=1;j<i;j++){
+        table[1][j]=(table[0][j-1]+table[0][j])%modValue;
       }
     }
   }
   
+  
   //출력
-  cout<<result;
+  if(first){
+    cout<<table[0][K];
+  }else{
+    cout<<table[1][K];
+  }
   
  
   return 0;
