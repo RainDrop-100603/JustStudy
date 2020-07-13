5c0674e59d8099004cdeb31039950f6b95e93f1f
rain <parkkwang2@naver.com>
2020-07-13 15:36:36 +0900

    tmp2

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -9,6 +9,36 @@
 
 using namespace std;
 
+void MatrixCopy(vector<vector<long long>>& m1,vector<vector<long long>>& m2){
+  int x=m1.size();
+  for(int i=0;i<x;i++){
+    for(int j=0;j<x;j++){
+      m1[i][j]=m2[i][j];
+    }
+  }
+}
+
+void MatrixClear(vector<vector<long long>>& m1){
+  int x=m1.size();
+  for(int i=0;i<x;i++){
+    for(int j=0;j<x;j++){
+      m1[i][j]=0;
+    }
+  }
+}
+
+void MatrixMulMod(vector<vector<long long>>& m1,vector<vector<long long>>& m2,vector<vector<long long>>& m3, int modValue){
+  int x=m1.size();
+  for(int i=0;i<x;i++){
+    for(int j=0;j<x;j++){
+      for(int k=0;k<x;k++){
+        m1[i][j]+=m2[i][k]*m3[k][j];
+      }
+      m1[i][j]%=modValue;
+    }
+  }
+}
+
 int main(){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
@@ -27,9 +57,8 @@ int main(){
   }
   
   //변수
-  vector<vector<long long>> matrix2(N,vector<long long>(N));
-  vector<vector<long long>> result(N,vector<long long>(N));
-
+  vector<vector<long long>> matrix2(N,vector<long long>(N));  //임시저장소
+  vector<vector<long long>> result(N,vector<long long>(N));   //결과
   vector<int> flag; //B=2^x1+2^x2+ ... 의 합이다. x1, x2, x3 ... 순서로 저장된 스택. x1>x2
   int tmp;
   int modValue=1000;
@@ -39,57 +68,27 @@ int main(){
     B-=pow(2,tmp);   //10^-15만큼 오차범위라 사용 가능. B의 범위가 더 커지면 바꾸자           
   }
   tmp=flag.front(); //계산할 때 사용 
+  int tmp2=flag.back();
 
   //계산
-  if(flag.back()==0){
-    flag.pop_back();
-    for(int i=0;i<N;i++){
-      for(int j=0;j<N;j++){
-        result[i][j]+=matrix[i][j];
-        result[i][j]%=modValue;
-      }
-    }     
+  //초기값
+  for(int i=1;i<=tmp2;i++){
+    MatrixMulMod(matrix2,matrix,matrix,modValue);
+    MatrixCopy(matrix,matrix2);
+    MatrixClear(matrix2);
   }
-  for(int i=1;i<=tmp;i++){
-    if(one){
-      one=!one;
-      for(int j=0;j<N;j++){   
-        for(int k=0;k<N;k++){
-          for(int x=0;x<N;x++){
-            matrix2[j][k]+=matrix[j][x]*matrix[x][k];
-            matrix2[j][k]%=modValue;
-          }
-        }
-      }
-    }else{
-      one=!one;
-      for(int j=0;j<N;j++){   
-        for(int k=0;k<N;k++){
-          for(int x=0;x<N;x++){
-            matrix[j][k]+=matrix2[j][x]*matrix2[x][k];
-            matrix[j][k]%=modValue;
-          }
-        }
-      }
-    }
+  flag.pop_back();
+  MatrixCopy(result,matrix);
+  tmp2++;
+
+  for(int i=tmp2;i<=tmp;i++){
+    MatrixMulMod(matrix2,matrix,matrix,modValue);
+    MatrixCopy(matrix,matrix2);
+    MatrixClear(matrix2);
     if(i==flag.back()){
-      flag.pop_back();
-      if(one){
-        for(int i=0;i<N;i++){
-          for(int j=0;j<N;j++){
-            result[i][j]+=matrix[i][j];
-            result[i][j]%=modValue;
-          }
-        }
-      }else{
-        for(int i=0;i<N;i++){
-          for(int j=0;j<N;j++){
-            result[i][j]+=matrix2[i][j];
-            result[i][j]%=modValue;
-          }
-        }
-      }
-      
+      MatrixMulMod(matrix2,matrix,result,modValue);
+      MatrixCopy(result,matrix2);
+      MatrixClear(matrix2);     
     }
   }
 
