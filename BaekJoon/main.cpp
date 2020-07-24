4241f922f22b45faf3ccd6338ad4aeb8c945edab
rain <parkkwang2@naver.com>
2020-07-24 15:36:51 +0900

    tmp

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -9,6 +9,43 @@
 
 using namespace std;
 
+//timeout
+void BK2261(){
+//입력
+  int pointNum,x,y;
+  cin>>pointNum;
+  vector<pair<int,int>> v;
+  for(int i=0;i<pointNum;i++){
+    cin>>x>>y;
+    v.push_back(make_pair(x,y));
+  }
+//준비
+  sort(v.begin(),v.end());
+  v.erase(unique(v.begin(),v.end()),v.end());
+  long long xDiff=v[0].first-v[1].first;
+  long long yDiff=v[0].second-v[1].second;
+  long long key=xDiff*xDiff+yDiff*yDiff;
+//계산
+  for(int i=0;i<pointNum;i++){
+    for(int j=i+1;j<pointNum;j++){
+      xDiff=v[i].first-v[j].first;
+      xDiff*=xDiff;
+      if(xDiff>key){
+        break;
+      }
+      yDiff=v[i].second-v[j].second;
+      yDiff*=yDiff;
+      if(yDiff>key){
+        break;
+      }
+      if(xDiff+yDiff<key){
+        key=xDiff+yDiff;
+      }
+    }
+  }
+//출력
+  cout<<key;
+}
 
 int main(){
   cin.tie(NULL);
@@ -18,21 +55,17 @@ int main(){
   
 
   //입력
-  int testcase;
+  
 
   //변수
   
 
-  
-
   //계산
 
-  
-  //초기값
-  
+
   
   //출력
-  
+  BK2261();
   
  
   return 0;
