1cab8eba84b653b709cf8771d0363d4f4226ff88
rain <parkkwang2@naver.com>
2020-08-03 10:51:42 +0900

    tmp

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <algorithm>
 #include <utility>
+#include <map>
 
 //#include "GoodFunction.h"
 
@@ -24,29 +25,63 @@ void BK2261(){
   v.erase(unique(v.begin(),v.end()),v.end());
   long long xDiff=v[0].first-v[1].first;
   long long yDiff=v[0].second-v[1].second;
-  long long key=xDiff*xDiff+yDiff*yDiff;
+  long long minLen=xDiff*xDiff+yDiff*yDiff;
 //계산
   for(int i=0;i<pointNum;i++){
     for(int j=i+1;j<pointNum;j++){
       xDiff=v[i].first-v[j].first;
       xDiff*=xDiff;
-      if(xDiff>key){
+      if(xDiff>minLen){
         break;
       }
       yDiff=v[i].second-v[j].second;
       yDiff*=yDiff;
-      if(yDiff>key){
-        break;
-      }
-      if(xDiff+yDiff<key){
-        key=xDiff+yDiff;
+      if(xDiff+yDiff<minLen){
+        minLen=xDiff+yDiff;
       }
     }
   }
 //출력
-  cout<<key;
+  cout<<minLen;
 }
-
+void BK2261_2(){
+//입력
+  int pointNum,x,y;
+  cin>>pointNum;
+  vector<pair<int,int>> v;
+  for(int i=0;i<pointNum;i++){
+    cin>>x>>y;
+    v.push_back(make_pair(x,y));
+  }
+//준비
+  sort(v.begin(),v.end());
+  v.erase(unique(v.begin(),v.end()),v.end());
+  long long xDiff=v[0].first-v[1].first;
+  long long yDiff=v[0].second-v[1].second;
+  long long minLen=xDiff*xDiff+yDiff*yDiff;
+  // xValue Tree
+  int prev=-1;
+  map<int,int> xTree;
+  for(int i=0;i<v.size();i++){
+    if(v[i].first!=prev){
+      prev=v[i].first;
+      xTree.insert(make_pair(prev,i));
+    }
+  }
+//계산
+  map<int,int>::iterator iter;
+  map<int,int>::iterator upperbound;
+  while(true){
+    int minLen2=sqrt(minLen);
+    int minLen3=minLen;
+    upperbound=xTree.upper_bound(minLen2+1);
+    for(iter=xTree.lower_bound(minLen2);*iter;iter++){
+      vector<int> yValueArr;
+    }
+  }
+//출력
+  cout<<minLen; 
+}
 int main(){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
