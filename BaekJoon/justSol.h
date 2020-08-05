5a9dec2e3b1eeeb0a88d7c5e633020a740e3e5a7
rain <parkkwang2@naver.com>
2020-08-05 17:30:03 +0900

    tmp

--- BaekJoon/justSol.h
+++ BaekJoon/justSol.h
@@ -1218,7 +1218,43 @@ void BK2805(){
 
   printf("%d",left-1);
 }
-
+void BK2110(){ 
+//input
+  int n,m;
+  scanf("%d %d",&n,&m);
+  vector<int> v(n);
+  for(int i=0;i<n;i++){
+    scanf("%d",&v[i]);
+  }
+//prepare  
+  sort(v.begin(),v.end());
+//calc
+  int left,right,mid;  
+  left=1;right=v[n-1];
+  vector<int>::iterator iterFirst=v.begin();
+  vector<int>::iterator iterEnd=v.end();
+  vector<int>::iterator iterNow;
+  while(true){
+    mid=(left+right)/2;
+    iterNow=iterFirst;
+    for(int i=0;i<m-1;i++){   //first = 1, add m-1
+      if(iterNow==iterEnd){
+        break;
+      }
+      iterNow=lower_bound(v.begin(),v.end(),*iterNow+mid);
+    }
+    if(iterNow==iterEnd){ //End여서는 안된다/
+      right=mid-1;
+    }else{
+      left=mid+1;
+    }
+    if(left>right){
+      break;
+    }
+  }
+  
+  printf("%d",left-1);
+}
 
 
 #endif
