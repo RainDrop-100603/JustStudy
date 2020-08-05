45505680e0ec46ee748bdfaee788b27b3841266b
rain <parkkwang2@naver.com>
2020-08-05 13:47:36 +0900

    BK2110

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -20,42 +20,32 @@ void BK2110(){
   }
 //prepare  
   sort(v.begin(),v.end());
-  int prev=v[0];
-  int now;
-  vector<int> d(n-1);     //이웃 집과의 거리를 저장한 배열 
-  for(int i=0;i<n-1;i++){
-    now=v[i+1];
-    d[i]=now-prev;
-    prev=now;
-  }
-  sort(d.begin(),d.end());  
 //calc
-  int dist,left,right,mid;
-  left=0;right=n-1;
+  int left,right,mid;  
+  left=1;right=v[n-1];
   vector<int>::iterator iterFirst=v.begin();
   vector<int>::iterator iterEnd=v.end();
   vector<int>::iterator iterNow;
   while(true){
-    dist=d[(left+right)/2];
+    mid=(left+right)/2;
     iterNow=iterFirst;
-    int i=0;
-    for(i=0;i<m;i++){
+    for(int i=0;i<m-1;i++){   //first = 1, add m-1
       if(iterNow==iterEnd){
         break;
       }
-      iterNow=lower_bound(v.begin(),v.end(),*iterNow+dist);
+      iterNow=lower_bound(v.begin(),v.end(),*iterNow+mid);
     }
     if(iterNow==iterEnd){ //End여서는 안된다/
-      right=(left+right)/2-1;
+      right=mid-1;
     }else{
-      left=(left+right)/2+1;
+      left=mid+1;
     }
     if(left>right){
       break;
     }
   }
-
-  printf("%d",d[left-1]);
+  
+  printf("%d",left-1);
 }
 int main(){
   //cin.tie(NULL);
