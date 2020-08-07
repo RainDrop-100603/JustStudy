7c875eb3916bf8ac467cc4a88af2f6353da3b757
Rain <parkkwang2@naver.com>
2020-08-07 17:53:25 +0900

    tmp

--- BaekJoon/justSol.h
+++ BaekJoon/justSol.h
@@ -1443,5 +1443,44 @@ void BK11286(){
   }
   
 }
+void BK1655(){
+//input
+  int N;
+  scanf("%d",&N);
+//prepare  
+  int mid,tmp,gap;
+  multiset<int> leftQ;
+  multiset<int> rightQ;
+//calc
+  //n==1
+  scanf("%d",&mid);
+  printf("%d\n",mid);
+  map<int,int> tree;
+  //n>1
+  for(int i=0;i<N-1;i++){
+    scanf("%d",&tmp);
+    if(tmp>mid){
+      rightQ.insert(tmp);
+      gap=rightQ.size()-leftQ.size();
+      while(gap>1){
+        leftQ.insert(mid);
+        mid=*rightQ.begin();
+        rightQ.erase(rightQ.begin());
+        gap--;
+      }
+    }else{
+      leftQ.insert(tmp);
+      gap=leftQ.size()-rightQ.size();
+      while(gap>0){
+        rightQ.insert(mid);
+        mid=*leftQ.rbegin();
+        leftQ.erase(--leftQ.end());
+        gap--;
+      }
+    }
+    printf("%d\n",mid);
+  }
+}
+
 
 #endif
--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -11,44 +11,63 @@
 
 using namespace std;
 
-void BK1655(){
+void BK2293_Memover(){
 //input
-  int N;
-  scanf("%d",&N);
-//prepare  
-  int mid,tmp,gap;
-  multiset<int> leftQ;
-  multiset<int> rightQ;
-//calc
-  //n==1
-  scanf("%d",&mid);
-  printf("%d\n",mid);
-  map<int,int> tree;
-  //n>1
-  for(int i=0;i<N-1;i++){
-    scanf("%d",&tmp);
-    if(tmp>mid){
-      rightQ.insert(tmp);
-      gap=rightQ.size()-leftQ.size();
-      while(gap>1){
-        leftQ.insert(mid);
-        mid=*rightQ.begin();
-        rightQ.erase(rightQ.begin());
-        gap--;
-      }
-    }else{
-      leftQ.insert(tmp);
-      gap=leftQ.size()-rightQ.size();
-      while(gap>0){
-        rightQ.insert(mid);
-        mid=*leftQ.rbegin();
-        leftQ.erase(--leftQ.end());
-        gap--;
-      }
-    }
-    printf("%d\n",mid);
+  int N,K;
+  scanf("%d %d",&N,&K);
+  vector<int> arr(N);
+  for(auto& ele: arr){
+    scanf("%d",&ele);
   }
+//prepare  
+  sort(arr.begin(),arr.end());
+  vector<vector<int>> table(K+1,vector<int>(N));  //A(ij)==최댓값이 arr[j]인 i의 경우의 수, 0열에는 1저장
+  for(int i=0;i<N;i++){
+    table[0][i]=1;
+  } 
+  int dist,prev;
+//calc
+  for(int i=1;i<K+1;i++){
+    prev=0; //A(ij)=A(i,j-1)+A(i-arr[j],j); ,prev=A(i,j-1);
+    for(int j=0;j<N;j++){
+      dist=i-arr[j];
+      if(dist>=0){
+        prev+=table[dist][j];
+      }
+      table[i][j]=prev;
+    }
+  }
+  printf("%d",table[K][N-1]);
 }
+void BK2293_2(){  //table에 유효한 값만 저장하자 
+//input
+  int N,K;
+  scanf("%d %d",&N,&K);
+  vector<int> arr(N);
+  for(auto& ele: arr){
+    scanf("%d",&ele);
+  }
+//prepare  
+  sort(arr.begin(),arr.end());
+  vector<vector<pair<int,int>>> table(N,vector<pair<int,int>>(0));  // i행은 arr[i]의 유효한 값: pair<num,count>를 저장
+  for(int i=0;i<N;i++){                                             // i 행의 pair<num,count> : num 에서 최댓값이 i인 경우의 수는 count
+    table[i].push_back(make_pair(0,1));                             // 기존의 A(i,j)는 table[j].find(lowerbound[i]);
+  }                                                                 //lower bound는 pair.first 값이다. 이걸 어떻게 찾을 것이냐? map?직접 만들기? 
+  int dist,prev;
+//calc
+  for(int i=1;i<K+1;i++){
+    prev=0; //A(ij)=A(i,j-1)+A(i-arr[j],j); ,prev=A(i,j-1);
+    for(int j=0;j<N;j++){
+      dist=i-arr[j];
+      if(dist>=0){
+        prev+=table[dist][j];
+      }
+      table[i][j]=prev;
+    }
+  }
+  printf("%d",table[K][N-1]);
+}
+
 int main(){
   //cin.tie(NULL);
   //cin.sync_with_stdio(false);
@@ -57,7 +76,7 @@ int main(){
   //변수
   //계산
   //출력
-  BK1655();
+  BK2293();
   
   return 0;
 }
