5c65f6420eb8ffa438d9967e9ca877a8a9613cb5
Rain <parkkwang2@naver.com>
2020-08-07 09:51:41 +0900

    BK1655

--- BaekJoon/justSol.h
+++ BaekJoon/justSol.h
@@ -9,6 +9,7 @@
 #include <utility>
 #include <algorithm>
 #include <set>
+#include <map>
 
 using namespace std;
 
@@ -1394,6 +1395,53 @@ void BK11279(){
   }
   
 }
-
+void PrintIter(map<int,int>::iterator& iter,map<int,int>& tree){
+  cout<<iter->first<<'\n';
+  if(iter->second==1){
+    tree.erase(iter);
+  }else{
+    iter->second-=1;
+  }
+}
+void BK11286(){
+//input
+  int N;
+  scanf("%d",&N);
+//prepare  
+//calc
+  map<int,int> tree;
+  int tmp;
+  for(int i=0;i<N;i++){
+    scanf("%d",&tmp);
+    if(tmp==0){
+      if(tree.size()==0){
+        printf("%d\n",0);
+      }else{
+        map<int,int>::iterator iterP=tree.lower_bound(0); //양수에서 가장 작은 수
+        if(iterP==tree.begin()){  //양수만 있는 경우
+          PrintIter(iterP,tree);
+        }else if(iterP==tree.end()){  //음수만 있는 경우 
+          PrintIter(--tree.end(),tree);
+        }else{
+          map<int,int>::iterator iterM=--tree.lower_bound(0);             //음수에서 가장 0에 가까운 수 
+          if(iterP->first+iterM->first<0){
+            PrintIter(iterP,tree);
+          }else{
+            PrintIter(iterM,tree);
+          }
+        }
+      }
+    //tmp!=0 
+    }else{
+      map<int,int>::iterator iter=tree.find(tmp);
+      if(iter==tree.end()){
+        tree.insert(make_pair(tmp,1));
+      }else{
+        iter->second+=1;
+      }
+    }
+  }
+  
+}
 
 #endif
--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -11,53 +11,43 @@
 
 using namespace std;
 
-void PrintIter(map<int,int>::iterator& iter,map<int,int>& tree){
-  cout<<iter->first<<'\n';
-  if(iter->second==1){
-    tree.erase(iter);
-  }else{
-    iter->second-=1;
-  }
-}
-void BK11286(){
+void BK1655(){
 //input
   int N;
   scanf("%d",&N);
 //prepare  
+  int mid,tmp,gap;
+  multiset<int> leftQ;
+  multiset<int> rightQ;
 //calc
+  //n==1
+  scanf("%d",&mid);
+  printf("%d\n",mid);
   map<int,int> tree;
-  int tmp;
-  for(int i=0;i<N;i++){
+  //n>1
+  for(int i=0;i<N-1;i++){
     scanf("%d",&tmp);
-    if(tmp==0){
-      if(tree.size()==0){
-        printf("%d\n",0);
-      }else{
-        map<int,int>::iterator iterP=tree.lower_bound(0); //양수에서 가장 작은 수
-        if(iterP==tree.begin()){  //양수만 있는 경우
-          PrintIter(iterP,tree);
-        }else if(iterP==tree.end()){  //음수만 있는 경우 
-          PrintIter(--tree.end(),tree);
-        }else{
-          map<int,int>::iterator iterM=--tree.lower_bound(0);             //음수에서 가장 0에 가까운 수 
-          if(iterP->first+iterM->first<0){
-            PrintIter(iterP,tree);
-          }else{
-            PrintIter(iterM,tree);
-          }
-        }
+    if(tmp>mid){
+      rightQ.insert(tmp);
+      gap=rightQ.size()-leftQ.size();
+      while(gap>1){
+        leftQ.insert(mid);
+        mid=*rightQ.begin();
+        rightQ.erase(rightQ.begin());
+        gap--;
       }
-    //tmp!=0 
     }else{
-      map<int,int>::iterator iter=tree.find(tmp);
-      if(iter==tree.end()){
-        tree.insert(make_pair(tmp,1));
-      }else{
-        iter->second+=1;
+      leftQ.insert(tmp);
+      gap=leftQ.size()-rightQ.size();
+      while(gap>0){
+        rightQ.insert(mid);
+        mid=*leftQ.rbegin();
+        leftQ.erase(--leftQ.end());
+        gap--;
       }
     }
+    printf("%d\n",mid);
   }
-  
 }
 int main(){
   //cin.tie(NULL);
@@ -67,7 +57,7 @@ int main(){
   //변수
   //계산
   //출력
-  BK11286();
+  BK1655();
   
   return 0;
 }
