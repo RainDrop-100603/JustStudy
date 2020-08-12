3be7c3194953bace6467f5b3ba8380afd51c31e3
rain <parkkwang2@naver.com>
2020-08-12 10:29:42 +0900

    tmp

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -14,19 +14,21 @@ void BK11066(){
 //input
   int N;
   scanf("%d",&N);
-  vector<vector<int>> table(N,vector<int>(N));  //A(ij)==(i+1)에서 시작하는 (j+1)길이의 값
+  vector<int> arr(N); //input value
   for(int i=0;i<N;i++){
-    scanf("%d",&table[i][0]);
+    scanf("%d",&arr[i]);
   }
 //prepare
+  vector<vector<int>> table(N,vector<int>(N));  //A(ij)==[i,i+j+1) 범위의 value를 만드는데 필요한 cost의 최소값
   int prevMin,newMin;
 //calc
-  //A(ij)=min(A(ik)~A(i+k,j-k))
+  //A(ij)=min(A(ik)+A(i+k+1,j-k-1)+sum(ij)),k=0~j-1,sum(ij)=sum(arr[i]~arr[j])
   for(int j=1;j<N;j++){
-    for(int i=0;i<N-j+1;i++){
+    for(int i=0;i<N-j;i++){
       prevMin=INT32_MAX;
-      //k=1, k= j-1일때는 추가작업이 없지만, 이외에는 이전 작업을 더해줘여 한다. ex 30 40 40이면 (30+40) + ((30+40)+40) 이런식이다.
-      for(int k=0;k<j;k++){
+      //k=0, k= j-1일때는 추가작업이 없지만, 이외에는 이전 작업을 더해줘여 한다. ex 30 40 40이면 (30+40) + ((30+40)+40) 이런식이다. 만드는데 든 비용 !=길이
+      prevMin=min(prevMin,table[i][0]+table[i+1][j-1]);
+      for(int k=1;k<j-1;k++){
         prevMin=min(prevMin,table[i][k]+table[i+k][j-k]);
       }
       table[i][j]=prevMin; 
@@ -40,7 +42,7 @@ void BK11066(){
     }
     printf("\n");
   }
-  //printf("%d\n",table[1][N]);
+  //printf("%d\n",table[0][N-1]);
 }
 
 int main(){
