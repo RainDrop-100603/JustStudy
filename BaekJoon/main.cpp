75102ae4f5e19b1cc8b00852f5c655447be0a8aa
rain <parkkwang2@naver.com>
2020-08-12 13:13:47 +0900

    BK11066

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -20,29 +20,31 @@ void BK11066(){
   }
 //prepare
   vector<vector<int>> table(N,vector<int>(N));  //A(ij)==[i,i+j+1) 범위의 value를 만드는데 필요한 cost의 최소값
-  int prevMin,newMin;
+  int prevMin,newMin,tmpSum;
 //calc
-  //A(ij)=min(A(ik)+A(i+k+1,j-k-1)+sum(ij)),k=0~j-1,sum(ij)=sum(arr[i]~arr[j])
+  //A(ij)=min(A(ik)+A(i+k+1,j-k-1))+sum(ij),k=0~j-1,sum(ij)=sum(arr[i]~arr[j])
   for(int j=1;j<N;j++){
     for(int i=0;i<N-j;i++){
       prevMin=INT32_MAX;
-      //k=0, k= j-1일때는 추가작업이 없지만, 이외에는 이전 작업을 더해줘여 한다. ex 30 40 40이면 (30+40) + ((30+40)+40) 이런식이다. 만드는데 든 비용 !=길이
-      prevMin=min(prevMin,table[i][0]+table[i+1][j-1]);
-      for(int k=1;k<j-1;k++){
-        prevMin=min(prevMin,table[i][k]+table[i+k][j-k]);
+      tmpSum=0;
+      for(int k=i;k<=i+j;k++){
+        tmpSum+=arr[k];
       }
-      table[i][j]=prevMin; 
+      for(int k=0;k<j;k++){
+        prevMin=min(prevMin,table[i][k]+table[i+k+1][j-k-1]);
+      }
+      table[i][j]=prevMin+tmpSum; 
     }
   }
 //output
-  printf("\n\n");
-  for(auto& ele:table){
-    for(auto& ele2:ele){
-      printf("%d ",ele2);
-    }
-    printf("\n");
-  }
-  //printf("%d\n",table[0][N-1]);
+//   printf("\n\n");
+//   for(auto& ele:table){
+//     for(auto& ele2:ele){
+//       printf("%d ",ele2);
+//     }
+//     printf("\n");
+//   }
+  printf("%d\n",table[0][N-1]);
 }
 
 int main(){
