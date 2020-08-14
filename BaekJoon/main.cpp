f6fffc89044f21fae8d90601fd6ac3b622e29af2
rain <parkkwang2@naver.com>
2020-08-14 15:20:31 +0900

    tmp

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -14,67 +14,58 @@ void BK7579(){
 //input
   int N,M;
   scanf("%d %d",&N,&M); //N== # program, M==need size;
-  vector<pair<int,int>> cost(N);  //first=rerun cost, second=getMem size
-  for(auto& ele:cost){
+  vector<pair<int,int>> arr(N);  //first=rerun cost, second=getMem size
+  for(auto& ele:arr){
     scanf("%d",&ele.second);
   }
-  for(auto& ele:cost){
+  for(auto& ele:arr){
     scanf("%d",&ele.first);
   }
 //prepare
-  sort(cost.begin(),cost.end());
+  sort(arr.begin(),arr.end());
   map<int,int> costMap1;  //map[i]= i번째 원소까지 input한 tree
   map<int,int> costMap2;  //key : cost의 값, //value : 해제되는 memory 값
-  costMap1.insert(0,0);
-  costMap2.insert(0,0);
+  costMap1.insert(make_pair(0,0));
+  costMap2.insert({0,0});
   bool first; //first면 costMap1==Map[i], costMap2==Map[i-1]
   int ans;
   int cost,memSize;
 //calc
   //map 정렬
   for(int i=0;i<N;i++){
-    map<int,int>::iterator iter;
-    map<int,int>::iterator prevIter;
-    map<int,int>::iterator nowIter;
-    if(first){
-      for(iter=costMap2.begin();iter!=costMap2.end();iter++){
-        cost=iter->first+cost[i].first;
-        memSize=iter->second+cost[i].second;
-        prevIter=costMap2.find(cost);
-        if(prevIter==costMap2.end()){
-          costMap1.insert(cost,memSize);
-        }else{
-          if(memSize>prevIter->second){
-            nowIter=costMap1.find(cost);
-            if(nowIter==costMap1.end()){
-              costMap1.insert(cost,memSize);
-            }else{
-              nowIter->second=memSize;
+    auto nowMap=&costMap1;
+    auto prevMap=&costMap2;
+    if(!first){
+        auto* tmp=nowMap;
+        nowMap=prevMap;
+        prevMap=tmp;
+    }
+    for(auto iter=prevMap->begin();iter!=prevMap->end();iter++){
+        cost=iter->first+arr[i].first;
+        memSize=iter->second+arr[i].second;
+        auto prevIter=prevMap->find(cost);
+        if(prevIter!=prevMap->end()){
+            if(prevIter->second>memSize){
+                memSize=prevIter->second;
             }
-          }
         }
-      }
-    }else{
-      // for(iter=costMap1.begin();iter!=costMap1.end();iter++){
-      //   cost=iter->first+cost[i].first;
-      //   memSize=iter->second+cost[i].second;
-      //   prevIter=costMap1.find(cost);
-      //   if(prevIter==costMap1.end()){
-      //     costMap2.insert(cost,memSize);
-      //   }else{
-      //     if(memSize>prevIter->second){
-      //       nowIter=costMap2.find(cost);
-      //       if(nowIter==costMap1.end()){
-      //         costMap2.insert(cost,memSize);
-      //       }else{
-      //         nowIter->second=memSize;
-      //       }
-      //     }
-      //   }
-      // }
+        auto ret=nowMap->insert({cost,memSize});   //ret.first==해당하는 위치의 iter, ret.second==성공여부
+        if(!ret.second){
+            ret.first->second=memSize;
+        }
     }
   }
-  //
+  // value 검색
+  auto nowMap=&costMap2;
+  if(!first){
+    nowMap=&costMap2;
+  }
+  for(auto& ele: *nowMap){
+    if(ele.second>=M){
+        ans=ele.first;
+        break;
+    }
+  }
 //output
   // for(auto& ele:movTable){
   //   for(auto& ele2:ele){
