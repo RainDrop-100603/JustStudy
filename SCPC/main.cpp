2ddaf985e93e99ad2673a1789a9d248e0d0ba969
Rain <parkkwang2@naver.com>
2020-08-21 20:58:38 +0900

    tmp

--- SCPC/main.cpp
+++ SCPC/main.cpp
@@ -25,9 +25,9 @@ using namespace std;
 */
 
 int Answer;
-pair<int,int> line[2000]; //줄의 시작과 끝 점
-int table1[1500][2000]; //A(ji)=i에서 j개 줄을 타서 도착하는 위치
-int table2[1500][2000]; //B(ji)=i에서 j번째 줄부터 타서 도착하는 위치
+pair<int,int> line[2000]; //라인
+int table1[2001][1501]; //A(ji)=i에서 j개 라인을 타서 도착하는 위치     /i=0 : 아무 라인도 안탐
+int table2[2001][1501]; //B(ji)=i에서 j번째 라인부터 타서 도착하는 위치/ i=K: 아무 라인도 안탐
 
 
 int main(int argc, char** argv)
@@ -40,7 +40,6 @@ int main(int argc, char** argv)
 	for(test_case = 0; test_case  < T; test_case++)
 	{
 		Answer = 0;
-		/////////////////////////////////////////////////////////////////////////////////////////////
     //input
     int K,N,m,from,to;
     cin>>N>>K>>m;  //N:세로선 , K: 가로이음줄-line
@@ -50,41 +49,72 @@ int main(int argc, char** argv)
     }
     //prepare
       //table1
-    for(int i=0;i<N;i++){
+    for(int i=1;i<N+1;i++){
       table1[0][i]=i;
     }
-    table1[0][line[0].first]=line[0].second;  //first : from, second: to
-    for(int i=1;i<K;i++){
-      from=line[i].first;
-      to=line[i].second;
-      for(int j=0;j<N;j++){
+    for(int i=1;i<K+1;i++){
+      from=line[i-1].first;
+      to=line[i-1].second;
+      for(int j=1;j<N+1;j++){
         if(table1[i-1][j]==from){
           table1[i][j]=to;
+        }else if(table1[i-1][j]==to){
+          table1[i][j]=from;
         }else{
           table1[i][j]=table1[i-1][j];
         }
       }
     }
       //table2
-    for(int i=0;i<N;i++){
-      table2[K-1][i]=i;
+    for(int i=1;i<N+1;i++){
+      table2[K][i]=i;
     }
-    table2[K-1][line[K-1].first]=line[K-1].second;
-    for(int i=K-2;i>=0;i--){
+    for(int i=K-1;i>=0;i--){
       from=line[i].first;
       to=line[i].second;
-      for(int j=0;j<N;j++){
+      for(int j=1;j<N+1;j++){
         table2[i][j]=table2[i+1][j];
       }
       table2[i][from]=table2[i+1][to];
+      table2[i][to]=table2[i+1][from];
     }
     //calculate
-    
-		/////////////////////////////////////////////////////////////////////////////////////////////
+    for(int i=0;i<m;i++){
+      cin>>from>>to;
+      int minAns=K+1;
+      for(int j=0;j<K+1;j++){
+        int from2=table1[j][from];
+        int cnt(0);
+        while(j+cnt<=K){
+          if(table2[j+cnt][from2]==to){
+            minAns=min(minAns,cnt);
+            break;
+            }
+          cnt++;
+        }
+      }
+      if(minAns==K+1){
+        minAns=-1;
+      }
+      Answer+=minAns;
+    }
 		
+    // for(int i=0;i<K+1;i++){
+    //   for(int j=1;j<N+1;j++){
+    //     cout<<table1[i][j]<<" ";
+    //   }
+    //   cout<<"\n";
+    // }
+    // for(int i=0;i<K+1;i++){
+    //   for(int j=1;j<N+1;j++){
+    //     cout<<table2[i][j]<<" ";
+    //   }
+    //   cout<<"\n";
+    // }
+
 		// Print the answer to standard output(screen).
 		cout << "Case #" << test_case+1 << "\n";
-		cout << Answer << " " << (N+1)*(N+1)-Answer << endl;
+		cout << Answer << endl;
 	}
 
 	return 0;//Your program should return 0 on normal termination.
