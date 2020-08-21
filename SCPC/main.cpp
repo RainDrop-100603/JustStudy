85b1e1f7bb235ba8fe0c0d6d07944ba95ad5bb01
Rain <parkkwang2@naver.com>
2020-08-21 20:59:26 +0900

    Question__tmpFinal

--- SCPC/main.cpp
+++ SCPC/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
 	int T, test_case;
 	cin >> T;
 
-	for(test_case = 0; test_case  < T; test_case++)
+	for(test_case = 0; test_case < T; test_case++)
 	{
 		Answer = 0;
     //input
