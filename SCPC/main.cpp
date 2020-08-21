fa4e95f02abebe16b7724285a267c318c4c17bfe
Rain <parkkwang2@naver.com>
2020-08-22 00:40:20 +0900

    Q3_graph

--- SCPC/main.cpp
+++ SCPC/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 
 using namespace std;
 
@@ -26,8 +27,9 @@ using namespace std;
 
 int Answer;
 
-pair<int,int> graph[1501][2000];  //graph[0]은 버린다.
-int graphidx[1501];   //각 node의 원소가 몇 개 있는지 알 수 있다.
+bool comp(const pair<int,int>& p1,const pair<int,int>& p2){
+  return p1.first<p2.first;
+}
 
 int main(int argc, char** argv)
 {
@@ -42,59 +44,49 @@ int main(int argc, char** argv)
     //input
     int K,N,m,from,to;
     cin>>N>>K>>m;  //N:세로선 , K: 가로이음줄-line
-    for(int i=0;i<K;i++){
-      graph[from][graphidx[from]++];
-    }
-    //prepare
-      //table1
+    vector<vector<pair<int,int>>> graph(N+1,vector<pair<int,int>>());
     for(int i=1;i<N+1;i++){
-      table1[0][i]=i;
+      graph[i].push_back({0,i});
     }
     for(int i=1;i<K+1;i++){
-      from=line[i-1].first;
-      to=line[i-1].second;
-      for(int j=1;j<N+1;j++){
-        if(table1[i-1][j]==from){
-          table1[i][j]=to;
-        }else if(table1[i-1][j]==to){
-          table1[i][j]=from;
-        }else{
-          table1[i][j]=table1[i-1][j];
-        }
-      }
+      cin>>from>>to;
+      graph[from].push_back(make_pair(i,to)); //first=depth(ith line)
+      graph[to].push_back(make_pair(i,from));
     }
-      //table2
-    for(int i=1;i<N+1;i++){
-      table2[K][i]=i;
-    }
-    for(int i=K-1;i>=0;i--){
-      from=line[i].first;
-      to=line[i].second;
-      for(int j=1;j<N+1;j++){
-        table2[i][j]=table2[i+1][j];
-      }
-      table2[i][from]=table2[i+1][to];
-      table2[i][to]=table2[i+1][from];
-    }
+    //prepare
+      
     //calculate
     for(int i=0;i<m;i++){
       cin>>from>>to;
-      int minAns=K+1;
-      for(int j=0;j<K+1;j++){
-        int from2=table1[j][from];
-        int cnt(0);
-        while(j+cnt<=K){
-          if(table2[j+cnt][from2]==to){
-            minAns=min(minAns,cnt);
-            break;
+      int maxdepth(-1);
+      int cnt(0);
+      pair<int,int> tmpP;
+      deque<pair<int,int>> dq;
+      dq.push_back({-1,from});  //0~K-1 이므로 항상 -1보다 크다
+      //BFS
+      //15개에서 13개가 걸렸다는것이 두개를 뺐다는 의미는 아니다. 1개만 빼서 그렇게 만들 수 있다.
+      while(!dq.empty()){
+        int sz=dq.size(); //get depth of BFS
+        for(int i=0;i<sz;i++){
+          tmpP=dq.front();
+          dq.pop_front();
+          auto iter=upper_bound(graph[tmpP.second].begin(),graph[tmpP.second].end(),make_pair(tmpP.first,-1),comp);
+          auto iterEnd=graph[tmpP.second].end();
+          for(iter;iter!=iterEnd;iter++){
+            dq.push_back(*iter);
+            if(iter->second==to){
+              maxdepth=cnt;
             }
-          cnt++;
+          }
         }
+        cnt++;
       }
-      if(minAns==K+1){
-        minAns=-1;
+      if(maxdepth==-1){
+        Answer--;
+      }else{
+        Answer+=K-maxdepth+cnt;
       }
-      Answer+=minAns;
+      
     }
 
 		// Print the answer to standard output(screen).
