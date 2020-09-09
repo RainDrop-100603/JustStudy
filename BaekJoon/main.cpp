f25c609030ee4414ee290255ea6c63e7af64baf6
rain <parkkwang2@naver.com>
2020-09-09 16:25:23 +0900

    BK1956

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -14,46 +14,6 @@
 
 using namespace std;
 
-// void FloydWarshall(const vector<map<int,int>>& graph, vector<vector<int>>& cost_V,vector<vector<int>>& prev_V){ //음수 가중치도 허용한다, DP 이용
-//   /*
-//   Floyd-Warshall Algorithm
-//     DP table 두개를 이용하는 Algorithm
-//       1. Vertex간 최단거리를 저장하는 table A
-//       2. 최단거리일 때, 도착 Vertex의 직전 Vertex를 저장하는 table B
-//       # table의 갱신: 경유하는 vertex가 0개 ~ #vertex 개 일때까지 차례로 DP
-//         ==모든 vertex를 경유 가능하도록 추가할 때까지
-//     A(i,j)=i에서 j로 가는 최단거리 cost
-//     B(i,j)=i에서 j로 가는 최단거리일때, j의 직전 vertex
-//     DP(x)->DP(x+1)에서 vertex k가 경유지로 추가될 경우, Edge(K,dest)
-//       A(i,dest)=min(A(i,dest),A(i,k)+Edge(k,dest))
-//       B(i,dest)=k, iff A(i,dest) is changed
-//   */
-//   int num_V(cost_V.size()),INF(INT32_MAX),NIL(-1);  //there is no "-1" vertex, so NIL==-1 
-//   //initialize
-//   for(int i=0;i<num_V;i++){ 
-//     for(int j=0;j<num_V;j++){ //default value
-//       cost_V[i][j]=INF;
-//       prev_V[i][j]=NIL;
-//     }
-//     cost_V[i][i]=0; //vertex i to i == 0
-//     for(auto& ele: graph[i]){ //default Edge,first==dest, second==cost
-//       cost_V[i][ele.first]=ele.second;
-//       prev_V[i][ele.first]=i;
-//     }
-//   }
-//   //Dynamic Programing
-//   for(int mid=0;mid<num_V;mid++){
-//     for(int dest=0;dest<num_V;dest++){
-//       for(int from=0;from<num_V;from++){
-//         if(cost_V[mid][dest]!=INF&&cost_V[from][mid]!=INF&&cost_V[from][mid]+cost_V[mid][dest]<cost_V[from][dest]){
-//            cost_V[from][dest]=cost_V[from][mid]+cost_V[mid][dest];
-//            prev_V[from][dest]=mid;
-//          }
-//       }
-//     }
-//   }
-// }
-
 void BK1956(){  // Dijkstra
   /*
   문제조건
@@ -67,9 +27,6 @@ void BK1956(){  // Dijkstra
     FloydWarshall : 쓸만한듯
       vertex간 거리를 저장하는 table에서, A(ij)+A(ji): j=1~#edge == i로 돌아오는 모든 사이클 
       -> 1로 돌아오는 사이클만 보는것이 아님을 고려해야한다.
-
-    
-
   */
 //input
   int V,E,a,b,c;
@@ -89,10 +46,14 @@ void BK1956(){  // Dijkstra
   FloydWarshall(graph,cost_V,prev_V);
 //output
   int result=INT32_MAX;
-  for(int i=2;i<V;i++){
-    result=min(result,cost_V[1][i]+cost_V[i][1]);
+  for(int i=1;i<=V;i++){
+    for(int j=i+1;j<=V;j++){
+      if(cost_V[i][j]!=INT32_MAX&&cost_V[j][i]!=INT32_MAX){
+        result=min(result,cost_V[i][j]+cost_V[j][i]);
+      }
+    }
   }
-  if(result<0||result==INT32_MAX){ //if there is one INF, sum is INF or minus
+  if(result==INT32_MAX){ //if there is one INF, sum is INF or minus
     cout<<-1;
   }else{
     cout<<result;
