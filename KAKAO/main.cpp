119bb7b554839d94ce1eafae7d1cfaf75fe9a1ef
Rain <parkkwang2@naver.com>
2020-09-12 17:17:30 +0900

    tmp

--- KAKAO/main.cpp
+++ KAKAO/main.cpp
@@ -1,40 +1,66 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-vector<string> solution(vector<string> orders, vector<int> course) {
-  vector<string> answer;
-  //prepare
-  int N=orders.size();  // 고객의 수
-  vector<int> bitMask(N); // 주문을 비트마스크로 변경 A-Z
+vector<int> solution(vector<string> info, vector<string> query) {
+  vector<int> answer;
+  int N=info.size();
+  vector<vector<int>> table(N,vector<int>(5));  //bitmask save
   for(int i=0;i<N;i++){
-    for(auto& ele:orders[i]){
-      bitMask[i]+=(1<<(ele-32));
+    string& tmpS=info[i];
+    int tmp;
+    //언어
+    switch(tmpS[0]){
+      case 'c': tmp=1; break;
+      case 'j': tmp=2; break;
+      case 'p': tmp=4; break;
+      default: tmp=7; break;
     }
-  }
-  char M(0);
-  for(auto& ele:orders){
-    M=max(M,ele.back());  //알파벳중 가장 큰 값을 찾는다.
-  }
-  M-=32;  //A=0, Z=25
-  vector<vector<bool>> bitTable(N,vector<bool>(1<<M));  // 다 비교해버린 table
-  for(int i=0;i<N;i++){
-    for(int j=0;j<(1<<M);j++){
-      if((bitMask[i]&j)==j){
-        bitTable[i][j]=true;
-      }
+    table[i][0]=tmp;
+    //직군
+    int idx(0);
+    idx=tmpS.find(' ',idx);
+    idx++;
+    switch(tmpS[idx]){
+      case 'b': tmp=1; break;
+      case 'f': tmp=2; break;
+      default: tmp=3; break;
     }
-  }
-  //calc
-  vector<int> tmpV;
-  for(int i=0;i<N;i++){
-    for(int j=0;j<(1<<M);j++){
-      if((bitMask[i]&j)==j){
-        bitTable[i][j]=true;
-      }
+    table[i][1]=tmp;
+    //경력
+    idx=tmpS.find(' ',idx);
+    idx++;
+    switch(tmpS[idx]){
+      case 'j': tmp=1; break;
+      case 's': tmp=2; break;
+      default: tmp=3; break;
     }
+    table[i][2]=tmp;
+    //소울푸드
+    idx=tmpS.find(' ',idx);
+    idx++;
+    switch(tmpS[idx]){
+      case 'c': tmp=1; break;
+      case 'p': tmp=2; break;
+      default: tmp=3; break;
+    }
+    table[i][3]=tmp;
+    //
+    idx=tmpS.find(' ',idx);
+    idx++;
+    table[i][4]=stoi(tmpS.substr(idx));
   }
-
+  
   return answer;
+}
+
+int main(){
+
+  string s1="java backend junior pizza 150";
+  string s2="python frontend senior chicken 210";
+  string s3="python frontend senior chicken 150";
+  cout<<s1[s1.find(' ',5)+1]<<endl;
+  return 0;
 }
