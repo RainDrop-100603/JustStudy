29215d2c0a6ef8b1f3f37b2660cd6ca9d57efa83
rain <parkkwang2@naver.com>
2020-09-23 17:56:33 +0900

    tmp

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -5,118 +5,37 @@
 #include <vector>
 
 using namespace std;
-void BoardCoverInput(vector<vector<bool>>& boardTable){
-  int height,width;
-  cin>>height>>width;
-  string tmpS;
-  for(int i=0;i<height;i++){
-    boardTable.push_back(vector<bool>(width));
-    cin>>tmpS;
-    for(int j=0;j<width;j++){
-      if(tmpS[j]=='#'){
-        boardTable[i][j]=false;
-      }else{
-        boardTable[i][j]=true;
-      }
-    }
+void ClockSyncInput(vector<int>& clockArr){
+  for(int i=0;i<16;i++){
+    cin>>clockArr[i];
   }
 }
-bool BoradCoverNoAns(vector<vector<bool>>& boardTable){
-  int cnt(0);
-  for(auto& ele:boardTable){
-    for(auto&& ele2:ele){
-      if(ele2){
-        cnt++;
-      }
-    }
-  }
-  if(cnt%3!=0){
-    return true;
-  }else{
-    return false;
-  }
-}
-int BoardCoverAlgo(vector<vector<bool>>& boardTable){
+int ClockSyncAlgo(vector<int>& clockArr,vector<vector<int>>& switchClock){
   /*
-  제한시간 2초
+  제한시간 10초
   제한메모리 2^16kb=64MB
   전략1
     brute force
-      왼쪽 위부터 오른쪽 아래로 가면서 chk 가능, 모양은 4가지 ㄴ, ㄱ, Symmetric ㄴ, Symmetric ㄱ
-        중복은 자연히 걸러짐
-        타일을 올리면 false로 바꾼다.
-        (x,y)기준, (x,y)&(x,y+1)&(x+1,y+1), (x,y)&(x,y+1)&(x-1,y+1), (x,y)&(x+1,y)&(x+1,y+1), (x,y)&(x,y+1)&(x+1,y)
-      first position을 찾는다
-        기저: 찾지 못하면 first==-1, return 1;
-        first 포지션의 4방향을 search
-          4모양 모두 불가능 하면 return 0;
-          가능하다면 재귀
     time complexity
-      4^(width*height/3)<=10^60 //하얀색이 50칸 이하라는 조건이 존재한다.
-      white<50 -> 4^13=6*10^7
     mem complexity
-      width*height<=400
-   
   */
-  //first position
-  int height=boardTable.size();
-  int width=boardTable[0].size();
-  int yPos=-1,xPos=-1;
-  for(int i=0;i<height;i++){
-    for(int j=0;j<width;j++){
-      if(boardTable[i][j]){
-        xPos=j;
-        yPos=i;
-        i=height;
-        break;
-      }
-    }
-  }
   //기저
-  if(yPos==-1){
-    return 1;
-  }
   //Algo
-  int result(0);
-  bool xp1(xPos+1<width),xm1(xPos>0),yp1(yPos+1<height);
-  if(xp1&&yp1){
-    if(boardTable[yPos][xPos+1]&&boardTable[yPos+1][xPos+1]){
-      boardTable[yPos][xPos]=boardTable[yPos][xPos+1]=boardTable[yPos+1][xPos+1]=false;
-      result+=BoardCoverAlgo(boardTable);
-      boardTable[yPos][xPos]=boardTable[yPos][xPos+1]=boardTable[yPos+1][xPos+1]=true;
-    }
-    if(boardTable[yPos+1][xPos]&&boardTable[yPos+1][xPos+1]){
-      boardTable[yPos][xPos]=boardTable[yPos+1][xPos]=boardTable[yPos+1][xPos+1]=false;
-      result+=BoardCoverAlgo(boardTable);
-      boardTable[yPos][xPos]=boardTable[yPos+1][xPos]=boardTable[yPos+1][xPos+1]=true;
-    }
-    if(boardTable[yPos+1][xPos]&&boardTable[yPos][xPos+1]){
-      boardTable[yPos][xPos]=boardTable[yPos+1][xPos]=boardTable[yPos][xPos+1]=false;
-      result+=BoardCoverAlgo(boardTable);
-      boardTable[yPos][xPos]=boardTable[yPos+1][xPos]=boardTable[yPos][xPos+1]=true;
-    }
-  }
-  if(xm1&&yp1){
-    if(boardTable[yPos+1][xPos-1]&&boardTable[yPos+1][xPos]){
-      boardTable[yPos][xPos]=boardTable[yPos+1][xPos-1]=boardTable[yPos+1][xPos]=false;
-      result+=BoardCoverAlgo(boardTable);
-      boardTable[yPos][xPos]=boardTable[yPos+1][xPos-1]=boardTable[yPos+1][xPos]=true;
-    }
-  }
+  int result(-1);
   return result;
 }
-void BoardCover(){
+void ClockSync(){
   int testCase;
   cin>>testCase;
+  vector<vector<int>> switchClock={
+    {0,1,2},{3,7,9,11},{4,10,14,15},{0,4,5,6,7},
+    {6,7,8,10,12},{0,2,14,15},{3,14,15},
+    {4,5,7,14,15},{1,2,3,4,5},{3,4,5,9,13}
+  };
   while(testCase--){
-    //true=white, false=black;
-    vector<vector<bool>> boardTable;
-    BoardCoverInput(boardTable);
-    if(BoradCoverNoAns(boardTable)){
-      cout<<"0\n";
-    }else{
-      cout<<BoardCoverAlgo(boardTable)<<"\n";
-    }
+    vector<int> clockArr(16);
+    ClockSyncInput(clockArr);
+    cout<<ClockSyncAlgo(clockArr,switchClock)<<"\n";
   }
 }
 
@@ -124,7 +43,7 @@ int main(void){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
   
-  BoardCover();
+  ClockSync();
 
   return 0;
 }
