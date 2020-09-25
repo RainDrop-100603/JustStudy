48d0d0fc5e4bfc5b11af314db403f5e5e44ecacb
rain <parkkwang2@naver.com>
2020-09-25 17:57:24 +0900

    tmp

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -5,46 +5,72 @@
 #include <vector>
 
 using namespace std;
-void QuadTreeInput(string& treeData){
-  cin>>treeData;
+void FenceInput(vector<int>& fenceData){
+  int fenceNum,tmp;
+  cin>>fenceNum;
+  fenceData.reserve(fenceNum);
+  for(int i=0;i<fenceNum;i++){
+    cin>>tmp;
+    fenceData.push_back(tmp);
+  }
 }
-string QuadTreeAlgo(string::iterator& iter){
+int FenceAlgo(vector<int>& fenceData,int left, int right){
   /*
   제한시간 1초
   제한메모리 2^16kb=64MB
   전략1
-    기저: b or w를 만났을 때, 그대로 반환
-    재귀: x를 만났을 때, 이때 상하반전 이므로 순서를 바꿔야 한다. 이때 반환되어 오는 값을 result에 더한다 
-      1234 순서가 3412 순서로
-    반환: string 값을 합쳐서 반환
+    기저: left==right일 때, max=leftmax=rightmax=fenceData[left];
+    재귀: L(left,mid), R(mid+1,right) 재귀
+    반환: max, leftmax, right max
+      leftmax= L's leftmax
+      rightmax= R's right max
+      newMax=if(fenceData[mid]<fenceData[mid+1])
+        L's rightmax + R's leftmax*fenceData[mid]/fenceData[mid+1]
+        else: vice versa
+      max=max(newMax, L's max, R's max);
     time complexity
-      O(N): N 번 호출, 총 길이 N 만큼 문자열 합
+      O(NlgN): lgN 번 호출, O(n) 비교
     mem complexity
       O(N)
-    업데이트
-      iterator를 이용하여 전달인자 간소화
+  전략2
+    앞에서부터 뒤로 search
+      key, prev, now 존재
+        key 왼쪽으로는 모두 key보다 크거나 같다
+        prev는 now 직전
+          now가 prev보다 작다면,
+    stack을 이용하는 건데 헷갈려
+  전략 3
+    quick sort 전략을 이용한다.
+      가장 작은 pivot을 구한다
+        분할: (left,pivot-1), (pivot+1,right)로 분리
+        반환: result= max(pivot*(right-left),func(left,pivot-1),func(pivot+1,right));
+        기저: right=left -> return fenceData[left];
+      총 lgN번 구한다
+      time complexity: NlgN
+      mem complexity: N
   */
-  //기저, b and w
-  char head=*iter;
-  iter++;
-  if(head=='b'||head=='w'){
-    return string(1,head);
+  //기저, left==right일 때, max=leftmax=rightmax=fenceData[left];
+  if(right==left){
+    return fenceData[left];
   }
-  //Algo, x인 경우
-  string tmp[4];
-  for(int i=0;i<4;i++){
-    tmp[i]=QuadTreeAlgo(iter);
+  //Algo, 
+  int pivot,minValue(12345);
+  for(int i=left;i<=right;i++){
+    if(fenceData[i]<minValue){
+      minValue=fenceData[i];
+      pivot=i;
+    }
   }
-  return string("x")+tmp[2]+tmp[3]+tmp[0]+tmp[1];
+  int leftMax=FenceAlgo(fenceData,left,pivot-1);
+  return result;
 }
-void QuadTree(){
+void Fence(){
   int testCase;
   cin>>testCase;
   while(testCase--){
-    string treeData;
-    QuadTreeInput(treeData);
-    string::iterator iter=treeData.begin();
-    cout<<QuadTreeAlgo(iter)<<"\n";
+    vector<int> fenceData;
+    FenceInput(fenceData);
+    cout<<FenceAlgo(fenceData)<<"\n";
   }
 }
 
@@ -52,7 +78,7 @@ int main(void){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
   
-  QuadTree();
+  Fence();
 
   return 0;
 }
