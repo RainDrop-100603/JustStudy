ff7bb8fb7905e40ce0517dd6592ed4108fa359a0
rain <parkkwang2@naver.com>
2020-10-05 17:40:12 +0900

    solChk:prevTmp is my Sol

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -6,6 +6,93 @@
 
 using namespace std;
 
+//O(n^2) 곱셈 알고리즘
+vector<int> multiply(const vector<int> &a, const vector<int> &b){
+    vector<int> c(a.size() + b.size() + 1, 0);
+    for (int i = 0; i < a.size(); i++)
+        for (int j = 0; j < b.size(); j++)
+            c[i + j] += (a[i] * b[j]);
+    return c;
+}
+
+//a += b*(10^k)
+void addTo(vector<int> &a, const vector<int> &b, int k){
+    a.resize(max(a.size(), b.size() + k));
+    for (int i = 0; i < b.size(); i++)
+        a[i + k] += b[i];
+}
+
+//a -= b
+void subFrom(vector<int> &a, const vector<int> &b){
+    a.resize(max(a.size(), b.size()) + 1);
+    for (int i = 0; i < b.size(); i++)
+        a[i] -= b[i];
+}
+
+vector<int> karatsuba(const vector<int> &a, const vector<int> &b){
+    int an = a.size(), bn = b.size();
+    if (an < bn)
+        return karatsuba(b, a);
+    if (an == 0 || bn == 0)
+        return vector<int>();
+    //크기가 작은경우 카라츠바 알고리즘을 사용하지 않고 구한다.
+    if (an <= 50)
+        return multiply(a, b);
+
+    /*카라츠바 알고리즘
+    ∴ z0 + ( z1 * 10^half ) + ( z2 * 10^(half*2) )
+        z0 = a0 * b0
+        z2 = a1 * b1
+        z1 = (a0 + b1) * (b0 + b1) - z0 - z2
+        a0 = a 앞부분 절반 b0 = b 앞부분 절반
+        a1 = a 뒷부분 절반 b1 = b 뒷부분 절반
+    */
+
+    //a와 b를 절반으로 나눈다.
+    int half = an / 2;
+    vector<int> a0(a.begin(), a.begin() + half);
+    vector<int> a1(a.begin() + half, a.end());
+    vector<int> b0(b.begin(), b.begin() + min<int>(bn, half));
+    vector<int> b1(b.begin() + min<int>(bn, half), b.end());
+
+    vector<int> z2 = karatsuba(a1, b1);
+    vector<int> z0 = karatsuba(a0, b0);
+
+    addTo(a0, a1, 0);
+    addTo(b0, b1, 0);
+
+    vector<int> z1 = karatsuba(a0, b0);
+    subFrom(z1, z0);
+    subFrom(z1, z2);
+
+    vector<int> res;
+    addTo(res, z0, 0);
+    addTo(res, z1, half);
+    addTo(res, z2, half * 2);
+
+    return res;
+}
+
+int hugs(const string &members, const string &fans){
+    int N = members.size(), M = fans.size();
+    vector<int> A(N), B(M);
+    for (int i = 0; i < N; i++)
+        A[i] = (members[i] == 'M');
+    for (int i = 0; i < M; i++)
+        B[M-i-1] = (fans[i] == 'M');
+
+    vector<int> C = karatsuba(A, B);
+    int allHugs = 0;
+
+    //모든 멤버들이 펜을 만나기 시작한 시점부터
+    for (int i = N - 1; i < M; i++)
+        if (C[i] == 0)
+            allHugs++;
+
+    return allHugs;
+}
+
+
 class longNum{
   //ver0.1, 2020_10_4
   //ele=num[idx]*10^idx, sign: true means positive
@@ -402,13 +489,60 @@ int FanmeetingAlgo(longNum& member, longNum& fan){
   }
   return ans;
 }
+void FanmeetingRand(string& memS,string fanS){
+  long long fanLen=rand()*rand();
+  long long memLen=fanLen%rand();
+  for(int i=0;i<fanLen;i++){
+    switch(rand()%2){
+      case 1:
+        fanS.push_back('M');
+      case 0:
+        fanS.push_back('F');
+    }
+  }
+  for(int i=0;i<memLen;i++){
+    switch(rand()%2){
+      case 1:
+        memS.push_back('M');
+      case 0:
+        memS.push_back('F');
+    }
+  }
+}
+void FanmeeTingRandInput(longNum& member, longNum& fan,string& memberTmp,string& fanTmp){
+  vector<int> memberV,fanV;
+  memberV.reserve(memberTmp.length());
+  for(auto& ele: memberTmp){
+    if(ele=='M'){
+      memberV.push_back(1);
+    }else{
+      memberV.push_back(0);
+    }
+  }
+  fanV.reserve(fanTmp.length());
+  for(auto& ele: fanTmp){
+    if(ele=='M'){
+      fanV.push_back(1);
+    }else{
+      fanV.push_back(0);
+    }
+  }
+  member=move(memberV);
+  fan=move(fanV);
+}
 void Fanmeeting(){
-  int testCase;
-  cin>>testCase;
-  while(testCase--){
+  while(true){
     longNum member,fan;
-    FanmeetingInput(member,fan);
-    cout<<FanmeetingAlgo(member,fan)<<"\n";
+    //FanmeetingInput(member,fan);
+    string memS,fanS;
+    FanmeetingRand(memS,fanS);
+    FanmeeTingRandInput(member,fan,memS,fanS);
+    int Sol(hugs(memS,fanS));
+    int mySol(FanmeetingAlgo(member,fan));
+    if(Sol!=mySol){
+      cout<<member<<endl<<fan<<endl;
+      break;
+    }
   }
 }
 
@@ -418,18 +552,8 @@ int main(void){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
   
-  //Fanmeeting();
+  Fanmeeting();
   //longNumTest();
   //moveTest_vec();
-  longNum tmp("123654653213543516513515126513215313546513465");
-  longNum a(tmp.karatsuba(tmp));
-  longNum b(tmp.operator*(tmp));
-  longNum c(tmp.karatsuba_notNorm(tmp));
-  longNum d(tmp.mult_notNorm(tmp));
-  cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl;
-  c.print_withSpace();cout<<endl;
-  d.print_withSpace();cout<<endl;
-  c.normalize();
-  cout<<c<<endl;
   return 0;
 }
