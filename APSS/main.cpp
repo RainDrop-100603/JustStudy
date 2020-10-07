78bcc61c7695f56f7d5a0add983892fbbfec7e39
rain <parkkwang2@naver.com>
2020-10-07 14:12:02 +0900

    tmp

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -6,15 +6,44 @@
 
 using namespace std;
 
-
-
-
+void WildcardInput(string& wildcard,vector<string>& fileName){
+  int fileNum;
+  string tmp;
+  cin>>wildcard>>fileNum;
+  fileName.reserve(fileNum);
+  for(int i=0;i<fileNum;i++){
+    cin>>tmp;
+    fileName.push_back(move(tmp));
+  }
+}
+int WildcardAlgo(string& wildcard,vector<string>& fileName){
+  /*
+  제한시간 2초
+  제한메모리 2^16kb=64MB
+  전략1
+    Dynamic Programming
+    time complexity
+    mem complexity
+  */
+  int result(0);
+  return result;
+}
+void Wildcard(){
+  int testCase;
+  cin>>testCase;
+  while(testCase--){
+    string wildcard;
+    vector<string> fileName;
+    WildcardInput(wildcard,fileName);
+    cout<<WildcardAlgo(wildcard,fileName)<<"\n";
+  }
+}
 
 int main(void){
   cin.tie(NULL);
   cin.sync_with_stdio(false);
   
-  //Fanmeeting();
+  Wildcard();
   
   return 0;
 }
