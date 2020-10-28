30f8b2a2fa880fd7bde8f2cce5fec583d39aa418
rain <parkkwang2@naver.com>
2020-10-28 17:06:58 +0900

    packing_1

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -20,16 +20,15 @@ void Packing_Input(int& weight,vector<string>& itemName,vector<int>& itemWeight,
   }
 }
 int Packing_DP(vector<vector<int>>& DP_desp,vector<vector<int>>& DP_choice,vector<int>& itemWeight,vector<int>& itemDesp,int prevChoice,int weightRemain){
-  //기저, weightRemain<=0
-  if(weightRemain<0) return -987654321;
-  if(weightRemain==0) return 0;
+  //기저, weightRemain<0
+  if(weightRemain<0) return -123456789;
   int& result=DP_desp[prevChoice][weightRemain];
   if(result!=-1) return result;
   //substructrue
   result=0;
   int& nextPick=DP_choice[prevChoice][weightRemain];
   int tmp;
-  for(int nextChoice=prevChoice+1;nextChoice<DP_desp.size();nextChoice++){
+  for(int nextChoice=prevChoice+1;nextChoice<itemDesp.size();nextChoice++){
     tmp=itemDesp[nextChoice]+Packing_DP(DP_desp,DP_choice,itemWeight,itemDesp,nextChoice,weightRemain-itemWeight[nextChoice]);
     if(tmp>result){
       nextPick=nextChoice;
@@ -43,12 +42,13 @@ vector<int> Packing_Algo(int weight,vector<int>& itemWeight,vector<int>& itemDes
   2초, 64MB, 테스트케이스 50개
   입력:물건의 수(N)1~100, 캐리어의 용량(W)1~1000, N개의 물건을 이름,부피,절박도 순서로 각 줄에 주어짐, 이름:공백없는 알파벳 대소문자 1~20, 부피와절박도는 1~1000
   출력:첫 줄에는 최대 절박도 합과 가져갈 물건들의 개수 출력, 이후 한 줄마다 각 물건들의 이름을 출력, 조합이 여러개일 경우 하나만 출력한다.
+  제한: 절박도 최대=100*1000=100000
   전략1
     Dynamic Programming
       func(직전에 선택한 물건, 남은 캐리어 용량)= 절박도의 합
         DP: size 100*1000, 물건은 순서대로만 선택 가능
         substructure: func(prevChoice,weightRemain)=for(i=prevChoice+1~N), max, func(i,weightRemain-i_weight)
-        기저: weightRemain<1, prevChoice=N
+        기저: weightRemain<0
         정답: 경로를 저장한 DP를 이용하여 출력
     time complexity
       #func(NW)*func(N)+=O(N^2*W)
@@ -61,36 +61,16 @@ vector<int> Packing_Algo(int weight,vector<int>& itemWeight,vector<int>& itemDes
   vector<vector<int>> DP_choice=DP_desp;
   //DP 채우기
   int tmp=Packing_DP(DP_desp,DP_choice,itemWeight,itemDesp,0,weight);
-  // //디버깅
-  // cout<<"------------------------------------\n";
-  // for(auto& ele:DP_desp){
-  //   for(auto& ele2:ele){
-  //     cout<<ele2<<" ";
-  //   }
-  //   cout<<'\n';
-  // }
-  // cout<<"------------------------------------\n";
-  // cout<<"------------------------------------\n";
-  // for(auto& ele:DP_choice){
-  //   for(auto& ele2:ele){
-  //     cout<<ele2<<" ";
-  //   }
-  //   cout<<'\n';
-  // }
-  // cout<<"------------------------------------\n";
   //정답 생성
   vector<int> result;
   result.push_back(tmp);
-  int nextPick=DP_choice[0][weight];
+  int weightRemain=weight;
+  int nextPick=DP_choice[0][weightRemain];
   while(nextPick!=-1){
     result.push_back(nextPick);
-    nextPick=DP_choice[nextPick][weight-itemWeight[nextPick]];
+    weightRemain-=itemWeight[nextPick];
+    nextPick=DP_choice[nextPick][weightRemain];
   }
-  cout<<"------------------------------------\n";
-  for(auto& ele:result){
-    cout<<ele<<" ";
-  }cout<<endl;
-  cout<<"------------------------------------\n";
   return result;
 }
 void Packing(){
