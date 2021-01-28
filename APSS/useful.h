e94de1ba5e0406dd26a2d52391d01fcde4b0d5e9
rain <parkkwang2@naver.com>
2021-01-28 10:15:49 +0900

    recursive tip

--- APSS/useful.h
+++ APSS/useful.h
@@ -73,6 +73,7 @@ string to something
 간단한 팁
   공백 string을 반환하고 싶다면 return string();
   lower_bound를 통해 pair->first만 비교하고 싶다면, make_pair(value,numeric_limits<int>::min())을 비교 인자로 넣으면 된다.
+  재귀함수의 경우, 선 재귀 후 확인, 즉 조건을 서두에서 chk하는것이 좋다. (의도하지 않은 동작 방지) 
 ------------------------------
 자주하는 실수
   산술 오버플로우::매우 자주 발생하는 오류 
