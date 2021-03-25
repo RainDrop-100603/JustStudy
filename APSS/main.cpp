63e75a1cb15e6e339ca416cf39a5aacab6fa5220
rain <parkkwang2@naver.com>
2021-03-25 17:45:35 +0900

    SUSHI

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -11,167 +11,100 @@
 using namespace std;
 
 // memOver 대비 필요, 특정 조건을 만족하면 바로 ret하게 만들어 수행시간 향상 
-void BLOCKGAME_Input(vector<vector<string>>& board){
-  string tmp;
-  for(int i=0;i<5;i++){
-    cin>>tmp;
-    for(int j=0;j<5;j++){
-      board[i][j]=tmp[j];
-    }
+void SUSHI_Input(vector<pair<int,int>>& menu, int& budget){
+  int menuLen;
+  cin>>menuLen>>budget;
+  menu.resize(menuLen);
+  for(auto& ele: menu){
+    cin>>ele.first>>ele.second;
   }
 }
-char BLOCKGAME_cache(vector<char>& DP_cache, int can_use_bitmask){
-  //기저
-  if(can_use_bitmask==0){
-    return -1;
+int SUSHI_Algo(vector<pair<int,int>> menu,int budget){
+  //budget과 menu 100 나누기, menu sort
+  for(auto& ele:menu){
+    ele.first/=100;
   }
-  //기저 2
-  char& result=DP_cache[can_use_bitmask];
-  if(result!=-2){
-    return result;
-  }
-  //Algo
-  for(int idx=0;idx<25;idx++){
-    if((1<<idx)&can_use_bitmask){
-      can_use_bitmask-=(1<<idx);
-      //오른쪽 끝이 아닐경우, 오른쪽으로 확장 가능
-      if((idx%5!=4)&&(1<<(idx+1)&can_use_bitmask)){
-        can_use_bitmask-=(1<<(idx+1));
-        if(BLOCKGAME_cache(DP_cache,can_use_bitmask)==-1){
-          result=1;
-          return result;
-        }
-        //오른쪽 윗부분 체크
-        if((idx+1-5>0)&&((1<<(idx+1-5))&can_use_bitmask)){
-          can_use_bitmask-=(1<<(idx+1-5));
-          if(BLOCKGAME_cache(DP_cache,can_use_bitmask)==-1){
-            result=1;
-            return result;
-          }
-          can_use_bitmask+=(1<<(idx+1-5));
-        }
-        //오른쪽 아랫부분 체크
-        if((idx+1+5<25)&&((1<<(idx+1+5))&can_use_bitmask)){
-          can_use_bitmask-=(1<<(idx+1+5));
-          if(BLOCKGAME_cache(DP_cache,can_use_bitmask)==-1){
-            result=1;
-            return result;
-          }
-          can_use_bitmask+=(1<<(idx+1+5));
-        }
-        //왼쪽 아랫부분 체크
-        if((idx+5<25)&&((1<<(idx+5))&can_use_bitmask)){
-          can_use_bitmask-=(1<<(idx+5));
-          if(BLOCKGAME_cache(DP_cache,can_use_bitmask)==-1){
-            result=1;
-            return result;
-          }
-          can_use_bitmask+=(1<<(idx+5));
-        }
-        //왼쪽 윗부분 체크
-        if((idx-5>=0)&&((1<<(idx-5))&can_use_bitmask)){
-          can_use_bitmask-=(1<<(idx-5));
-          if(BLOCKGAME_cache(DP_cache,can_use_bitmask)==-1){
-            result=1;
-            return result;
-          }
-          can_use_bitmask+=(1<<(idx-5));
-        }
-        can_use_bitmask+=(1<<(idx+1));
-      }
-      //맨아래가 아닐경우, 아래로 확장 가능 
-      if((idx+5<25)&&((1<<(idx+5))&can_use_bitmask)){
-        can_use_bitmask-=(1<<(idx+5));
-        if(BLOCKGAME_cache(DP_cache,can_use_bitmask)==-1){
-          result=1;
-          return result;
-        }
-        can_use_bitmask+=(1<<(idx+5));
-      }
-      can_use_bitmask+=(1<<idx);
+  budget/=100;
+  sort(menu.begin(),menu.end());
+  //DP생성, 초기화
+  vector<int> tmp_cache(201,0);
+  for(auto& ele:menu){
+    if(ele.first<=budget){
+      tmp_cache[ele.first]=ele.second;
     }
   }
-  //결과값 return, 승리한것을 반환하지 못하면 ,패배 했다느 뜻
-  result=-1;
+  //Algo
+  for(int cost=0;cost<=budget;cost++){
+    if(tmp_cache[cost%201]!=0){
+      for(auto& ele:menu){
+        if(cost+ele.first<=budget){
+          int& value=tmp_cache[(cost+ele.first)%201];
+          value=max(value,tmp_cache[cost%201]+ele.second);
+        }
+      }
+    }
+  }
+  //result
+  int result=0;
+  for(auto& ele:tmp_cache){
+    result=max(result,ele);
+  }
   return result;
 }
-string BLOCKGAME_Algo(vector<vector<string>>& board){
-  //DP생성, 비트마스크 생성
-  vector<char> DP_cache(1<<25,-2);
-  int can_use_bitmask=0;
-  for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-      if(board[i][j]=="."){
-        can_use_bitmask+=1<<(5*i+j);
-      }
-    }
-  }
-  //Algo
-  int result=BLOCKGAME_cache(DP_cache,can_use_bitmask);
-  if(result==1){
-    return string("WINNING");
-  }else if(result==-1){
-    return string("LOSING");
-  }
-  return string("ERROR");
-}
-void BLOCKGAME(){
-  //BLOCKGAME
+void SUSHI(){
+  //SUSHI
   /*설명 및 입력
   설명
-    5×5 크기의 게임판에서 시작해, 둘이 번갈아 가며 블럭을 하나씩 게임판에 내려놓습니다. 
-    블럭은 L 모양으로 구성된 3칸짜리 블럭과 2칸짜리 블럭이 있으며, 항상 게임판에 있는 줄에 맞춰 내려놓아야 합니다. 
-    블럭들은 서로 겹칠 수 없습니다. 
-    각 블록은 자유롭게 뒤집거나 회전해서 올려놓을 수 있습니다. 
-    두 사람이 번갈아가며 블록을 올려놓다가 더 올려놓을 수 없게 되면 마지막에 올려놓은 사람이 승리합니다.
-    진행 중인 게임판이 주어질 때 이번 차례인 사람이 승리할 수 있는 방법이 있는지를 판단하는 프로그램을 작성하세요.
+    회전초밥집에는 n종류의 메뉴가 있는데, 운영진들은 각 메뉴에 대해 선호도를 매겼습니다.
+    운영진들은 주어진 예산 안에서 선호도의 합을 최대한으로 하도록 초밥을 먹고 싶습니다. 
+    각 종류의 초밥은 무한정으로 공급된다고 가정합시다. 이 때 얻을 수 있는 최대한의 선호도는 얼마일까요?
   입력
-    입력의 첫 줄에는 테스트 케이스의 수 C (C≤50)가 주어집니다. 
-    각 테스트 케이스는 다섯 줄에 각 다섯 개의 문자로 구성되며, #는 이미 블록이 놓인 칸, 마침표(.)는 블록이 없는 칸을 의미합니다.
+    입력의 첫 줄에는 테스트 케이스의 수 c(1 <= c <= 50)가 주어집니다. 
+    각 테스트 케이스의 첫 줄에는 초밥의 종류 n(1 <= n <= 20)과 운영진들의 예산 m (1 <= m <= 2,147,483,647)이 주어집니다. 
+    그 후 n 줄에 각 초밥의 가격과 선호도가 순서대로 주어집니다. 가격은 20,000 이하의 자연수로, 항상 100 의 배수입니다. 선호도는 20 이하의 자연수입니다.
   출력
-    각 테스트 케이스마다 한 줄을 출력합니다. 
-    이번 차례인 사람이 항상 이길 수 있는 방법이 있다면 WINNING을, 항상 질 수밖에 없다면 LOSING을 출력합니다.
+    각 테스트 케이스별로 한 줄에 가능한 선호도의 최대 합을 출력합니다.
   제한조건
-    2초, 64MB
+    8초, 64MB
   */
   /*힌트
-    대칭게임이므로, 두 사용자가 DP_cache를 공유한다.
-    f=max(for(모든 나의행동){나의 행동 & f(남은 게임판에서 상대의 승리여부)}) , 나의승리=1, 상대의 승리=-1
-    DP_default: -1
-    board를 사용하거나, board를 bitmask로 바꾸어 사용하거나, 둘 중 하나만 하면 된다.
-      25칸이므로 bitmask를 사용할 수 있다.
-      board를 bijection하는 함수가 필요하다.
-    왼쪽위(0,0)부터 오른쪽아래(4,4)로 진행하며, 확장 위치는 각 블록별로 다르게
-      2블록은 우측과 아래측
-      4블록은 우측 확장후, 위, 아래, 우측위, 우측아래 4방향으로 확장
+    예산이 1~int_max이지만, 금액은 항상 100의 배수다. 즉 DP이용시 cache의 크기를 100배 줄일 수 있다.
+    가격은 항상 20000원 이하이다. 현재금액에서 20000원보다 낮은 금액은 이용하지 않으므로, sliding window기법으로 mem 절약이 가능하다.
+      cost 오름차순으로 정렬해야 한다.
+      위 두개를 이용하면, 0~200까지, 총 201만큼의 cache만으로 연산이 가능하다.
+        0~199까지 할 경우, 200이 0에 저장되어 순서가 꼬이는 문제가 발생한다.
+      작은단위에서 큰 단위로 계산해야한다. 0~budget
+      마지막 정해진 금액을 초과할 경우, cache내부에서 가장 큰 값이 정답이다.
+      if(DP[x] != 0) -> for(모든 음식 1~n) DP[x+cost] = max(DP[x+cost],DP[x] + value) 
   */
   /*전략
   전략1
     Dynamic Programing
-      정답(큰단위)->작은단위 DP
-      f=max(for(모든 나의행동){나의 행동 & f(남은 게임판에서 상대의 승리여부)}) , 나의승리=1, 상대의 승리=-1
-        행동: 25번 반복, 블록 확인은 2개(2블록)+4개(3블록) = 6n
-        총 연산수(DP_cache크기): bitamsk=2^25 = 2^n
+      sliding window 기법을 사용한다.
+      작은단위 -> 큰단위 DP
+      금액과 예산은 모두 100으로 먼저 나눈뒤 사용한다.
+      if(DP[x] != 0) -> for(모든 음식 1~n) DP[x+cost] = max(DP[x+cost],DP[x] + value) 
+        cache 크기: 201
+        연산시간: 20(메뉴의 갯수)
+        연산횟수: 0~21474836 (int_max/100)
     시간:
-      O(2^n * n)
+      4억회
     크기:
-      O(2^n)
-    sol유의사항
-      int로 DP를 만들면 memover가 떠서, char로 바꾸어주었다.
-        vector<bitset<1>> 2줄을 이용할 수도 있다.(vector<bool>은 사용하지 않는다) 
+      201 byte
   */
   //Sol
   int testCase;
   cin>>testCase;
   while(testCase--){
-    vector<vector<string>> board(5,vector<string>(5));
-    BLOCKGAME_Input(board);
-    string result=BLOCKGAME_Algo(board);
+    int budget;
+    vector<pair<int,int>> menu; 
+    SUSHI_Input(menu,budget);
+    int result=SUSHI_Algo(menu,budget);
     cout<<result<<'\n';
   }
 }
 
 int main(void){
-  BLOCKGAME();
+  SUSHI();
   return 0;
 }
