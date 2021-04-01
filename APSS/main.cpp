38a1fbcce877d4f4fa26434b4345b55feece546c
rain <parkkwang2@naver.com>
2021-04-01 11:16:25 +0900

    t

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 //사회복무요원 교육 
 
 // greedy, 증명부분 괜찮았음, 참고하자 
-void LUNCHBOX_Input(int& number,vector<int>& warmTime,vector<int>& eatingTime){
+void STRJOIN_Input(int& number,vector<int>& warmTime,vector<int>& eatingTime){
   cin>>number;
   warmTime.resize(number);
   for(auto& ele: warmTime){
@@ -25,7 +25,7 @@ void LUNCHBOX_Input(int& number,vector<int>& warmTime,vector<int>& eatingTime){
     cin>>ele;
   }
 }
-int LUNCHBOX_Algo(int number,const vector<int>& warmTime,const vector<int>& eatingTime){
+int STRJOIN_Algo(int number,const vector<int>& warmTime,const vector<int>& eatingTime){
   //pair로 배열 만들고 내림차순 정렬
   vector<pair<int,int>> food;
   for(int i=0;i<number;i++){
@@ -41,29 +41,41 @@ int LUNCHBOX_Algo(int number,const vector<int>& warmTime,const vector<int>& eati
   }
   return result;
 }
-void LUNCHBOX(){
-  //LUNCHBOX
+void STRJOIN(){
+  //STRJOIN
   /*설명 및 입력
   설명
-    they provided different N lunch boxes. and put all lunch boxes to a refrigerator.
-    Specifically, i-th lunch box needs Mi seconds to microwave and Ei seconds to eat.
-    Ainu7 needs to schedule microwave usage order to minimize lunch time. 
-    Lunch time is defined as the duration from the beginning of microwaving of any lunch box to the end of eating for all participants.
+    n 개의 문자열들의 길이가 주어질 때 필요한 최소 비용을 찾는 프로그램을 작성하세요.
+      문자열을 합치는데 드는 비용: 각 문자열의 길이의 합
   입력
-    The first line of the input contains one integer T, the number of test cases.
-    Each test case consists of three lines. The first line of each test case contains N(1≤N≤10000), the number of the participants.
-    N integers will follow on the second line. They represent M1, M2, ⋯, MN.
-    Similarly, N integers will follow on the third line, representing E1, E2, ⋯, EN.
+    입력의 첫 줄에는 테스트 케이스의 수 c (c <= 50) 가 주어집니다. 
+    각 테스트 케이스의 첫 줄에는 문자열의 수 n (1 <= n <= 100) 이 주어지며, 다음 줄에는 n 개의 정수로 각 문자열의 길이가 주어집니다. 
+    각 문자열의 길이는 1,000 이하의 자연수입니다.
   출력
-    For each test case, print the minimized lunch time in one line. It is guaranteed that the answer is always strictly less than 231.
+    각 테스트 케이스마다 한 줄에 모든 문자열을 합칠 때 필요한 최소 비용을 출력합니다.
   제한조건
-    5초, 64MB
+    1초, 64MB
   */
   /*힌트
-    번역
-      N개의 도시락, 각각의 데우는 시간 M, 먹는시간 E
-      데우기 시작인 이후, 다 먹으면 끝난다.
-      최소시간을 구하라
+    최대한 작은 단위로 합치는것이 유리하다
+      큰 것을 여러번 합치면, 그만큼 비용이 증가한다.
+    한 str에 처음부터 끝까지 합치는 것은 적절하지 못하다
+      한 str에 여러번 합치면 결국 길이가 길어지고 비용이 증가한다.
+    가장 작은 str 2개를 골라 합친 후, 합쳐진 str을 포함하여 가장 작은 str 2개를 다시 골라 합치는 것을 반복한다.
+      합치는 횟수는 n-1로 동일하므로, 합치는 cost를 매번 최소로 하는것이 최적값이다.
+      greedy choice property:
+        n개의 원소에 대해, 각 원소x가 결합에 참여하는 횟수를 times(x)라 하자
+        greedy하게 선택한 경우 len(x1)<len(x2)에 대해, times(x1)>times(x2) 이다.
+        정답 S 중, len(x1)<len(x2)일때 times(x1)<=times(x2)인 선택이 있다고 하자.
+          해당하는 선택을 바꾸어 greedy하게 바꾼 후에도 여전히 정답이라면, greedy한 선택이 정답임을 알 수 있다.
+          정답: l1*t1+l2*t2
+          greedy: l1*t2+l2*t1
+            정답 - greedy = (l1-l2)(t1-t2) >=0 
+          즉 정답 S중에 greedy한 선택이 있음을 알 수 있다.
+      optimal substructure:
+        S(n)= S(x)+S(n-x)
+        
+
     데우는 시간은 모두 동일하다
       데우면서 먹을수 있다.
       즉 데우는 시간은 절약이 불가하지만 ,먹는 시간은 절약이 가능하다.
@@ -102,13 +114,13 @@ void LUNCHBOX(){
   while(testCase--){
     int number;
     vector<int> warmTime, eatingTime;
-    LUNCHBOX_Input(number,warmTime,eatingTime);
-    int result=LUNCHBOX_Algo(number,warmTime,eatingTime);
+    STRJOIN_Input(number,warmTime,eatingTime);
+    int result=STRJOIN_Algo(number,warmTime,eatingTime);
     cout<<result<<'\n';
   }
 }
 
 int main(void){
-  LUNCHBOX();
+  STRJOIN();
   return 0;
 }
