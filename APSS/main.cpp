f6032d4ada4821f6cbcf6900b9298002b6eafc8d
rain <parkkwang2@naver.com>
2021-04-19 17:56:36 +0900

    t

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -15,158 +15,141 @@ using namespace std;
 //사회복무요원 교육 
 
 // greedy, atan2 함수, 원에서의 각도를 이용할땐, 2pi마다 반복되는것을 고려하고 정규화, 자료형 잘 확인하기  
-void MINASTIRITH_Input(int& pointNum,vector<vector<double>>& pointArr){
-  cin>>pointNum;
-  pointArr=vector<vector<double>>(pointNum,vector<double>(3));
-  for(auto& ele: pointArr){
-    cin>>ele[0];
-    cin>>ele[1];
-    cin>>ele[2];
+void BOARDCOVER2_Input(vector<vector<char>>& board, vector<vector<char>>& block){
+  int height,width,row,column;
+  cin>>height>>width>>row>>column;
+  board=vector<vector<int>>(height,vector<int>(width));
+  block=vector<vector<int>>(row,vector<int>(column));
+  for(auto& eleRow:board){
+    for(auto& eleCol:eleRow){
+      cin>>eleCol;
+    }
+  }
+  for(auto& eleRow:block){
+    for(auto& eleCol:eleRow){
+      cin>>eleCol;
+    }
   }
 }
-int MINASTIRITH_func(const vector<pair<double,double>>& pointAngleArr,const vector<int>& cache, int idx){
-  int count(1);
-  double correction(pointAngleArr[idx].first);
-  while(idx!=-1){
-    if(pointAngleArr[idx].second>=acos(0)*4+correction){
-      return count;
-    }
-    idx=cache[idx];
-    count++;
-  }
-  return 1000;
+void BOARDCOVER2_func(vector<vector<char>>& board, const vector<vector<char>>& block,int& result,int row,int col){
+  //각 위치 input 가능한지 확인
+    //각 위치 input
+    //재귀
+    //각 위치 다시 output
+    //result값은 계속 갱신
+  //리턴
 }
-int MINASTIRITH_Algo(int pointNum,const vector<vector<double>>& pointArr){
-  //변환
-  vector<pair<double,double>> pointAngleArr;
-  double pi=acos(0)*2;
-  for(auto& ele: pointArr){
-    double pointAngle=atan2(ele[0],ele[1]);
-    if(ele[2]>=16.0){
-      return 1;
+
+int BOARDCOVER2_Algo(vector<vector<char>> board, vector<vector<char>> block){
+  //.과 #을 0과 1로 변환
+  for(auto& ele: board){
+    for(auto& ele2: ele){
+      if(ele2=='#'){
+        ele2=1;
+      }else{
+        ele2=0;
+      }
     }
-    double oversightAngle=asin(ele[2]/16)*2;  
-    double start=fmod(pointAngle-oversightAngle+2*pi,2*pi);
-    double end=start+oversightAngle*2;
-    pointAngleArr.push_back(make_pair(start,end));
   }
-  sort(pointAngleArr.begin(),pointAngleArr.end());
-  //cache 생성, n번째 초소에서
-  vector<int> cache(pointNum,-1); 
-  for(int i=0;i<pointNum;i++){
-    double prev_end=pointAngleArr[i].second;
-    double tmp_now_end=prev_end;
-    for(int j=i+1;j<pointNum;j++){
-      if(pointAngleArr[j].first>prev_end){
-        break;
-      }
-      if(pointAngleArr[j].second>tmp_now_end){
-        cache[i]=j;
-        tmp_now_end=pointAngleArr[j].second;
+  for(auto& ele: block){
+    for(auto& ele2: ele){
+      if(ele2=='#'){
+        ele2=1;
+      }else{
+        ele2=0;
       }
     }
-  } 
-  //algo
+  }
+  //block의 8개의 방향, 중복은 제거
+  vector<vector<vector<char>>> block_arr;
+  block_arr.push_back(block);
+  //블록 뒤집기
+  vector<vector<char>> col_reverse_block;
+  for(auto ele:block){
+    reverse(ele.begin(),ele.end());
+    col_reverse_block.push_back(ele);
+  }
+  vector<vector<char>> row_reverse_block;
+  for(auto iter=block.rbegin();iter!=block.rend();iter++){
+    row_reverse_block.push_back(*iter);
+  }
+  //블록 넣기
+  vector<vector<char>> tmp_block;
+  bool canInput(true);
+  for(auto& ele: block_arr){
+    if(tmp_block.size()!=ele.size()){
+      break;
+    }
+    for(int row=0;row<ele.size();row++){
+      for(int col=0;col<ele[0].size();col++){
+        if(tmp_block[row][col]!=ele[row][col]){
+          canInput=false;
+          row=ele.size(); //두개의 for문을 탈출하기 위함
+          break;
+        }
+      }
+    }
+    if(!canInput) break;
+  }
+  if(canInput){
+    block_arr.push_back(tmp_block);
+  }
+
+
   int result(1000);
-  for(int idx=0;idx<pointNum;idx++){
-    result=min(result,MINASTIRITH_func(pointAngleArr,cache,idx));
-  }
+  BOARDCOVER2_func(board,block,result,0,0);
   return result;
 }
-void MINASTIRITH(){
-  //MINASTIRITH
+void BOARDCOVER2(){
+  //BOARDCOVER2
   /*설명 및 입력
   설명
-    반지름이 8 킬로미터나 되는 거대한 원형 성벽, n 개의 초소
-      각 초소들은 해당 위치를 중심으로 반지름 ri 의 원 내부를 감시
-    최소의 인원으로 성벽의 모든 부분을 감시하기 위해, 일부 초소에만 병사를 배치하려고 합니다. 
-    각 초소의 위치와 감시 범위가 주어질 때, 성벽의 모든 부분을 감시하기 위해 필요한 최소한의 병사 수를 계산하는 프로그램을 작성하세요.
+    H×W 크기의 게임판과 한 가지 모양의 블록이 여러 개 있습니다. 
+    게임판에 가능한 많은 블록을 올려놓고 싶은데, 게임판은 검은 칸과 흰 칸으로 구성된 격자 모양을 하고 있으며 이 중에서 흰 칸에만 블록을 올려놓을 수 있습니다. 
+    이때 블록들은 자유롭게 회전해서 놓을 수 있지만, 서로 겹치거나, 격자에 어긋나게 덮거나, 검은 칸을 덮거나, 게임판 밖으로 나가서는 안 됩니다.
+    게임판과 블록의 모양이 주어질 때 최대 몇 개의 블록을 올려놓을 수 있는지 판단하는 프로그램을 작성하세요.
   입력
-    입력의 첫 줄에는 테스트 케이스의 수 c (c <= 50) 가 주어집니다. 
-    각 테스트 케이스의 첫 줄에는 초소의 개수 n (1 <= n <= 100) 이 주어지며, 
-    그 후 n 줄에 각 3 개의 실수로 각 초소의 위치 yi, xi, 그리고 감시 범위 ri (0 < ri <= 16.001) 가 주어집니다.
-  성벽은 (0,0) 을 중심으로 하는 반지름 8 인 원으로, 모든 초소는 이 성벽 위에 위치합니다.
+    입력의 첫 줄에는 테스트 케이스의 수 T (T≤100)가 주어집니다. 
+    각 테스트 케이스의 첫 줄에는 게임판의 크기 H, W (1≤H, W≤10), 그리고 블록의 모양을 나타내는 격자의 크기 R, C (1≤R, C≤10)가 주어집니다. 
+    다음 H줄에는 각각 W 글자의 문자열로 게임판의 정보가 주어집니다. 문자열의 각 글자는 게임판의 한 칸을 나타내며, #은 검은 칸, 마침표는 흰 칸을 의미합니다. 
+    다음 R줄에는 각 C 글자의 문자열로 블록의 모양이 주어집니다. 이 문자열에서 #은 블록의 일부, 마침표는 빈 칸을 나타냅니다.
+    각 게임판에는 최대 50개의 흰 칸이 있으며, 각 블록은 3개 이상 10개 이하의 칸들로 구성됩니다. 
+    변을 맞대고 있는 두 변이 서로 연결되어 있다고 할 때, 블록을 구성하는 모든 칸들은 서로 직접적 혹은 간접적으로 연결되어 있습니다.
   출력
-    각 테스트 케이스마다 한 줄에 필요한 최소의 병사 위치를 출력합니다. 
-    만약 어떻게 하더라도 성벽의 모든 부분을 감시할 수 없다면 IMPOSSIBLE 을 대신 출력합니다. 
-    입력에 주어지는 초소의 좌표, 혹은 감시 범위가 최대 0.0000001 만큼 변하더라도 답은 변하지 않는다고 가정해도 좋습니다.
+    각 테스트 케이스마다 게임판에 놓을 수 있는 최대의 블록 수를 출력합니다.
   제한조건
     2초, 64MB
   */
   /*힌트
-    실수 자릿수를 사용한다
-      0.0000001만큼 변해도 답은 변하지 않는다 -> 소수점 7자리까지는 정확하며, 넉넉하게 범위를 줬다고 생각하자 
-      변환은 최대한 줄이는것이 오차를 줄인다.
-    한바퀴 도는법
-      원의 둘레를 기준으로 한다.
-      중심에서의 각도를 기준으로 한다.
-    풀이법 직관
-      한 초소가 끝나는 위치를 포함하는 모든 초소중에, 가장 먼 범위까지 포함하는 초소를 선택한다.
-        좌우 모두 선택? 한 방향으로만 선택?
-      생긴건 원이지만, 직선으로 치환해서 생각할 수 있다.
-        모든 범위를 포함하며, 가장적은 갯수를 사용하게 하는법
-        초소의 각도, 초소가 감시하는 범위의 각도를 이용하자
-          pair 자료형(감시시작위치, 감시 끝 위치) 이용하면 편할듯
-    최저값을 구해야 하므로 IMPOSSIBLE은 초소의 갯수 + 100로 정하자 
-    Greedy
-      직선으로 치환하여 생각하자, 결과 x의 n번째 선택은 x(n), 
-        x(n+1).start가 x(n).end보다 뒤에 있으며, x(n+1).end가 가장 큰 n+1을 선택하자.
-      시작위치는 반드시 포함된다. 그러나 Sol은 시작위치를 포함하지 않을 수 있다.
-        시작위치를 제거하고 다시 Algo를 돌린다.
-          IMPOSSIBLE가 나올 때 까지 반복한다. 
-          IMPOSSIBLE이 나올 때의 최솟값이 정답이다.
-      Greedy Choice Property: 
-        greedy 하지 않은 sol a가 있다고 하자.
-          a(n+1).start<=a(n).end, a(n+2).start<=a(n+1).end 관계가 성립할 것이다.
-        a(n+1)을 greedy한 선택 x(n+1)로 대체했다고 가정하자
-          x(n+1).start<=a(n).end, a(n+2).start<=a(n+1).end<=x(n+1).end
-        a(n+1)을 x(n+1)로 바꾸어도 sol이므로, greedy한 선택을 포함하는 sol이 있음을 알 수 있다.
-      Optimal Substructure:
-        S(n)=greedy(n)+S(n-1)
-        n개에서의 sol은, n개에서의 greedy 한 선택과 남은 n-1개에서의 sol과 같다. (표현 책보고 다듬기)
+    조합 풀이법을 사용하자
+      완전탐색을 구현한 후 하나씩 최적화를 진행한다.
+    흰 칸의 개수는 최대 50개임을 이용하자(게임판은 최대 100칸)
+    완전탐색
+      좌상단에서 우하단으로 가면서, 각 위치에서 블록을 놓을 수 있는지 확인한다.
+      이때 블록은 좌우,상하로 뒤집을 수 있으며, 90도를 돌릴 수 있으므로 8가지의 모양을 가진다.
   */
   /*전략
   전략1
-    Greedy Algorithm
-      각 점을 pair(감시시작위치, 감시 끝 위치)로 변환
-        감시 시작 위치를 기준으로 정렬
-        choice(n).start <= choice(n+1).start <= choice(n).end 인 choice(n+1)에 대해, choice(n+1).end가 가장 큰 값 선택 
-      IMPOSSIBLE=초소의 갯수 + 100
-      시작위치에서 (360도 + 보정치)가 넘을때까지 greedy한 선택을 한다. -> O(n)
-        choice(n).start <= choice(n+1).start <= choice(n).end 인 choice(n+1)에 대해, choice(n+1).end가 가장 큰 값 선택 -> O(n)
-        보정치: 시작초소의 시작위치 
-      시작위치를 포함하지 않는것이 답일 수 있으므로, 시작위치를 삭제하고 다음위치부터 시작하는 것을 반복한다. -> O(n)
-        IMPOSSIBLE이 뜨면 stop
+    combination search
     시간:
       O(N^3) 
     크기:
       O(N)
     보완
-      greedy한 선택은 cache로 저장해 둘 수 있다 -> n^2
-        전체시간 o(n^2)으로 단축 
-      atan2함수를 사용하자 
-      시작위치를 0~2pi로 정규화 해야 위 전략을 사용할 수 있다.
-        기존에는 초소가 -pi~pi, 감시각도가 0~pi여서 감시 시작위치는 -2pi~pi
-      시작 위치를 삭제할 필요없이, 다음위치부터 시작하면 된다.
-        idx번째부터 end까지 체크하도록 한다.
+      \
   */
   //Sol
   int testCase;
   cin>>testCase;
   while(testCase--){
-    int pointNum;
-    vector<vector<double>> pointArr;
-    MINASTIRITH_Input(pointNum,pointArr);
-    auto result=MINASTIRITH_Algo(pointNum,pointArr);
-    if(result==1000){
-      cout<<"IMPOSSIBLE"<<"\n";
-    }else{
-      cout<<result<<"\n";
-    }
+    vector<vector<char>> board,block;
+    BOARDCOVER2_Input(board,block);
+    auto result=BOARDCOVER2_Algo(board,block);
+    cout<<result<<"\n";
   }
 }
 
 int main(void){
-  MINASTIRITH();
+  BOARDCOVER2();
   return 0;
 }
