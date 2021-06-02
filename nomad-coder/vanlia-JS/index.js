e73212419c2ddff4d8730b72a2c2bd1d283ea3d5
rain <parkkwang2@naver.com>
2021-06-02 11:38:16 +0900

    ttt

--- nomad-coder/vanlia-JS/index.js
+++ nomad-coder/vanlia-JS/index.js
@@ -4,3 +4,14 @@ console.dir(title);
 
 title.innerHTML="Title Changed";
 title.style.color="red";
+
+function handleResize(event){
+  console.log(event);
+}
+
+function titleClicked(event){
+  title.style.color="black";
+}
+
+window.addEventListener("resize",handleResize);   //window(화면) 에서 resize event가 발생하면, handleResize라는 함수를 실행, event는 JS에서 자동으로 전달? 
+title.addEventListener("click",titleClicked);
