6adbc3b1ff40486aad7eaa3ea2cea7202f86b002
rain <parkkwang2@naver.com>
2021-06-02 11:20:55 +0900

    t

--- nomad-coder/vanlia-JS/index.js
+++ nomad-coder/vanlia-JS/index.js
@@ -1,18 +1,6 @@
-// const obj1={name: "test", male: true};
-// const var1=3
-// const arr1=["3",4,true]
-// const obj2={
-//   ele1:obj1, ele2:var1, ele3:arr1}
-// console.log(obj2);
-// const arr2=[obj1,var1,arr1,obj2];
-// console.log(arr2);
-// const arr3=[]
-// const obj3={}
+const title=document.getElementById("title");
 
-// 테스트 코드
-var obj = {
-	attributes: []
-}
+console.dir(title);
 
-console.log('test')
-(obj.attributes || []).forEach(function(i) {})
+title.innerHTML="Title Changed";
+title.style.color="red";
