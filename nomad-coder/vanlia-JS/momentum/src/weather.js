249592597108e4c7fbdd680c5fdc979ebffe1d0d
Rain <parkkwang2@naver.com>
2021-06-17 11:03:11 +0900

    0617

--- nomad-coder/vanlia-JS/momentum/src/weather.js
+++ nomad-coder/vanlia-JS/momentum/src/weather.js
@@ -6,9 +6,10 @@ const WEATHER_API_KEY="33a4b37f047d9d66fe381f85dc77ba6f",
 //실제 날씨 표현, 날씨 예보도 추가 
 function weather_icon(str){
   if(str=="few clouds"||str=="scattered clouds"){return "⛅"+str;}
-  if(str=="temp"){return "🌡";}
+  if(str=="overcast clouds"){return "☁️"+str;}
+  if(str=="temp"){return "🌡️";}
   if(str=="humidity"){return "💧";}
-  if(str=="wind"){return "༄";}
+  if(str=="wind"){return "🌬️";}
   if(str=="clear sky"){return "🌞"+str;}
 }
 
