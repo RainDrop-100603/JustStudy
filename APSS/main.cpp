14ca93b9a65e9999b52224a4a9523bb392a54374
rain <parkkwang2@naver.com>
2021-06-30 17:52:06 +0900

    t

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -50,19 +50,19 @@ ostream& operator<<(ostream& os, vector2& vec){
   return os;
 }
 // 계산 기하, vector의 이용, double이용시 오차범위 항상 유의
-void PINBALL_Input(int& xPos,int& yPos,int& dx,int& dy,int& num,vector<int>& centerX,vector<int>& centerY,vector<int>& radius){
+void TREASURE_Input(int& xPos,int& yPos,int& dx,int& dy,int& num,vector<int>& centerX,vector<int>& centerY,vector<int>& radius){
   cin>>xPos>>yPos>>dx>>dy>>num;
   centerX.resize(num);centerY.resize(num);radius.resize(num);
   for(int i=0;i<num;i++){
     cin>>centerX[i]>>centerY[i]>>radius[i];
   }
 }
-vector2 PINBALL_lineSym(vector2 point1,vector2 a,vector2 b){
+vector2 TREASURE_lineSym(vector2 point1,vector2 a,vector2 b){
   //point1을 직선 ab에대해 선대칭 이동한다.
   vector2 crossPoint=point1.pFoot(a,b-a);
   return point1+(crossPoint-point1)*2;
 }
-pair<int,vector2> PINBALL_ifMet(vector<int>& centerX,vector<int>& centerY,vector<int>& radius,vector2 ballPoint, vector2 ballVector){
+pair<int,vector2> TREASURE_ifMet(vector<int>& centerX,vector<int>& centerY,vector<int>& radius,vector2 ballPoint, vector2 ballVector){
   double minDist=1000.0;
   int circleIdx=-1;
   for(int i=0;i<centerX.size();i++){
@@ -89,14 +89,14 @@ pair<int,vector2> PINBALL_ifMet(vector<int>& centerX,vector<int>& centerY,vector
   }
   return make_pair(circleIdx,ballPoint+ballVector*minDist);
 }
-vector<int> PINBALL_func(vector<int>& centerX,vector<int>& centerY,vector<int>& radius,vector2 ballPoint, vector2 ballVector, int count){
+vector<int> TREASURE_func(vector<int>& centerX,vector<int>& centerY,vector<int>& radius,vector2 ballPoint, vector2 ballVector, int count){
   //기저, count만큼 반복
   if(count==0){
     return vector<int>();
   }
   //원과 만나는 벡터 중 가장 가까운 벡터를 구한다.
-  pair<int,vector2> tmp=PINBALL_ifMet(centerX,centerY,radius,ballPoint,ballVector);
-  // pair<int,vector2> tmp2=PINBALL_ifMet(centerX,centerY,radius,ballPoint,ballVector);
+  pair<int,vector2> tmp=TREASURE_ifMet(centerX,centerY,radius,ballPoint,ballVector);
+  // pair<int,vector2> tmp2=TREASURE_ifMet(centerX,centerY,radius,ballPoint,ballVector);
   // vector2 tmp3=tmp.second-tmp2.second;
   // cout<<tmp.first<<":"<<tmp2.first<<tmp.second<<":"<<tmp2.second<<":"<<tmp3<<endl;;
   int circleIdx=tmp.first;
@@ -106,84 +106,65 @@ vector<int> PINBALL_func(vector<int>& centerX,vector<int>& centerY,vector<int>& 
   }
   // 새로운 핀볼의 위치와 방향벡터를 구한다.
   vector2 center(centerX[circleIdx],centerY[circleIdx]);
-  vector2 newBallVector=PINBALL_lineSym(ballPoint,center,newBallPoint)-newBallPoint;
+  vector2 newBallVector=TREASURE_lineSym(ballPoint,center,newBallPoint)-newBallPoint;
   newBallVector=newBallVector.normalize();
   //재귀, 정답을 반대로 저장함에 유위하자
-  vector<int> result=PINBALL_func(centerX,centerY,radius,newBallPoint,newBallVector,count-1);
+  vector<int> result=TREASURE_func(centerX,centerY,radius,newBallPoint,newBallVector,count-1);
   result.push_back(circleIdx);
   return result;
 }
-vector<int> PINBALL_Algo(int xPos,int yPos,int dx,int dy,int num,vector<int> centerX,vector<int> centerY,vector<int> radius){
+vector<int> TREASURE_Algo(int xPos,int yPos,int dx,int dy,int num,vector<int> centerX,vector<int> centerY,vector<int> radius){
   //재귀함수 구현
   vector2 ballPoint(xPos,yPos),ballVector(dx,dy); 
   ballVector=ballVector.normalize();
   int count=100;
-  vector<int> result=PINBALL_func(centerX,centerY,radius,ballPoint,ballVector,count);
+  vector<int> result=TREASURE_func(centerX,centerY,radius,ballPoint,ballVector,count);
   reverse(result.begin(),result.end());
   return result;
 }
-void PINBALL(){
-  // PINBALL
+void TREASURE(){
+  // TREASURE
   /*설명 및 입력
   설명
-    동혁이는 새로 나온 핀볼 게임을 하고 있습니다. 
-      이 핀볼 게임은 아주 큰 게임판에 여러 개의 장애물을 놓고, 밖에서 공을 던져 가장 많은 장애물을 맞추는 것을 목표로 합니다. 
-    공과 장애물은 완전한 원형이며, 공은 장애물에 부딪히면 항상 입사각과 반사각이 같도록 정반사됩니다. 
-      장애물은 게임판에 고정되어 있기 때문에 움직이지 않습니다.
-    장애물들의 위치와 크기가 주어지고, 공의 위치와 방향이 주어질 때 공이 부딪히는 장애물들의 번호를 계산하는 프로그램을 작성하세요. 
-      공은 반지름 1인 원이며, 항상 직선으로 움직인다고 가정합니다.
+    섬의 지도가 위 그림과 같이 N개의 점을 갖는 다각형으로 주어질 때, 
+      보물이 묻혀 있을 수 있는 곳은 두 점 (x1,y1)과 (x2,y2) 를 서로 대칭인 꼭지점으로 갖고, 
+      네 변이 모두 x축 혹은 y축에 평행한 직사각형 내부입니다. 
+    우리는 이 직사각형 내에 포함된 육지를 전부 조사하고 싶습니다. 
+    우리가 조사해야 할 부분의 넓이를 계산하는 프로그램을 작성하세요.
   입력
-    입력의 첫 줄에는 테스트 케이스의 수 C(C <= 50)가 주어집니다. 
-    각 테스트 케이스의 첫 줄에는 5개의 정수 x, y, dx, dy, N 이 주어집니다. 
-      이 때 x, y (0 <= x, y <= 100)는 현재 공 중심의 위치를 나타내며, dx, dy (-10 <= dx, dy <= 10)는 공의 방향을 나타냅니다. 
-      이 때 공은 현재 (x,y) 에서 (x+dx, y+dy)를 향해 일직선으로 굴러가고 있습니다. 
-    N(0 <= N <= 50)은 장애물의 개수를 나타내며, 각 장애물에는 0번부터 N-1번까지의 번호가 매겨져 있습니다. 
-      그 다음 N줄에는 0번부터 각 장애물의 정보가 3개의 정수 x_i, y_i, r_i 로 주어집니다. 
-      (0 <= x_i, y_i, r_i <= 100) 이 때 i번 장애물은 (x_i, y_i)을 중심으로 한 반지름 r_i인 원 형태를 하고 있습니다.
-  장애물은 게임판 밖에 걸쳐 있을 수도 있습니다. 
-    두 장애물 사이의 거리가 2 이하인 경우는 없습니다. 
-    시작 위치에서 공은 장애물과 겹쳐 있거나 닿아 있지 않습니다.
-  공은 게임판의 벽에 닿는 순간 게임판에서 떨어진다고 가정합니다. 
-    입력에 공이 게임판의 벽과 장애물에 동시에 닿는 경우는 주어지지 않습니다.
+    입력의 첫 줄에는 테스트 케이스의 수 C (C <= 50) 가 주어집니다. 
+    각 테스트 케이스의 첫 줄에는 다섯 개의 정수로 직사각형의 꼭지점의 좌표 x1 , y1 , x2 , y2 (0 <= x1 < x2 <= 100,0 <= y1 < y2 <= 100) 
+      그리고 섬의 지도를 나타내는 다각형 꼭지점의 수 N (3 <= N <= 100) 이 주어집니다. 
+      그 후 N 줄에 각 2개의 정수로 각 꼭지점의 좌표 Xi , Yi (0 <= Xi, Yi <= 100) 가 주어집니다. 
+      꼭지점은 시계 반대 방향으로 주어지며, 마지막 점은 첫 번째 점과 연결되어 있습니다.
+    주어진 섬의 면적이 0이거나, 섬의 경계선이 자기 자신과 교차하거나 겹치는 경우는 없습니다.
   출력
-    각 테스트 케이스마다 한 줄에 공이 닿는 순서대로 각 장애물의 번호를 출력합니다. 
-      만약 공이 장애물에 100번 넘게 부딪힐 경우 첫 100번 부딪히는 장애물의 번호만을 출력합니다.
-    주어진 입력에서 장애물의 크기나 위치가 10^-7 만큼 변하더라도 답은 변하지 않는다고 가정해도 좋습니다.
+    각 테스트 케이스마다 한 줄에 조사해야 할 육지의 넓이를 출력합니다. 
+    10-7 이하의 절대/상대 오차를 갖는 답은 정답으로 인정됩니다.
   제한조건
     1초, 64MB
   */
   /*힌트
-    시작점을 어떻게 구현하는가
-      힌트
-        벡터를 구할때 시작점을 (0,0)으로 두고 계산하면 벡터만으로 계산이 된다.
-    원과 벡터가 만나는지를 구할 수 있어야 한다.
-      힌트
-        가장 처음 만나는 원을 구하는 방법
-        원을 만나지 않는다는 것은, 벽을 만난다는 것과 같다.
-          원을 만났더라도, 이때의 좌표가 범위(0~100)를 벗어나면, 벽을 만난것으로 판단한다.
-      방법1
-        원의 중심과 벡터와의 거리를 구한다. -> 원과 만나는지 알 수 있다.
-          거리는 1+원의 반지름 이하여야 한다.
-        시작점부터 교점까지의 좌표가 가장 작은 것이 가장 먼저 만나는 점이다.
-          시작점-원의 중심 벡터를 a라 하자, 핀볼의 경로는 벡터 b
-          벡터a를 벡터b에 projection, 원의 중심과 벡터b의 거리를 구하고, 원의 반지름+1을 이용한 피타고라스의 정리(그림그려보자)
-            핀볼의 이동거리와 새로운 중심을 구할 수 있다.
-          원과 핀볼의 교점도 구할 수 있다. (필요할지는 모르겠다)
-            핀볼의 새로운 중심에서 원의 중심을 향한 벡터를 c라 하자
-            벡터 a와 벡터 c를 더하면, 핀볼의 시작점에서 핀볼과 원의 교점으로 향하는 벡터다. 
-        모든 원에 대해 수행하면 된다.
-    입사각과 반사각이 같도록 튕겨나가는 것을 구현해야 한다.
-      힌트
-        두 벡터 사이의 각도를 구하는 방법
-        벡터를 특정 각도만큼 회전시키는 방법
-      방법1
-        벡터에 대해 점을 선대칭으로 이동시킨다.
-        원과의 교점과, 대칭이동된 점을 잇는 벡터를 구하면 된다.
-    함수 자체는 재귀를 이용하면 된다.
-      벽을 만나거나 count=100이 되면 stop
-    풀이법
-      1. 가장 먼저 만나는 원을 구한다. 원이 없거나 범위를 벗어난다면 벽이다.
-      2. 원과의 교점과 입사각을 구하고, 입사각과 반사각이 같도록 교점에서 벡터를 회전시킨다. 이후 1을 반복    
+    섬은 단순 다각형(simple polygon)이다.
+    조사해야할 부분의 모양
+      1. 하나의 단순 다각형
+      2. 여러개의 단순 다각형
+    꼭짓점을 이용한 방법
+      조사해야할 부분의 꼭짓점
+        1. 섬과 직사각형 영역의 교점
+        2. 섬 내부의 점
+      꼭짓점을 어떻게 이어서 모양을 만들어야하나?
+    선분을 이용한 방법
+      보물 영역과 섬 영역의 경우의 수
+        1. 섬 영역이 보물 영역을 완전히 포함 -> 꼭짓점 4개가 섬에 포함됨
+        2. 영역이 일부 겹치면서, 하나의 단순 다각형 영역을 이룸
+        3. 영역이 일부 겹치면서, 여러개의 단순 다각형 영역을 이룸
+      구현방법
+        1. 모든 섬의 꼭짓점이 보물영역 바깥에 있다면, 섬 영역이 보물영역을 완전히 포함한다.
+        2. 만약 포함되는 꼭짓점이 있다면, 해당 영역에서 시작하여 겹치는 부분을 찾아낸다.
+          2-1. 계속하여 다음 꼭짓점을 순회하며 찾는다. 이때 보물 영역과의 교점이 생긴다면, 해당 부분을 꼭짓점으로 한다.
+            2-1-1. 다음 꼭짓점이 직전 꼭짓점과 같은 선분에 있다면, 그대로 꼭짓점을 추가한다.
+            2-1-2. 다음 꼭짓점이 직전 꼭짓점과 다른 선분에 있다면, 그 선분까지 이어지도록 꼭짓점을 추가한다.
   */
   /*전략
   전략1
@@ -211,8 +192,8 @@ void PINBALL(){
   while(testCase--){
     int xPos,yPos,dx,dy,num;
     vector<int> centerX,centerY,radius;
-    PINBALL_Input(xPos,yPos,dx,dy,num,centerX,centerY,radius);
-    auto result=PINBALL_Algo(xPos,yPos,dx,dy,num,centerX,centerY,radius);
+    TREASURE_Input(xPos,yPos,dx,dy,num,centerX,centerY,radius);
+    auto result=TREASURE_Algo(xPos,yPos,dx,dy,num,centerX,centerY,radius);
     //cout<<"::::";
     for(int i=0;i<result.size();i++){
       if(i) cout<<" ";
@@ -224,7 +205,7 @@ void PINBALL(){
 int main(void){
     //clock_t start,end;
     //start=clock();
-  PINBALL();
+  TREASURE();
     //end=clock();;
     //cout<<"time(s): "<<(double)(end-start)/CLOCKS_PER_SEC<<endl;
   return 0;
