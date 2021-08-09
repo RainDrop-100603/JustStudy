7ddc00cb549b60175d1b7be0fba0478c9a24ceff
rain <parkkwang2@naver.com>
2021-08-09 11:27:15 +0900

    cmpDBL and vector2

--- programming_tips/algorithm/cmpDBL.cpp
+++ programming_tips/algorithm/cmpDBL.cpp
@@ -1,27 +1,23 @@
-#include "doubleControl.h"
+#include "cmpDBL.h"
 
-int doubleControl::absolute(double a, double b, double absTol){
-  if(absTol==-1){absTol=absTolerance;}
-  if(fabs(a-b)<=absTol){return 0;}
+int cmpDBL_abs(double a, double b, double absTolerance){
+  if(fabs(a-b)<=absTolerance)
+    return 0;
   return a>b ? 1 : -1;
 }
-
-int doubleControl::relevance(double a, double b, double relTol){
-  if(relTol==-1){relTol=relTolerance;}
-  if(fabs(a-b)<=max(fabs(a),fabs(b))*relTol){return 0;}
+int cmpDBL_rel(double a, double b, double relTolerance){
+  if(fabs(a-b)<=max(fabs(a),fabs(b))*relTolerance)
+    return 0;
   return a>b ? 1 : -1;
 }
-
-int doubleControl::absRel(double a, double b, double absTol, double relTol){
-  if(absTol==-1){absTol=absTolerance;}
-  if(relTol==-1){relTol=relTolerance;}
-  if(fabs(a-b)<=absTol){return 0;}
-  if(fabs(a-b)<=max(fabs(a),fabs(b))*relTol){return 0;}
+int cmpDBL_absRel(double a, double b, double absTolerance, double relTolerance){
+  if(fabs(a-b)<=absTolerance)
+    return 0;
+  if(fabs(a-b)<=max(fabs(a),fabs(b))*relTolerance)
+    return 0;
   return a>b ? 1 : -1;
 }
-
-int doubleControl::unitInTheLastPlace(double a, double b, int ulpTol){
-  if(ulpTol==-1){ulpTol=ulpTolerance;}
+int cmpDBL_ULP(double a, double b, int ulpTolerance){
   //bit를 double이 아닌 long long으로 해석하여(포인터 형변환), 두 값을 정수형태로 비교
   //  1ulp는 숫자의 크기에 따라 다르다 -> 상대범위 비교에 가깝다. 0에 가까울 경우 에러가 발생할 수 있다.
   double diff=a-b;
@@ -34,17 +30,15 @@ int doubleControl::unitInTheLastPlace(double a, double b, int ulpTol){
     return diff>0 ? 1: -1;  //부호가 다르면 비교가 easy
   }
   long long ulpsDiff=abs(na-nb); //ulps 비교
-  if(ulpsDiff<=ulpTol) 
+  if(ulpsDiff<=ulpTolerance) 
     return 0;
   return (diff>0) ? 1: -1; 
 }
-
-int doubleControl::absULP(double a, double b, double absTol, double ulpTol){
-  if(absTol==-1){absTol=absTolerance;}
-  if(ulpTol==-1){ulpTol=ulpTolerance;}
+int cmpDBL_absULP(double a, double b, double absTolerance,int ulpTolerance){
   double diff=a-b;
   //absolute
-  if(fabs(diff)<=absTol){return 0;}
+  if(fabs(diff)<=absTolerance)
+    return 0;
   //unit in the last place
   long long na=*reinterpret_cast<long long*>(&a); //a의 포인터를 long long으로 바꾸고, 그 값을 읽음 
   long long nb=*reinterpret_cast<long long*>(&b);
@@ -53,7 +47,8 @@ int doubleControl::absULP(double a, double b, double absTol, double ulpTol){
     return diff>0 ? 1: -1;  //부호가 다르면 비교가 easy
   }
   long long ulpsDiff=abs(na-nb); //ulps 비교
-  if(ulpsDiff<=ulpTol) 
+  if(ulpsDiff<=ulpTolerance) 
     return 0;
   return (diff>0) ? 1: -1; 
 }
+
---
+++ programming_tips/algorithm/cmpDBL.h
@@ -0,0 +1,31 @@
+#ifndef __CMPDBL_H__
+#define __CMPDBL_H__
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int cmpDBL_abs(double a, double b, double absTolerance=1e-10);//작은 수 비교
+int cmpDBL_rel(double a, double b, double relTolerance=__DBL_EPSILON__);//큰 수 비교
+int cmpDBL_absRel(double a, double b, double absTolerance=1e-10, double relTolerance=__DBL_EPSILON__);
+int cmpDBL_ULP(double a, double b, int ulpTolerance=4);// 큰 수 비교
+int cmpDBL_absULP(double a, double b, double absTolerance=1e-10,int ulpTolerance=4);
+
+class cmpDBLclass{
+  private:
+    double absTolerance;
+    double relTolerance;
+    int ulpTolerance;
+  public:
+    //생성자
+    cmpDBLclass(double absTol=1e-10,double relTol=__DBL_EPSILON__, int ulpTol=4):absTolerance(absTol),relTolerance(relTol), ulpTolerance(ulpTol){}
+    //-1: a<b, 0: a==b, 1: a>b
+    int absolute(double a, double b){return cmpDBL_abs(a,b,absTolerance);}
+    int relevance(double a, double b){return cmpDBL_rel(a,b,relTolerance);}
+    int absRel(double a, double b){return cmpDBL_absRel(a,b,absTolerance,relTolerance);}
+    int unitInTheLastPlace(double a, double b){return cmpDBL_ULP(a,b,ulpTolerance);}
+    int absULP(double a, double b){return cmpDBL_absULP(a,b,absTolerance,ulpTolerance);}
+};
+
+#endif
