72ee6a049701b43087e797f1ddc9f55e1cf72aa2
office <office>
2021-08-26 11:42:45 +0900

    t

--- APSS/main.cpp
+++ APSS/main.cpp
@@ -13,28 +13,58 @@
 
 using namespace std;
 
-// stack을 이용한 간단한 문제, unsigend int를 이용한 mod 처리
-void JAEHASAFE_Input(int& target,int& length){
-  cin>>target>>length;
+// KMP search 변형문제 
+void JAEHASAFE_Input(vector<string>& config){
+  int configNum;
+  cin>>configNum;
+  config.resize(configNum+1);
+  for(auto& ele: config){
+    cin>>ele;
+  }
 }
-int JAEHASAFE_Algo(int target,int length){
-  //준비 및 초기화
-  queue<int> q;
-  int qSum(0),result(0);
-  unsigned int num(1983);
-  //순회
-  for(int idx=0;idx<=length;idx++){
-    int input=num%10000+1;
-    q.push(input);
-    qSum+=input;
-    while(qSum>target){
-      qSum-=q.front();
-      q.pop();
+vector<int> JAEHASAFE_getFailure(const string& str){
+  vector<int> result(str.length()+1,0);
+  int match=0;
+  for(int idx=1;idx<str.length();idx++){
+    while(match>0&&str[idx]!=str[match]){
+      match=result[match];
     }
-    if(qSum==target){
-      result++;
+    if(str[idx]==str[match]){
+      match++;
+      result[idx+1]=match;
     }
-    num=num*214013 + 2531011;
+  }
+  return result;
+}
+vector<int> JAEHASAFE_KmpSearch(const string& base, const string& target){
+  vector<int> result;
+  vector<int> failure=JAEHASAFE_getFailure(target);
+  int match=0,strLen=base.length();
+  for(int idx=0;idx<strLen*2-1;idx++){
+    while(match>0&&base[idx%strLen]!=target[match]){
+      match=failure[match];
+    }
+    if(base[idx%strLen]==target[match]){
+      match++;
+      if(match==strLen){
+        result.push_back(idx-match+1);
+        match=failure[match];
+      }
+    }
+  }
+  return result;
+}
+int JAEHASAFE_Algo(const vector<string>& config){
+  bool clockwise(true);
+  int result(0),strLen(config[0].length()); //strlen==금고의 알파벳 개수 
+  for(int i=0;i<config.size()-1;i++){
+    auto tmp=JAEHASAFE_KmpSearch(config[i+1],config[i]);
+    if(clockwise){
+      result+=tmp.front();
+    }else{
+      result+=strLen-tmp.back();
+    }
+    clockwise=!clockwise;
   }
   return result;
 } 
@@ -66,24 +96,22 @@ void JAEHASAFE(){
   /*힌트
     같은 동물이 나온다고 찾은것이 아니라, 모든 동물의 위차가 맞아야 한다.
       따라서 idx를 하나씩 돌리며 찾으면 10000 * 10000 * 50 이 걸려서 시간내에 찾을 수 없다.
-    
+    KMP search를 이용하여 시작위치를 찾을 수 있다.
+      다이얼의 일치상태를 비교하려면 항상 n개(다이얼의 원소의 수)를 비교해야한다.
+        KMP search에서 idx를 두배로 늘리고, 넘어가는 부분은 mod 처리한다.
+      시계 방향으로 돌리는것은 KMPresult.front()번 회전
+      반시계 방향은 n - KMPresult.back()번 회전
   */
   /*전략
   전략1
-    아이디어: 스택
-      변수: queue, num(1983), queueSum(0), result(0)
-      1. for( i = [0,N] )
-        queue.push(num)
-        queueSum+=num;
-        while(queueSum>K){
-          queueSum-=queue.front
-          queue.pop
-        }
-        if(queueSum==K)
-          result+=+
-        num=number(idx,num);
+    아이디어: KMP search
+      KMP search를 M번 수행하여 시작위치를 찾는다. M * O(2N)
+        항상 N개를 비교해야하므로, idx를 넘어가는 부분은 mod 처리한다.
+        시계방향: KMP result . front
+        반시계방향: N - KMP result . back
+      
     분석
-      time complexity: O(N)
+      time complexity: O(MN) <= 50 * 2 * 10000 
       mem complexity: O(N)
     피드백
   */
@@ -95,9 +123,9 @@ void JAEHASAFE(){
   cout.precision(10);
   //각 테스트케이스
   while(testCase--){
-    int target,length;
-    JAEHASAFE_Input(target,length);
-    auto result=JAEHASAFE_Algo(target,length);
+    vector<string> config;
+    JAEHASAFE_Input(config);
+    auto result=JAEHASAFE_Algo(config);
     // cout<<"::::";
     cout<<result<<endl;
   }
