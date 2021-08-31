e8787421ac5edf9c322a55ff793ec8a396f2fbdd
Rain <parkkwang2@naver.com>
2021-09-01 00:44:57 +0900

    challenge2

--- nomad-coder/airbnb-clone/users/admin.py
+++ nomad-coder/airbnb-clone/users/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Field",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                )
+            },
+        ),
+    )
+
+    # list_display = ("username", "gender", "language", "currency", "superhost")
+    # list_filter = ("superhost", "currency", "language")
--- nomad-coder/airbnb-clone/users/models.py
+++ nomad-coder/airbnb-clone/users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    """custom user model"""
+    """Custom User Model"""
 
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
@@ -17,6 +17,29 @@ class User(AbstractUser):
         (GENDER_OTHERS, "Other"),
     )
 
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_ENGLISH = "en"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_ENGLISH, "English"),
+    )
+
+    CURRENCY_KRW = "krw"
+    CURRENCY_USD = "usd"
+
+    CURRENCY_CHOICES = ((CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD"))
+
     avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.TextField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.TextField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
