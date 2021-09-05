295af9fa8432cf32e4cc6ae340cdc42618dfb0de
Rain <parkkwang2@naver.com>
2021-09-05 20:08:26 +0900

    t

--- nomad-coder/airbnb-clone/config/settings.py
+++ nomad-coder/airbnb-clone/config/settings.py
@@ -46,10 +46,12 @@ PROJECT_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
 ]
 
 
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
