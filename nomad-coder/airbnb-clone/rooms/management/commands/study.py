ba4baaf31a581854dbff94244d05ca30e58b0536
office <office>
2021-09-07 11:37:51 +0900

    t

---
+++ nomad-coder/airbnb-clone/rooms/management/commands/study.py
@@ -0,0 +1,14 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+
+
+class Command(BaseCommand):
+    help = "for study"
+
+    def add_arguments(self, parser):
+        parser.add_a
+        return super().add_arguments(parser)
+
+    def handle(self, *args, **options):
+        return super().handle(*args, **options)
