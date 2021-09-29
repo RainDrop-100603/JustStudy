948c6a44e824406c7e09493132641f6515b611c6
Rain <parkkwang2@naver.com>
2021-09-29 17:36:20 +0900

    t

--- my_program/todo_calendar/config/urls.py
+++ my_program/todo_calendar/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("calendar/", include("calendars.urls", namespace="calendars")),
+    path("calendars/", include("calendars.urls", namespace="calendars")),
 ]
