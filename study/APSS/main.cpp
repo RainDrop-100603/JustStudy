b6679ba6f8450aa91308e55d5675c6e52ddf67e7
office <office>
2021-09-30 17:57:37 +0900

    t

--- study/APSS/main.cpp
+++ study/APSS/main.cpp
@@ -12,114 +12,130 @@
 
 using namespace std;
 
-// suffix arr,
-void HABIT_Input(string& speech, int& isHabit) { cin >> isHabit >> speech; }
-struct HABIT_Comparator {  // suffixArr에서의 정렬에 사용한다. 각 접미사가 t만큼 비교되어 있을 때, 2t만큼 비교한다.
-    const vector<int>& group;
-    int t;
-    HABIT_Comparator(const vector<int>& _group, int _t) : group(_group), t(_t) {}
-    bool operator()(int a, int b) {
-        if (group[a] != group[b]) {
-            return group[a] < group[b];
-        }
-        return group[a + t] < group[b + t];
+// @* tree, preorder, inorder, postorder, 적절한 재귀를 통해, tree를 구현하지 않고도 문제를 풀어냈다.
+void FORTRESS_Input(int& fortressNum, vector<int>& fortressXpos, vector<int>& fortressYpos, vector<int>& fortressRadius) {
+    cin >> fortressNum;
+    fortressXpos.resize(fortressNum);
+    for (auto& ele : fortressXpos) {
+        cin >> ele;
     }
-};
-vector<int> HABIT_suffixArr(const string& str) {
-    int strLen = str.length();
-    // group[i] = order, str[i:] 의 order
-    vector<int> group(strLen + 1);  // comparator 연산시 group[strLen]까지 접근한다.
-    group[strLen] = -1;             //문자열 a와 a+b는 사전순으로 a가 더 앞에 온다.
-    for (int i = 0; i < strLen; i++) {
-        // ASCII CODE 자체가 order라고 봐도 무방하다.
-        group[i] = str[i];
+    fortressYpos.resize(fortressNum);
+    for (auto& ele : fortressYpos) {
+        cin >> ele;
     }
-    // perm[order]=i, str[i:]의 order
-    vector<int> perm(strLen);
-    for (int i = 0; i < strLen; i++) {
-        perm[i] = i;
+    fortressRadius.resize(fortressNum);
+    for (auto& ele : fortressRadius) {
+        cin >> ele;
     }
-    //정렬
-    int t = 1;
-    while (t < strLen) {
-        // 각 접미사의 2t만큼의 접두사를 비교하여 정렬한다.
-        HABIT_Comparator cmp2T(group, t);
-        sort(perm.begin(), perm.end(), cmp2T);
-        t *= 2;
-        if (t >= strLen) {
-            break;
-        }
-        // group 갱신
-        vector<int> tmpGroup(strLen);
-        int prev = perm[0];
-        tmpGroup[prev] = 0;
-        for (int i = 1; i < strLen; i++) {
-            int now = perm[i];
-            if (cmp2T(prev, now)) {
-                tmpGroup[now] = tmpGroup[prev] + 1;
-            } else {
-                tmpGroup[now] = tmpGroup[prev];
+}
+int FORTRESS_isSubtree(vector<int>& fortressXpos, vector<int>& fortressYpos, vector<int>& fortressRadius, int root, int idx) {
+    int rx(fortressXpos[root]), ry(fortressYpos[root]), rr(fortressRadius[root]);
+    int nx(fortressXpos[idx]), ny(fortressYpos[idx]), nr(fortressRadius[idx]);
+    double distance = sqrt(pow(rx - nx, 2) + pow(ry - ny, 2));
+    //포함하는가, 포함되는가를 따진다.
+    int contain = 1;
+    if (nr > rr) {
+        rr = nr;
+        contain = -1;
+    }
+    //계산
+    if (distance < rr) {
+        return contain;
+    } else {
+        return 0;
+    }
+}
+vector<int> FORTRESS_getHeights(vector<int>& fortressXpos, vector<int>& fortressYpos, vector<int>& fortressRadius, vector<int>& tree) {
+    //기저
+    if (tree.size() == 0) {
+        return vector<int>(1, -1);
+    }
+    // subtree 구분하기
+    int rootIdx = tree[0];
+    vector<vector<int>> subtrees;
+    for (int idx = 1; idx < subtrees.size(); idx++) {
+        bool inserted(false);
+        //각 subtree에 포함되는지 확인
+        for (auto& subtree : subtrees) {
+            int root = subtree.front();
+            int is_subtree = FORTRESS_isSubtree(fortressXpos, fortressYpos, fortressRadius, root, idx);
+            if (is_subtree == 1) {
+                subtree.push_back(idx);
+                inserted = true;
+                break;
+            } else if (is_subtree == -1) {
+                subtree.insert(subtree.begin(), idx);
+                inserted = true;
+                break;
             }
-            prev = now;
         }
-        group = tmpGroup;
+        //어느 subtree에도 포함되지 않는다면, 새로운 subtree(child)이다.
+        if (!inserted) {
+            subtrees.push_back(vector<int>(1, idx));
+        }
     }
-    return perm;
-}
-int HABIT_commonprefix(const string& str, int begin1, int begin2) {
-    int len = 0;
-    while (begin1 < int(str.length()) && begin2 < int(str.length()) && str[begin1] == str[begin2]) {
-        len++;
-        begin1++;
-        begin2++;
+    //재귀하기
+    vector<int> heights;
+    for (auto& subtree : subtrees) {
+        auto height = FORTRESS_getHeights(fortressXpos, fortressYpos, fortressRadius, subtree);
+        heights.push_back(height[0] + 1);
     }
-    return len;
+    sort(heights.begin(), heights.end(), greater<>());
+    return heights;
 }
-int HABIT_Algo(string speech, int isHabit) {
-    int maxHabit = 0;
-    vector<int> suffixArr = HABIT_suffixArr(speech);
-    for (int i = 0; i + isHabit <= speech.length(); i++) {
-        maxHabit = max(maxHabit, HABIT_commonprefix(speech, suffixArr[i], suffixArr[i + isHabit - 1]));
+int FORTRESS_Algo(int fortressNum, vector<int> fortressXpos, vector<int> fortressYpos, vector<int> fortressRadius) {
+    vector<int> subtree(fortressNum);
+    for (int i = 0; i < fortressNum; i++) {
+        subtree[i] = i;
     }
-    return maxHabit;
+    auto heights = FORTRESS_getHeights(fortressXpos, fortressYpos, fortressRadius, subtree);
+    if (heights.size() == 1) {
+        return heights[0] + 1;  //외벽 밖으로 나갈 수 있다.
+    } else {
+        return heights[0] + heights[1];
+    }
 }
-void HABIT() {
+void FORTRESS() {
     /*설명 및 입력
     설명
-        정박사가 지금까지 했던 발표들과 강의들에서 했던 말을 모두 음성 인식을 통해 대본으로 만들었습니다.
-        이 때 대본 중 K 번 이상 등장하는 부분 문자열을 "말버릇" 이라고 합시다.
-        대본이 주어질 때 가장 긴 말버릇의 길이를 찾는 프로그램을 작성하세요.
+        성벽들의 정보가 주어질 때 가장 성벽을 많이 넘어야 하는 두 지점 간을 이동하기 위해 몇 번이나 성벽을 넘어야 하는지 계산하는 프로그램을 작성하세요.
     입력
-        입력의 첫 줄에는 테스트 케이스의 수 C (1 <= C <= 50) 가 주어집니다.
-        각 테스트 케이스는 두 줄로 주어지며, 첫 줄에는 K (1 <= K <= 1000) 가, 두 번째 줄에는 정박사의 발표 대본이 주어집니다.
-        대본은 공백 없이 알파벳 소문자로만 구성된 문자열이며 길이는 1 이상이고 4000 을 넘지 않습니다.
+        입력의 첫 줄에는 테스트 케이스의 수 C (1 <= C <= 100) 가 주어집니다.
+        각 테스트 케이스의 첫 줄에는 성벽의 수 N (1 <= N <= 100) 이 주어집니다.
+        그 후 N 줄에는 각 3개의 정수로 각 성벽의 위치와 크기에 대한 정보 xi , yi , ri 가 주어집니다.
+            (0 <= xi, yi <= 1000,1 <= ri <= 1000,0 <= i<N) 이 때 i 번 성벽은 (xi, yi) 를 중심으로 하는 반지름 ri 인 원형으로 설치되어 있습니다.
+        편의상 모든 성벽의 두께는 0이라고 가정하며, 입력에 주어지는 성벽들은 서로 겹치거나 닿지 않습니다.
+        입력에 주어지는 첫 번째 성벽은 외벽이며, 외벽은 입력에 주어지는 모든 다른 성벽을 포함합니다.
     출력
-        각 테스트 케이스마다 K 번 이상 등장하는 부분 문자열의 최대 길이를 출력합니다.
-        어떤 부분 문자열도 K 번 이상 등장하지 않는다면 0 을 출력합니다.
+        각 테스트 케이스마다 한 줄에 두 지점 간 이동을 위해 최대 몇 번이나 성벽을 넘어야 하는지를 출력하세요.
     제한조건
         1초, 64MB
     */
     /*힌트
-         suffix arr의 접두사를 이용하여, 모든 부분문자열을 구할 수 있다.
-            이때 중복등장하는 접두사들은, 접두사들의 suffix가 모두 이웃한다는 점을 이용할 수 있다.
-         이웃하는 suffix가 겹치는 정도를 모두 구하고, stack을 통해 K번 이상 등장한 문자열들의 길이를 구할 수 있다.
-            algospot - fence를 생각하자. 같은 원리이다.
+        각 child를 root로하는 subtree의 원소를 알 수 있다면, 문제를 재귀적으로 풀어나갈 수 있다.
+            만약 성벽이 기존의 child에 포함되거나/포함한다면, 기존 child와 같은 subtree에 존재한다.
+            만약 성벽이 기존의 child중 어느것에도 포함되지 않는다면, 이것은 새로운 child, 즉 새로운 subtree를 생성한다.
+        성벽은 겹치지 않으므로, 한 subtree의 root는 반지름이 가장 큰 것임을 알 수 있다.
+        재귀적 풀이: subtree의 모든 child의 height를 반환하면 된다.
     */
     /*전략
     전략1
-        아이디어: suffix arr, stack
-            suffix arr을 구한다. -> n (lgn)^2
-            suffix arr을 순회하며 -> n * O(n)
-                이웃하는 suffix arr의 겹치는 길이를 구한다. -> O(n)
-                stack을 이용하여 각 부분문자열의 최대길이를 구하자. -> O(1)
+        아이디어: tree, 재귀
+            root를 구한다 -> O(1)
+            subtrees를 구분하는 2차원 배열을 만든다.
+                subtrees[i] = i 번째 child를 root로 가지는 subtree의 원소들
+            성벽을 기존의 children과 비교한다 -> O(n^2)
+                기존의 child를 포함하거나/포함된다면, subtrees[i].push_back(now);
+                기존의 child와 관계가 없다면, subtrees.push_back(vector<int>(now));
+            heights를 나타내는 배열을 만든다.
+            각 children에 대해 재귀하여 subtree들의 height를 구한다. -> n times * O(n^2)
+                heights[i] = max(child_subtree_heights)+1
+            return heights
+            마지막으로 반환된 값을 가지고, 가장 큰 값과 그다음 큰 값을 더하면 된다.
         분석
-            time complexity: O(n^2)
+            time complexity: O(n^3)
             mem complexity: O(N)
         피드백
-            최초: 368
-            ver2: substr을 구하지 않도록 개선, 304
-            ver3: stack도 이용할 필요가 없다. 284
-                suffixarr[i]와 suffixarr[i+k-1]의 commonPreifx는, suffixarr[i+1 ~ i+k-2]의 commonPrefix이다.
     */
     // Sol
     int testCase;
@@ -129,10 +145,10 @@ void HABIT() {
     cout.precision(10);
     //각 테스트케이스
     while (testCase--) {
-        string speech;
-        int isHabit;
-        HABIT_Input(speech, isHabit);
-        auto result = HABIT_Algo(speech, isHabit);
+        int fortressNum;
+        vector<int> fortressXpos, fortressYpos, fortressRadius;
+        FORTRESS_Input(fortressNum, fortressXpos, fortressYpos, fortressRadius);
+        auto result = FORTRESS_Algo(fortressNum, fortressXpos, fortressYpos, fortressRadius);
         // cout << "::::";
         cout << result << endl;
     }
@@ -141,7 +157,7 @@ void HABIT() {
 int main(void) {
     //   clock_t start,end;
     //   start=clock();
-    HABIT();
+    FORTRESS();
     //   end=clock();;
     //   cout<<"time(s): "<<(double)(end-start)/CLOCKS_PER_SEC<<endl;
     return 0;
