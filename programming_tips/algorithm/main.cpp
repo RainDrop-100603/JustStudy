21c3c512405819fe421509e4cb12dcff6ab4c305
Rain <parkkwang2@naver.com>
2021-10-08 15:15:14 +0900

    t

--- programming_tips/algorithm/main.cpp
+++ programming_tips/algorithm/main.cpp
@@ -11,6 +11,16 @@ int main() {
         tree.insert(i);
     }
     tree.printTree();
-
+    cout << tree.findKth(10) << endl;
+    cout << tree.findKth(11) << endl;
+    cout << tree.find(10) << endl;
+    cout << tree.find(9) << endl;
+    cout << tree.countLessThan(9) << endl;
+    cout << tree.countLessThan(10) << endl;
+    for (int i = 0; i < 10; i++) {
+        tree.erase(i);
+        cout << tree.size() << endl;
+    }
+    tree.printTree();
     return 0;
 }
