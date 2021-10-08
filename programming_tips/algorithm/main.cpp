a61b2fb9670da73397716ae4dec7276180eccd25
Rain <parkkwang2@naver.com>
2021-10-08 16:21:58 +0900

    t

--- programming_tips/algorithm/main.cpp
+++ programming_tips/algorithm/main.cpp
@@ -10,17 +10,18 @@ int main() {
     for (int i = 0; i < 10; i++) {
         tree.insert(i);
     }
-    tree.printTree();
-    cout << tree.findKth(10) << endl;
-    cout << tree.findKth(11) << endl;
-    cout << tree.find(10) << endl;
-    cout << tree.find(9) << endl;
+    cout << tree.kthNode(9) << endl;
+    cout << tree.kthNode(10) << endl;
+    cout << tree.isExist(10) << endl;
+    cout << tree.isExist(9) << endl;
     cout << tree.countLessThan(9) << endl;
     cout << tree.countLessThan(10) << endl;
+    cout << tree.size() << endl;
+    cout << tree.min() << endl;
+    cout << tree.max() << endl;
     for (int i = 0; i < 10; i++) {
         tree.erase(i);
-        cout << tree.size() << endl;
+        cout << "erase: " << i << endl;
     }
-    tree.printTree();
     return 0;
 }
