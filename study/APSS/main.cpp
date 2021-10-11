13a138fe304066b1604a75488432706f90be2554
Rain <parkkwang2@naver.com>
2021-10-12 08:42:09 +0900

    t

--- study/APSS/main.cpp
+++ study/APSS/main.cpp
@@ -13,7 +13,77 @@
 using namespace std;
 
 // @*@*@* binary search tree(map), 시각화를 통해 문제해결의 힌트를 얻을 수 있다.
-void NERD2_Input(int& applicantsNum, vector<int>& arg1Arr, vector<int>& arg2Arr) {
+class INSERTION_node {
+   public:
+    int key, size;
+    INSERTION_node *left, *right;
+    INSERTION_node(int _key) : key(_key), size(1), left(NULL), right(NULL) {}
+    void setLeft(INSERTION_node* node) {
+        left = node;
+        calcSize();
+    }
+    void setRight(INSERTION_node* node) {
+        right = node;
+        calcSize();
+    }
+    void calcSize() {
+        size = 1;
+        if (left) size += left->size;
+        if (right) size += right->size;
+    }
+};
+class INSERTION_treap {
+    INSERTION_node* root;
+    INSERTION_node* makeBalanceTree(int size, int now) {
+        if (now >= size) {
+            return NULL;
+        }
+        auto node = new INSERTION_node(now);
+        node->setLeft(makeBalanceTree(size, now * 2 + 1));
+        node->setRight(makeBalanceTree(size, now * 2 + 2));
+        return node;
+    }
+    void setBalanceTree() {
+        for (int i = 0; i < root->size; i++) {
+            find(i)->key = i;
+        }
+    }
+    INSERTION_node* find(int idx) {
+        auto node = root;
+        while (true) {
+            int nodeIdx = node->left->size;
+            if (idx == nodeIdx) {
+                return node;
+            } else if (idx < nodeIdx) {
+                node = node->left;
+            } else {
+                node = node->right;
+                idx -= nodeIdx;
+            }
+        }
+    }
+
+   public:
+    INSERTION_treap(int size) : root(NULL) {
+        if (size > 0) {
+            root = new INSERTION_node(0);
+            makeBalanceTree(size, 0);
+            setBalanceTree();
+        }
+    }
+    int kthNode(int idx) {
+        if (root == NULL) return -1;
+        return find(idx)->key;
+    }
+    void moveNode(int from, int to) {
+        auto fromNode = find(from);
+        auto toNode = find(to);
+        if (to == 0) {
+        }
+        auto toNodeParent = find(to / 2);
+    }
+};
+void INSERTION_Input(int& applicantsNum, vector<int>& arg1Arr, vector<int>& arg2Arr) {
     cin >> applicantsNum;
     arg1Arr.resize(applicantsNum);
     arg2Arr.resize(applicantsNum);
@@ -21,7 +91,7 @@ void NERD2_Input(int& applicantsNum, vector<int>& arg1Arr, vector<int>& arg2Arr)
         cin >> arg1Arr[i] >> arg2Arr[i];
     }
 }
-int NERD2_Algo(int applicantsNum, vector<int> arg1Arr, vector<int> arg2Arr) {
+int INSERTION_Algo(int applicantsNum, vector<int> arg1Arr, vector<int> arg2Arr) {
     // tree: map을 생성한다
     map<int, int> tree;
     //모든 입력에 대해 순차적으로 처리한다, tree가 빈 경우는 맨 처음밖에 없으므로 미리 처리
@@ -52,63 +122,55 @@ int NERD2_Algo(int applicantsNum, vector<int> arg1Arr, vector<int> arg2Arr) {
     }
     return result;
 }
-void NERD2() {
+void INSERTION() {
     /*설명 및 입력
     설명
-        올해에도 참가 신청을 한 사람 중 진정한 프로그래밍 너드들만을 대회에 참가할 수 있도록 받아 주기로 했습니다.
-        종만의 새로운 이론에 따르면, 어떤 사람의 너드 지수는 다음 두 가지 값에 의해 결정됩니다.
-            알고스팟 온라인 채점 시스템에서 푼 문제의 수 p
-            밤 새면서 지금까지 끓여먹은 라면 그릇 수 q
-        이 이론에 따르면 어떤 참가 신청자 a 의 문제 수 pa 와 그릇 수 qa 를 다른 참가 신청자 b 의 문제 수 pb 와 그릇 수 qb 에 각각 비교했을 때,
-            pa < pb, qa < qb 라면 참가 신청자 a 는 너드일 가능성이 없습니다.
-            조직위는 너드일 가능성이 있는 사람들만을 대회에 받아주기로 했습니다.
-        한 사람의 참가 가능 여부는 다른 사람들에 의해 결정되기 때문에, 대회에 참가할 수 있는 사람의 수는 새로운 사람이 참가 신청을 할 때마다 계속 바뀝니다.
-        이렇게 각 사람이 참가 신청을 할 때마다 대회에 참가할 수 있는 사람들의 수가 어떻게 변하는지 계산하는 프로그램을 작성하세요.
+        1부터 N까지의 자연수가 한 번씩 포함된 길이 N 인 수열 A[] 를 삽입 정렬했습니다.
+        원래 수열은 알 수 없지만, 그 과정에서 각 원소가 왼쪽으로 몇 칸이나 이동했는지를 알고 있습니다.
+            예를 들어, 5 1 4 3 2 수열이 움직인 칸수를 표현하면 {0,1,1,2,3} 이 됩니다.
+        이 때 원래 수열을 찾아내는 프로그램을 작성하세요.
     입력
         입력의 첫 줄에는 테스트 케이스의 수 C (1 <= C <= 50) 가 주어집니다.
-        각 테스트 케이스의 첫 줄에는 참가 신청한 사람들의 수 N (1 <= N <= 50000) 이 주어집니다.
-        그 후 N 줄에 각 2개의 정수로 각 사람의 문제 수 pi 와 라면 그릇 수 qi 가 참가 신청한 순서대로 주어집니다 (0 <= pi,qi <= 100000) .
-        두 사람의 문제 수나 라면 그릇 수가 같은 경우는 없다고 가정해도 좋습니다.
-        입력의 양이 많으므로 가능한 빠른 입력 함수를 사용하는 것이 좋습니다..
+        각 테스트 케이스의 첫 줄에는 원 배열의 길이 N (1 <= N <= 50000) 이 주어집니다.
+        그 다음 줄에 N 개의 정수로 A의 각 위치에 있던 값들이 움직인 칸수가 주어집니다.
+        A 는 1부터 N 까지의 정수를 한 번씩 포함합니다.
     출력
-        각 사람이 참가 신청을 할 때마다 대회 참가 자격이 되는 사람의 수를 계산한 뒤, 각 테스트 케이스마다 그 합을 출력합니다.
+        각 테스트 케이스마다 재구성한 A[] 를 한 줄에 출력합니다.
     제한조건
         2초, 64MB
     */
     /*힌트
-        너드는, 다른 모든 사람과 비교했을 때, p 혹은 q 가 커야한다는 특징이 있다.
-        방법1: p와 q를 각각 tree형태로 저장하고, p와 q 모두 자기보다 큰 사람을 배열에 저장한다.
-                idx번째 입력까지의 결과는, 자기보다 큰 사람을 idx번째 까지의 사람만 인정하는것으로 계산하면 된다.
-            예상되는 시간: 비교시간(O(n)) * 모든사람(n times) + 트리구성(O(nlgn)) + 너드계산(O(n^2)) = O(n^2)
-            문제점: n<=50000이기 때문에 n^2으로는 풀 수 없다.
-        사람이 n명이기 때문에, 각각의 사람에 대해 검사하려면 lgn 시간에 검사를 끝내야한다.
-        방법2: 각각의 사람마다 두가지 값을 시간내에 계산하는것은 불가능하다. 즉, 하나의 값은 미리 계산해놔야 한다는 것이다.
-            p에대해 내림차순 정렬하고, 순서대로 q값을 tree input한다.
-            앞서 tree에 들어간 값들은 모두 now보다 p가 크므로, now는 q가 제일커야, tree에서 제일 커야 nerd가 될 수 있다.
-            정렬시간(nlgn) + tree생성 시간(nlgn) = O(nlgn) 으로 해결이 가능하다.
-            문제점: 참가자가 지원한 순서대로 심사되어야 한다.
-        책 참고 힌트: p(가로)와 q(세로)를 축으로 하여 좌표평면위에 나타내보자.
-            nerd에 해당하는 점들만을 표시하면, 해당 점들은 p값이 커질수록 q값이 작아짐을 알 수 있다.
-            즉, nerd에 해당하는 점들은 정리되어 있으므로 이를 참고할 수 있다.
-        방법3: tree(map)에 유효한 값들만 저장한다. pair(p,q)
-            insert & delete:
-                lower_bound를 통해 p값에 해당하는 위치를 찾는다. next= lower_bound(input), prev = next --
-                    q > prev_q: q<prev_q 혹은 prev가 없을 때 까지, prev를 delete한다.
-                    q > next_q: insert
-                    q < next_q: 아무것도 안한다.(nerd가 아니다)
-                매 동작마다 size를 통해 nerd의 수를 구할 수 있다.
+        입력이 크므로, 빠른 입력을 이용한다.
+        한번 움직인것은, 왼쪽으로 한칸 이동한 것이다.
+            즉 가상의 행렬을 만들어서 이를 움직이고, 움직인 결과가 insertion sort의 결과임을 이용하여, A[]를 재구성한다
+                예: ABCDE를 움직이면 BEDCA = 12345 이므로, ABCDE = 51432 이다.
+            문제점: 매번 최대 n time움직이면, 전체 시간은 O(n^2)이 된다.
+            특정한 방법을 이용하여 lgn시간내에 목적지를 찾아 한번에 이동하면 O(nlgn)으로 풀 수 있다.
+        custom tree를 이용하는 방법
+            초기상태: n번째 위치에 있는 node의 key는 n 이다.
+                n개의 node를 갖는 balance tree를 만들어서, n번째 node의 key를 n으로 대체한다.
+                balance tree 생성법
+                    root에서부터 높은 depth로 내려가면서
+                        차례대로 왼쪽에서 부터 순서를 센다고 가정하고 (0, 12, 34 56 과 같은식)
+                    n번째 node의 child는 각각 2n+1, 2n+2 번째 node다.
+                    재귀적으로, child가 범위 안에 들어오면 추가하고, 아니면 null을 추가한다.
+            특징: key로 정렬하지 않고, 임의로 정렬한다.
+            k번째 node가 어디에 있는지 알 수 있다.
+            원하는 n번째 위치에 input이 가능하다. 단, 더 큰 위치에서 더 작은 위치로만 가능하다.
+                    (더 작은 위치로도 약간만 생각하면 가능하다)
+                n번째 위치의 값을 input으로 대체한다.
+                기존에 있던 prev는 input의 right child으로 대체한다.
+            tree를 완성하고 나서, 트리를 0~N-1 번째 까지 순회한다.
+                0번째 값이 k라면, 0의 최초 위치는 k번째라는 뜻이다.-
+        n^2 time
+            arr, linked list,
     */
     /*전략
-        아이디어: tree(map), 시각화를 통한 문제의 특성 이해
-            입력을 순차적으로 처리한다. 입력 = (p,q)
-                delete
-                    prevIter = --lower_bound(p)
-                    while q > prevIter.q -> erase prevIter
-                insert
-                    nextIter = lower_bound(p)
-                    if nextIter == tree.end || q > nextIter.q
-                        insert (입력)
-                tree.size()를 구하여 정답 계산
+        아이디어: custom tree
+            custom tree를 만든다.
+
+
+            custom tree를 순회하며, 적절한 위치에 값을 넣어서 A[]를 복원한다.
         분석
             time complexity: O(nlgn)
                 입력처리 : 반복 n회
@@ -129,8 +191,8 @@ void NERD2() {
     while (testCase--) {
         int applicantsNum;
         vector<int> arg1Arr, arg2Arr;
-        NERD2_Input(applicantsNum, arg1Arr, arg2Arr);
-        auto result = NERD2_Algo(applicantsNum, arg1Arr, arg2Arr);
+        INSERTION_Input(applicantsNum, arg1Arr, arg2Arr);
+        auto result = INSERTION_Algo(applicantsNum, arg1Arr, arg2Arr);
         // cout << "::::";
         cout << result << endl;
     }
@@ -139,7 +201,7 @@ void NERD2() {
 int main(void) {
     // clock_t start,end;
     // start=clock();
-    NERD2();
+    INSERTION();
     // end=clock();;
     // cout<<"time(s): "<<(double)(end-start)/CLOCKS_PER_SEC<<endl;
     return 0;
