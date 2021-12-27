4121e3a7d375c9bfe2602f6092fb0b9abef95528
Rain <parkkwang2@naver.com>
2021-12-28 00:19:29 +0900

    solong 초안

---
+++ study/APSS/classes.h
@@ -0,0 +1,251 @@
+#ifndef __CLASSES_H__
+#define __CLASSES_H__
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class treap_node {
+   public:
+    int key, size, priority;
+    treap_node *left, *right;
+    treap_node(int _key) : key(_key), size(1), priority(rand()), left(NULL), right(NULL) {}
+    void setLeft(treap_node* node) {
+        left = node;
+        calcSize();
+    }
+    void setRight(treap_node* node) {
+        right = node;
+        calcSize();
+    }
+    void calcSize() {
+        size = 1;
+        if (left) size += left->size;
+        if (right) size += right->size;
+    }
+};
+class treap {
+    treap_node* root;
+    // organize tree
+    treap_node* insert(treap_node* node, treap_node* input) {
+        if (node == NULL) {
+            return input;
+        }
+        //
+        if (input->priority < node->priority) {
+            if (input->key < node->key) {
+                node->setLeft(insert(node->left, input));
+            } else {
+                node->setRight(insert(node->right, input));
+            }
+            return node;
+        }
+        //
+        auto splitted = split(node, input->key);
+        input->setLeft(splitted.first);
+        input->setRight(splitted.second);
+        return input;
+    }
+    treap_node* erase(treap_node* node, int key) {
+        if (key == node->key) {
+            return merge(node->left, node->right);
+        } else if (key < node->key) {
+            node->setLeft(erase(node->left, key));
+            return node;
+        } else {
+            node->setRight(erase(node->right, key));
+            return node;
+        }
+    }
+    pair<treap_node*, treap_node*> split(treap_node* node, int key) {
+        pair<treap_node*, treap_node*> result{NULL, NULL};
+        if (node != NULL) {
+            if (key < node->key) {
+                auto splitted = split(node->left, key);
+                node->setLeft(splitted.second);
+                result.first = splitted.first;
+                result.second = node;
+            } else {
+                auto splitted = split(node->right, key);
+                node->setRight(splitted.first);
+                result.first = node;
+                result.second = splitted.second;
+            }
+        }
+        return result;
+    }
+    treap_node* merge(treap_node* leftNode, treap_node* rightNode) {
+        if (leftNode == NULL) return rightNode;
+        if (rightNode == NULL) return leftNode;
+        //
+        if (leftNode->priority > rightNode->priority) {
+            leftNode->setRight(merge(leftNode->right, rightNode));
+            return leftNode;
+        } else {
+            rightNode->setLeft(merge(leftNode, rightNode->left));
+            return rightNode;
+        }
+    }
+    // use tree
+    treap_node* find(int key) {
+        auto node = root;
+        while (node != NULL) {
+            if (key == node->key) {
+                return node;
+            } else if (key < node->key) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+        }
+        return NULL;
+    }
+    treap_node* kthNode(int idx) {
+        idx++;  // idx begin from 1, instead of 0
+        if (root == NULL || root->size < idx) return NULL;
+        auto node = root;
+        while (true) {
+            int nodeIdx = 1;
+            if (node->left) nodeIdx += node->left->size;
+            if (idx == nodeIdx) {
+                return node;
+            } else if (idx < nodeIdx) {
+                node = node->left;
+            } else {
+                node = node->right;
+                idx -= nodeIdx;
+            }
+        }
+    }
+    // erase tree
+    void clean(treap_node* node) {
+        if (node->left) clean(node->left);
+        if (node->right) clean(node->right);
+        delete node;
+    }
+
+   public:
+    treap() : root(NULL) {}
+    ~treap() {
+        if (root) clean(root);
+    }
+    void insert(int key) {
+        if (!isExist(key)) {
+            root = insert(root, new treap_node(key));
+        }
+    }
+    void erase(int key) {
+        if (isExist(key)) {
+            auto target = find(key);
+            root = erase(root, key);
+            delete target;
+        }
+    }
+    bool isExist(int key) { return find(key) ? true : false; }
+    int kthKey(int idx) {
+        if (auto ret = kthNode(idx)) {
+            return ret->key;
+        } else {
+            cout << idx << " is larger than treap size" << endl;
+            return -123456789;
+        }
+    }
+    int operator[](int idx) { return kthKey(idx); }
+    bool empty() { return root == NULL; }
+    int size() { return root ? root->size : 0; }
+};
+
+class fenwicktree {
+    vector<int> tree;
+    int size;
+
+   public:
+    fenwicktree(int _size) : tree(_size + 1), size(_size) {}
+    void add(int pos, int value) {
+        pos++;  // idx starts from 1
+        while (pos <= size) {
+            tree[pos] += value;
+            pos += (pos & -pos);  // add pos' last bit
+        }
+    }
+    int pSum(int pos) {
+        pos++;
+        int result = 0;
+        while (pos > 0) {
+            result += tree[pos];
+            pos -= (pos & -pos);
+        }
+        return result;
+    }
+};
+
+class RangeMinimunQuery {
+    vector<int> rangeMin;
+    int capacity, size;
+    int USELESS = INT32_MAX;
+    int init(vector<int>& inputArr, int node, int left, int right) {
+        if (left == right) {
+            return rangeMin[node] = inputArr[left];
+        }
+        int leftMin = init(inputArr, node * 2 + 1, left, (left + right) / 2);
+        int rightMin = init(inputArr, node * 2 + 2, (left + right) / 2 + 1, right);
+        return rangeMin[node] = min(leftMin, rightMin);
+    }
+    int query(int node, int left, int right, int rangeLeft, int rangeRight) {
+        // query 범위 밖
+        if (rangeRight < left || right < rangeLeft) return USELESS;
+        // query 범위에 완전히 포함
+        if (rangeLeft <= left && right <= rangeRight) return rangeMin[node];
+        // query 범위에 일부 포함
+        int leftQuery = query(node * 2 + 1, left, (left + right) / 2, rangeLeft, rangeRight);
+        int rightQuery = query(node * 2 + 2, (left + right) / 2 + 1, right, rangeLeft, rangeRight);
+        return min(leftQuery, rightQuery);
+    }
+    int update(int node, int left, int right, int idx, int value) {
+        if (idx < left || right < idx) return USELESS;
+        if (left == right) {
+            return rangeMin[node] = value;
+        }
+        int leftMin = update(node * 2 + 1, left, (left + right) / 2, idx, value);
+        int rightMin = update(node * 2 + 2, (left + right) / 2 + 1, right, idx, value);
+        return rangeMin[node] = min(leftMin, rightMin);
+    }
+
+   public:
+    RangeMinimunQuery(vector<int>& inputArr) {
+        capacity = pow(2, ceil(log(inputArr.size()) / log(2)));
+        size = inputArr.size();
+        rangeMin.resize(capacity * 2);
+        init(inputArr, 0, 0, size - 1);
+    }
+    int query(int left, int right) { return query(0, 0, size - 1, left, right); }
+    bool update(int idx, int value) {
+        if (idx >= size) return false;
+        update(0, 0, size - 1, idx, value);
+        return true;
+    }
+    bool push(int value) {
+        if (size == capacity) return false;
+        size++;
+        update(0, 0, size - 1, size - 1, value);
+        return true;
+    }
+    bool pop() {
+        if (size == 0) return false;
+        update(0, 0, size - 1, size - 1, USELESS);
+        size--;
+        return true;
+    }
+};
+
+class TrieNode {
+    //  if you want to change input_char_range,
+    //  change "TrieNode* children", "func: int toIdx()"
+    TrieNode* children[26];
+    bool terminal;
+    TrieNode() : terminal(false) {}
+    ~TrieNode() { for () }
+};
+
+#endif
--- study/APSS/main.cpp
+++ study/APSS/main.cpp
@@ -10,169 +10,121 @@
 #include <string>
 #include <vector>
 
+#include "classes.h"
+
 using namespace std;
 
-void EDITORWARS_Input(int& peopleNum, int& commentNum, vector<int>& commentACK, vector<pair<int, int>>& commentElements) {
-    cin >> peopleNum >> commentNum;
-    commentACK.resize(commentNum);
-    commentElements.resize(commentNum);
-    for (int i = 0; i < commentNum; i++) {
-        char tmpType[3];
-        scanf("%s %d %d", tmpType, &commentElements[i].first, &commentElements[i].second);
-        if (tmpType[0] == 'A') {
-            commentACK[i] = 1;
-        } else {
-            commentACK[i] = 0;
+void SOLONG_Input(int& dictLen, int& inputLen, vector<string>& dictWord, vector<int>& dictPriority, vector<string>& inputWord) {
+    cin >> dictLen >> inputLen;
+    dictWord.resize(dictLen);
+    dictPriority.resize(dictLen);
+    inputWord.resize(inputLen);
+    char strBuffer[11];
+    for (int i = 0; i < dictLen; i++) {
+        scanf("%s %d", strBuffer, &dictPriority[i]);
+        dictWord[i] = string(strBuffer);
+    }
+    for (int i = 0; i < inputLen; i++) {
+        scanf("%s", strBuffer);
+        inputWord[i] = string(strBuffer);
+    }
+}
+int SOLONG_Algo(int dictLen, int inputLen, const vector<string>& dictWord, const vector<int>& dictPriority, const vector<string>& inputWord) {
+    // make trie
+    TrieNode trieRoot;
+    for (int i = 0; i < dictLen; i++) {
+        trieRoot.insert(dictWord[i], dictPriority[i], i);
+    }
+    // priority find
+    int ret = 0;
+    for (int i = 0; i < inputLen; i++) {
+        int wordLen = inputWord[i].length();
+        auto findResult = trieRoot.find(inpurWord[i]);
+        if (findResult && findResult.terminal != -1) {
+            wordLen = min(wordLen, trieRoot.autoComplete(inputWord[i], findResult.terminal, findResult.priority));
         }
-    }
-}
-int EDITORWARS_Find(vector<int>& parents, int idx) {
-    if (parents[idx] == idx) {
-        return idx;
-    }
-    return parents[idx] = EDITORWARS_Find(parents, parents[idx]);
-}
-int EDITORWARS_union(vector<int>& parents, vector<int>& rank, vector<int>& setSize, int eleA, int eleB) {
-    // invalid union
-    if (eleA == -1 || eleB == -1) return max(eleA, eleB);
-    // union
-    int rootA = EDITORWARS_Find(parents, eleA), rootB = EDITORWARS_Find(parents, eleB);
-    if (rootA == rootB) return rootA;
-    if (rank[rootB] > rank[rootA]) {
-        swap(rootA, rootB);
-    } else if (rank[rootA] == rank[rootB]) {
-        rank[rootA]++;
-    }
-    parents[rootB] = rootA;
-    setSize[rootA] += setSize[rootB];
-    return rootA;
-}
-string EDITORWARS_Algo(int peopleNum, int commentNum, vector<int> commentACK, vector<pair<int, int>> commentElements) {
-    // union-find prepare
-    vector<int> parents(peopleNum);
-    for (int i = 0; i < peopleNum; i++) {
-        parents[i] = i;
-    }
-    vector<int> rank(peopleNum, 1);
-    vector<int> setSize(peopleNum, 1);
-    // disjoint[A]=B means A and B are disjoint set
-    vector<int> disjoint(peopleNum, -1);
-    // ACK makes union, DIS makes disjoint
-    for (int i = 0; i < commentNum; i++) {
-        int rootA = EDITORWARS_Find(parents, commentElements[i].first);
-        int rootB = EDITORWARS_Find(parents, commentElements[i].second);
-        if (commentACK[i]) {
-            if (rootA == rootB) continue;
-            if (disjoint[rootA] == rootB) {
-                return string("CONTRADICTION AT ") + to_string(i + 1);
-            }
-            int rootNew = EDITORWARS_union(parents, rank, setSize, rootA, rootB);
-            // union's enemy is my enemy, enemy's enemy is my union
-            int disNew = EDITORWARS_union(parents, rank, setSize, disjoint[rootA], disjoint[rootB]);
-            disjoint[rootNew] = disNew;
-            if (disNew != -1) disjoint[disNew] = rootNew;
-        } else {
-            if (rootA == rootB) {
-                return string("CONTRADICTION AT ") + to_string(i + 1);
-            }
-            if (disjoint[rootA] == rootB) continue;
-            // enemy's enemy is my union
-            rootB = EDITORWARS_union(parents, rank, setSize, disjoint[rootA], rootB);
-            rootA = EDITORWARS_union(parents, rank, setSize, disjoint[rootB], rootA);
-            disjoint[rootA] = rootB, disjoint[rootB] = rootA;
-        }
+        ret += wordLen;
     }
     // return
-    int maxPeople = 0;
-    for (int i = 0; i < peopleNum; i++) {
-        if (EDITORWARS_Find(parents, i) != i) continue;
-        if (i < disjoint[i]) continue;  // 중복 방지
-        int mySize = setSize[i];
-        int enemySize = (disjoint[i] == -1 ? 0 : setSize[disjoint[i]]);
-        maxPeople += max(mySize, enemySize);
-    }
-    return string("MAX PARTY SIZE IS ") + to_string(maxPeople);
+    return ret;
 }
-void EDITORWARS() {
+void SOLONG() {
     /*설명 및 입력
     설명
-        모든 회원들이 vi 혹은 Emacs를 사용하는 프로그래밍 동호회에서 연말 파티를 개최하려 합니다.
-            서로 다른 편집기를 사용하는 사람들이 파티에 함께 참가하면 싸움이 나기 때문에 vi를 사용하는 사람들만 오는 파티,
-            Emacs를 사용하는 사람들만 오는 파티를 따로 하기로 했습니다.
-            이를 위해 지금까지 모든 회원들이 쓴 댓글을 모아 이들을 두 종류로 분류했습니다.
-                상호 인정: 이 유형의 댓글은 댓글을 쓴 사람과 원글을 쓴 사람이 같은 편집기를 쓴다는 사실을 의미합니다.
-                상호 비방: 이 유형의 댓글은 댓글을 쓴 사람과 원글을 쓴 사람이 다른 편집기를 쓴다는 사실을 의미합니다.
-        이것만으로는 물론 각 사용자가 어떤 편집기를 쓰는지는 알 수 없지만,
-            우선 서둘러 장소를 예약해야 하기 때문에 이 정보만으로 파티에 올 수 있는 최대 인원을 알아야 합니다.
-        댓글 정보가 주어질 때 이 댓글 정보 중 모순되는 것은 없는지 확인하고,
-            모순되는 것이 없을 때 한 파티의 가능한 최대 인원을 계산하는 프로그램을 작성하세요.
+        이번에 출시한 돌고래용 타블렛의 자동 완성 알고리즘은 사전에 저장된 N개의 단어들을 이용해 자동완성을 수행합니다.
+            돌고래가 새 단어를 타이핑하기 시작하면,
+            자동 완성 시스템은 지금까지 타이핑한 부분으로 시작하는 사전 내 단어 중 가장 출현 빈도가 높은 것을 자동으로 추천합니다.
+            만약 출현 빈도가 가장 높은 것이 여러 개 있다면 사전순으로(돌고래의 사전 역시 알파벳 순을 따릅니다)
+            가장 앞에 오는 단어가 추천되지요. 돌고래가 탭 키를 누르면 지금 추천된 단어가 자동으로 입력됩니다.
+            자동완성을 할 수 없다면 모두 입력해야 합니다.
+        자동완성 예시
+            단어(빈도): SO(3) AND(4) ALL(3)
+            입력횟수: SO=S+자동(2), AND=A+자동(2), ALL=AL+자동(3), THANKS(6)
+        단 이 타블렛에는 이미 입력한 단어를 편집하는 기능이 없기 때문에,
+            THANKS를 입력하고 "S"를 지워서 "THANK"를 만들거나
+            "THE"를 입력하고 "RE"를 덧붙여서 "THERE"를 입력하는 것은 불가능합니다.
+        타블렛의 자동완성 알고리즘이 사용하는 단어들과 각 단어들의 출현 빈도가 주어질 때,
+            주어지는 긴 문자열을 입력하기 위해 필요한 최소 타이핑 수를 계산하는 프로그램을 작성하세요.
     입력
-        입력의 첫 줄에는 테스트 케이스의 수 C (1≤C≤50)가 주어집니다.
-            각 테스트 케이스의 첫 줄에는 회원의 수 N (1≤N≤10000)과 분석한 댓글의 개수 M (1≤M≤100000)이 주어집니다.
-            각 회원은 0에서 N - 1 범위의 숫자로 표현됩니다.
-        그 후 M줄에 하나씩 각 댓글의 정보가 주어집니다. 각 댓글은 상호 인정, 혹은 상호 비방 댓글입니다.
-            상호 인정 댓글은 “ACK a b”(0≤a, b<N) 형태로 주어지며 상호 비방 댓글은 “DIS a b” 형태로 주어집니다.
+        입력의 첫 줄에는 테스트 케이스의 수 C(1<= C <=10)가 주어집니다.
+        각 테스트 케이스의 첫 줄에는 사전에 포함된 단어의 수 N (1 <=N <= 10000)과
+            입력할 단어의 수 M (1 <= M <= 20000)이 주어집니다.
+        그 후 N 줄에는 한 줄에 하나의 문자열과 정수로 사전에 포함된 단어와
+            해당 단어의 출현 빈도가 사전 순서와 무관하게 주어집니다.
+            같은 단어가 두번 이상 주어지는 일은 없으며, 출현 빈도는 1 과 10만 사이의 정수입니다.
+        그 다음 줄에 M개의 단어로 입력할 문자열이 주어집니다. 모든 단어는 알파벳 대문자이며, 길이는 최대 10입니다.
+        입력의 양이 많으므로 가능한 빠른 입력 함수를 이용하는 것이 좋습니다.
     출력
-        각 테스트 케이스마다 한 줄을 출력합니다.
-        댓글에 주어진 정보 중 모순이 없는 경우 한 파티에 올 수 있는 사람의 최대 수를 “MAX PARTY SIZE IS x”의 형태로 출력합니다.
-        댓글들에 주어진 정보 중 모순이 있는 경우, 모순이 처음으로 발생하는 댓글이 몇 번인지를 “CONTRADICTION AT i” 형태로 출력합니다.
-            댓글의 번호는 입력에 주어진 순서대로 1부터 시작한다고 합시다.
+        각 테스트 케이스마다 한 줄에 모든 단어들을 입력하기 위해 필요한 최소 타자 수를 출력합니다
     제한조건
-        4초, 64MB
+        3초, 64MB
     */
     /*힌트
-        상호 배타적 집합(disjoint set) 방법
-            ACK는 union-find를 통해 집합지을 수 있다.
-            DIS는 추가적인 수행 방법이 필요하다.
-                1) DIS는 ACK를 모두 수행하고 두 그룹을 찾은 후에 수행한다.??
-                    1. 둘 모두 그룹이 있을경우, 모순 관계를 확인한다.
-                    2. 하나만 그룹이 있을경우, 다른 그룹에 나머지를 포함시킨다.
-                    3. 둘 모두 그룹이 없을경우
-                        3.1 나중에 둘중 하나가 그룹에 포함될 경우
-                        3.2 나중에도 둘 모두 그룹이 없을 경우
-                            3.2.1 그룹이 없는 것들끼리, DIS관계가 있을 경우
-                2) DIS가 발생하면, 각 원소를 각각 속하는 그룹의 root로 만들어버린다.??
-                    ACK와 관계없이 순서대로 수행 가능하다.
-                    모순이 발생한다 -> 모순 반환
-                    정답 반환: ??
-                3) disjoint관계를 배열로 표현한다.
-                    - disjoint는 항상 set의 root간의 관계로 표시한다.
-                    - set이 두개밖에 없으므로, disjoint 관계는 단 하나의 set과 가질 수 있다.
-                    - 각 연산시, disjoint관계를 고려하여 추가적인 union연산 및 dis배열 갱신이 필요할 수 있다.
-                        ACK A B
-                            dis[A] and dis[B] exist : union A&B, union dis[A]&dis[B], update dis배열
-                            dis[A] or dis[B] exist: union A&B, update dis배열
-                            dis[A] and dis[B] not exist: union A& B
-                        DIS A B
-                            dis[A] and dis[B] exist: union A&dis[B], union B&dis[A], update dis베열
-                            dis[A] or dis[B] exist: union A&dis[B], update ids배열 (dis[A]가 존재할경우는 A,B 반대로)
-                            dis[A] and dis[B] not exist: update dis배열
-                    수행시간
-                        time complexity: n*(union, find, disjoint) +counting(n) = O(n)
-                        mem complexity: rank(n)+count(n)+parent(n) = O(n)
-                3-1) 책의 경우를 참고하여 깔끔하게 정리
-                    ACK: union A&B, "아군의 적은 나의 적" 원리를 적용하여 union 및 dis배열 정리
-                    DIS: "적의 적은 나의 아군" 원리를 적용하여 union 및 dis배열 정리
+        time complexity 측면
+            단어가 1만개, 입력이 2만개므로 상당히 크다.
+                입력의 상당한 시간이 걸릴 수 있으므로, O(n^2)미만의 속도가 되어야 할 것이다.
+            빠른 문자열 입력
+                1. cin과 string을 이용한 무지성 입력
+                2. scanf로 입력받기, 공백까지만 입력받으므로 이걸 이용하자
+                3. scanf로 입력받으면서 동시에 trie에 넣어버리기
+        알고리즘
+            trie를 이용한 방법
+                complexity 추정
+                    mem complexity: 문자의 최대길이는 개수(1만)*길이(10)*child배열의길이와 다른 원소(26+1)*4바이트 = 10.8MB
+                    time complexity: 길이가 10이므로 input, find는 10밖에 소모하지 않는다.
+                                    입력이 많으므로 1억회라고 가정해도, 각 입력당 1000회 수행할 수 있다.
+                설명
+                    현재노드에서 자동완성을 한다면 반환할 원소의 terimal과 priority를 저장하도록 하여, 자동완성을 용이하게 한다.
+                        각각 autoTerminal, autoPriority로 저장한다.
+                방법
+                    노드
+                        children은 알파벳 대문자에 대해 저장한다.
+                        terminal을 0과1이 아닌, 각 원소의 idx로 한다.
+                        priority를 저장한다.
+                        현재 노드에서 자동완성할 시 반환되는 단어의 terminal을 autoTerminal로, priority를 autoPriority로 저장한다.
+                    입력
+                        trie의 입력을 수행한다.
+                        입력을 수행하면서, priority를 비교하며 terminal과 priority를 갱신한다.
+                            갱신: priority>autoPriority인 경우, priority=autoPriority 면서 사전순으로 우선일경우
+                    탐색
+                        trie의 탐색을 수행한다. 단어가 없으면 NULL반환
+                    자동완성 탐색
+                        탐색을 수행하며, terminal = autoTerminal이면 , 지나온 node의 갯수 + 1(탭) 반환
+                        만약 단어가 없다면 20 반환 (단어의 최대 길이가 10이므로 항상 무시된다)
+                complexity 계산
+                    mem complexity: 1만 * 10 * 원소의개수(26+3)*4바이트 = 11.6MB
+                    time complexity: O(M+N)
+                        trie 생성: N*단어의길이(10)*단어비교(10) = O(N), N=단어의 개수
+                        자동완성 탐색: M*단어의길이(10) = O(M), M=입력의개수
     */
     /*
     아이디어 1
         설명:
-            각 원소들의 집합은 disjoint set 형태이며, 전체 집합은 단 두개임을 이용한다.
-                DIS, 즉 disjoint 관계는 dis배열로 표현한다.
-                ACK는 set을 합치는 union 연산이라 생각하면 된다.
-                각 연산시, disjoint관계를 고려하여 추가적인 union연산 및 dis배열 갱신이 필요할 수 있다.
-                    ACK A B
-                        dis[A] and dis[B] exist : union A&B, union dis[A]&dis[B], update dis배열
-                        dis[A] or dis[B] exist: union A&B, update dis배열
-                        dis[A] and dis[B] not exist: union A& B
-                    DIS A B
-                        dis[A] and dis[B] exist: union A&dis[B], union B&dis[A], update dis베열
-                        dis[A] or dis[B] exist: union A&dis[B], update ids배열 (dis[A]가 존재할경우는 A,B 반대로)
-                        dis[A] and dis[B] not exist: update dis배열
-            disjoint연산이므로, 기본적인 연산은 union-find 방식을 채용했다.
-            정답을 셀 때, disjoint인 관계가 있다면 큰 것을 세고, 없다면 그냥 세면 된다.
+            힌트-알고리즘-trie를 이용한 방법
+            현재노드에서 자동완성을 한다면 반환할 원소의 terimal과 priority를 저장하도록 하여, 자동완성을 용이하게 한다.
+                각각 autoTerminal, autoPriority로 저장한다.
         성능:
-            time complexity: O(M)
+            time complexity: O(M+N), M=입력의 갯수, N=단어의 갯수
             mem complexity: O(N)
         한계:
             전체 set이 두개일때만 사용할 수 있는 방법이다.
@@ -189,11 +141,10 @@ void EDITORWARS() {
     cin >> testCase;
     //각 테스트케이스
     while (testCase--) {
-        int peopleNum, commentNum;
-        vector<int> commentACK;  // 0 is ACK, 1 is DIS
-        vector<pair<int, int>> commentElement;
-        EDITORWARS_Input(peopleNum, commentNum, commentACK, commentElement);
-        auto result = EDITORWARS_Algo(peopleNum, commentNum, commentACK, commentElement);
+        int dictLen, inputLen;
+        vector<string> dictWord, inputWord;  // 0 is ACK, 1 is DIS
+        vector<int> dictPriority SOLONG_Input(dictLen, inputLen, dictWord, dictPriority, inputWord);
+        auto result = SOLONG_Algo(dictLen, inputLen, dictWord, dictPriority, inputWord);
         // cout << "::::";
         cout << result << endl;
     }
@@ -202,7 +153,7 @@ void EDITORWARS() {
 int main(void) {
     // clock_t start,end;
     // start=clock();
-    EDITORWARS();
+    SOLONG();
     // end=clock();;
     // cout<<"time(s): "<<(double)(end-start)/CLOCKS_PER_SEC<<endl;
     return 0;
