a033aa5e06f7d079b95954f76cc652dc2c2c30b3
Rain <parkkwang2@naver.com>
2022-01-23 18:29:45 +0900

    rename

---
+++ study/Deep_learning_from_scratch_py/MNIST_deeplearning/study.py
@@ -0,0 +1,11 @@
+import sys, os
+from deeplearning_data.mnist import load_mnist
+
+# 처음 한 번은 다운로드를 해서 시간이 좀 걸린다.
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+# 각 데이터의 형상 출력
+print(x_train.shape)  # (60000,784) , 784 = 28*28, train image
+print(t_train.shape)  # (60000,), train label
+print(x_test.shape)  # (10000,784), test image
+print(t_test.shape)  # (10000,), test label
