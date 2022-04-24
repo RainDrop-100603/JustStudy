5ec017ae4d82ea0c37f92fd44dc3ca7052ac76c4
Rain <parkkwang2@naver.com>
2022-04-24 13:35:20 +0900

    t

--- graduation_project/prototype/220303/functions.py
+++ graduation_project/prototype/220303/functions.py
@@ -4,3 +4,6 @@ table1 = ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
 
 sound = f"{table1[sound_label%12]}{sound_label/12}"
 # A4 -> 57, C0~ G10
+
+def make_output_label():
+    
